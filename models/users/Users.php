f85355ab166699c78ac60c694587824257fe3e5d
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-09-30 13:23:44 +0300

    Users model stub

--- models/users/Users.php
+++ models/users/Users.php
@@ -1,23 +1,62 @@
 <?php
 
-/**
- * Class Users
- */
-class Users extends \yii\base\Model {
+namespace app\models\users;
 
+use Yii;
+
+/**
+ * This is the model class for table "sys_users".
+ *
+ * @property int $id
+ * @property string $username Отображаемое имя пользователя
+ * @property string $login Логин
+ * @property string $password Хеш пароля
+ * @property string $salt Unique random salt hash
+ * @property string $email email
+ * @property string $comment Служебный комментарий пользователя
+ * @property string $create_date Дата регистрации
+ * @property int $daddy ID зарегистрировавшего/проверившего пользователя
+ * @property int $deleted Флаг удаления
+ */
+class Users extends \yii\db\ActiveRecord {
 	/**
-	 * @param $id
-	 * @return Users
+	 * {@inheritdoc}
 	 */
-	public static function findModel($id) {
-		return new self();
+	public static function tableName() {
+		return 'sys_users';
 	}
 
 	/**
-	 * @param array $array
-	 * @return Users
+	 * {@inheritdoc}
 	 */
-	public static function findOne(array $array) {
-		return new self;
+	public function rules() {
+		return [
+			[['username', 'login', 'password', 'salt', 'email', 'create_date'], 'required'],
+			[['comment'], 'string'],
+			[['create_date'], 'safe'],
+			[['daddy', 'deleted'], 'integer'],
+			[['username', 'password', 'salt', 'email'], 'string', 'max' => 255],
+			[['login'], 'string', 'max' => 64],
+			[['login'], 'unique'],
+			[['email'], 'unique'],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'id' => 'ID',
+			'username' => 'Отображаемое имя пользователя',
+			'login' => 'Логин',
+			'password' => 'Хеш пароля',
+			'salt' => 'Unique random salt hash',
+			'email' => 'email',
+			'comment' => 'Служебный комментарий пользователя',
+			'create_date' => 'Дата регистрации',
+			'daddy' => 'ID зарегистрировавшего/проверившего пользователя',
+			'deleted' => 'Флаг удаления',
+		];
 	}
 }
