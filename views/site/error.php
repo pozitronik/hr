6b0fa9a06674770c8b2f0f583732e006f02eebb8
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-09-30 11:38:05 +0300

    Remove unused code from template

--- views/site/error.php
+++ views/site/error.php
@@ -1,27 +1,27 @@
 <?php
-
-/* @var $this yii\web\View */
-/* @var $name string */
-/* @var $message string */
-/* @var $exception Exception */
+/**
+ * Шаблон страницы ошибки
+ * @var yii\web\View $this
+ * @var string $name
+ * @var string $message
+ * @var Exception $exception
+ */
 
 use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+<hr class="new-section-sm bord-no">
+<div class="row">
+	<div class="col-lg-6 col-lg-offset-3">
+		<div class="panel panel-trans text-center">
+			<div class="panel-heading">
+				<h1 class="error-code text-primary"><?= Html::encode($this->title); ?></h1>
+			</div>
+			<div class="panel-body">
+				<p><?= nl2br(Html::encode($message)); ?></p>
+				<div class="pad-top"><a class="btn-link text-semibold" href="/">На главную</a></div>
+			</div>
+		</div>
+	</div>
 </div>
