e63053ad29d0c0e6391949852739569396174ae1
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-07 22:49:18 +0300

    Демонстрационный хардкод

--- models/workgroups/Workgroups.php
+++ models/workgroups/Workgroups.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace app\models\workgroups;
 
+use app\models\employees\Employees;
+use app\models\users\Users;
 use yii\db\ActiveRecord;
 
 /**
@@ -12,6 +14,8 @@ use yii\db\ActiveRecord;
  * @property string $name Название
  * @property string $comment Описание
  * @property int $deleted
+ *
+ * @property-read Employees[] $employees
  */
 class Workgroups extends ActiveRecord {
 	/**
@@ -42,5 +46,62 @@ class Workgroups extends ActiveRecord {
 			'comment' => 'Описание',
 			'deleted' => 'Deleted'
 		];
+	}
+
+	/**
+	 * @return Employees[]
+	 */
+	public function getEmployees():array {
+		switch ($this->id) {
+			case 1:
+				return [
+					new Users([
+						'id' => 2,
+						'username' => 'Артемий Лебедев'
+					]),
+					new Users([
+						'id' => 3,
+						'username' => 'Юрий Дудь'
+					]),
+					new Users([
+						'id' => 4,
+						'username' => 'Гомер Симпсон'
+					]),
+				];
+			break;
+			case 2:
+				return [
+					new Users([
+						'id' => 5,
+						'username' => 'Артемий Лебедев'
+					]),
+					new Users([
+						'id' => 6,
+						'username' => 'Юрий Дудь'
+					]),
+					new Users([
+						'id' => 7,
+						'username' => 'Гомер Симпсон'
+					]),
+				];
+			break;
+			default:
+				return [
+					new Users([
+						'id' => 8,
+						'username' => 'Артемий Лебедев'
+					]),
+					new Users([
+						'id' => 9,
+						'username' => 'Юрий Дудь'
+					]),
+					new Users([
+						'id' => 10,
+						'username' => 'Гомер Симпсон'
+					]),
+				];
+			break;
+		}
+
 	}
 }
