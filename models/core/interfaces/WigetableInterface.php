7a968fadbce1d351c83dc7e7c9d299be74ba85c6
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-07 17:55:41 +0300

    Пробный интерфейс

---
+++ models/core/interfaces/WigetableInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Interface WigetableInterface
+ * Расширения контроллера, позволяющие добавлять его в меню
+ */
+interface WigetableInterface {
+
+	/**
+	 * Возвращает название пуннкта меню, добавляемого контроллером
+	 * @return string
+	 */
+	public function getMenuCaption(): string;
+
+	/**
+	 * Возвращает путь к иконке пункта меню
+	 * @return string
+	 */
+	public function getControllerIcon(): string;
+
+}
