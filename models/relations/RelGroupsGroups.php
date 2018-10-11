51c1732e4fc07551157ef4dbeec7b99be74263a9
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-11 11:24:31 +0300

    Стаб модели связи групп

---
+++ models/relations/RelGroupsGroups.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models\relations;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "rel_groups_groups".
+ *
+ * @property int $id
+ * @property int $parent_id Вышестоящая группа
+ * @property int $child_id Нижестоящая группа
+ * @property int $relation Тип связи
+ */
+class RelGroupsGroups extends ActiveRecord {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName() {
+		return 'rel_groups_groups';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules() {
+		return [
+			[['parent_id', 'child_id'], 'required'],
+			[['parent_id', 'child_id', 'relation'], 'integer'],
+			[['parent_id', 'child_id', 'relation'], 'unique', 'targetAttribute' => ['parent_id', 'child_id', 'relation']],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'id' => 'ID',
+			'parent_id' => 'Вышестоящая группа',
+			'child_id' => 'Нижестоящая группа',
+			'relation' => 'Тип связи',
+		];
+	}
+}
