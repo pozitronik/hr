2beb54bc729d134885246ae322a7b924b641a029
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-15 12:26:23 +0300

    Добавил все плагины в ассеты

--- widgets/structure/StructureWidgetAssets.php
+++ widgets/structure/StructureWidgetAssets.php
@@ -17,8 +17,25 @@ class StructureWidgetAssets extends AssetBundle {
 	];
 	public $js = [
 		'js/sigma.js/sigma.min.js',
-		'js/sigma.js/plugins/sigma.parsers.json.min.js'
-
+		'js/sigma.js/plugins/sigma.parsers.json.min.js',
+//		'js/sigma.js/plugins/sigma.exporters.svg.min.js',
+		'js/sigma.js/plugins/sigma.layout.forceAtlas2.min.js',
+		'js/sigma.js/plugins/sigma.layout.noverlap.min.js',
+		'js/sigma.js/plugins/sigma.neo4j.cypher.min.js',
+//		'js/sigma.js/plugins/sigma.parsers.gexf.min.js',
+		'js/sigma.js/plugins/sigma.pathfinding.astar.min.js',
+		'js/sigma.js/plugins/sigma.plugins.animate.min.js',
+		'js/sigma.js/plugins/sigma.plugins.dragNodes.min.js',
+		'js/sigma.js/plugins/sigma.plugins.filter.min.js',
+		'js/sigma.js/plugins/sigma.plugins.neighborhoods.min.js',
+		'js/sigma.js/plugins/sigma.plugins.relativeSize.min.js',
+		'js/sigma.js/plugins/sigma.renderers.customEdgeShapes.min.js',
+		'js/sigma.js/plugins/sigma.renderers.customShapes.min.js',
+		'js/sigma.js/plugins/sigma.renderers.edgeDots.min.js',
+		'js/sigma.js/plugins/sigma.renderers.edgeLabels.min.js',
+		'js/sigma.js/plugins/sigma.renderers.parallelEdges.min.js',
+		'js/sigma.js/plugins/sigma.renderers.snapshot.min.js',
+		'js/sigma.js/plugins/sigma.statistics.HITS.min.js',
 	];
 	public $depends = [
 		AppAsset::class
