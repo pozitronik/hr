a4311c5ad63176a8f83bdf2d2d7d6509247e0134
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-16 11:26:44 +0300

    Немного переделал стили, чтобы корректно отображались иконки непрописанных контроллеров

--- widgets/controller/views/controller.php
+++ widgets/controller/views/controller.php
@@ -10,20 +10,20 @@ use app\models\core\WigetableController;
 use yii\web\View;
 use yii\helpers\Url;
 
-$icon_url = Magic::hasProperty($model, 'menuIcon')?$model->menuIcon:"/img/admin/widget.png";//todo
+$style = (Magic::hasProperty($model, 'menuIcon'))?"style = 'background-image: url({$model->menuIcon});'":'';
 $action = ["{$model->route}/{$model->defaultAction}"];
 $caption = Magic::hasProperty($model, 'menuCaption')?$model->menuCaption:$model->id;
 ?>
 
 <a href="<?= Url::toRoute($action); ?>" class="icon">
-			<div class="panel panel-icon">
-				<div class="panel-body">
-					<div class="text-center bord-btm">
-						<img src="<?= $icon_url ?>">
-					</div>
-				</div>
-<!--				<div class="text-sm-center"><?= $caption ?><</div>-->
+	<div class="panel panel-icon">
+		<div class="panel-body">
+			<div class="text-center bord-btm">
+				<div class="menu-widget-icon" <?= $style ?> ></div>
 			</div>
+		</div>
+		<!--				<div class="text-sm-center"><?= $caption ?><</div>-->
+	</div>
 
 </a>
 
