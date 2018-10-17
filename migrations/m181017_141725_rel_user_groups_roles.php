183dab5a3dcaeb371ca421719e0d49dc0a54f965
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-17 17:25:47 +0300

    tablename fix

--- migrations/m181017_141725_rel_user_groups_roles.php
+++ migrations/m181017_141725_rel_user_groups_roles.php
@@ -10,12 +10,12 @@ class m181017_141725_rel_user_groups_roles extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->createTable('rel_user_groups_roles', [
+		$this->createTable('rel_users_groups_roles', [
 			'user_group_id' => $this->integer()->notNull()->comment('ID связки пользователь/группа'),
 			'role' => $this->integer()->notNull()->comment('Роль')
 		]);
 
-		$this->createIndex('user_group_id_role', 'rel_user_groups_roles', ['user_group_id', 'role'], true);
+		$this->createIndex('user_group_id_role', 'rel_users_groups_roles', ['user_group_id', 'role'], true);
 	}
 
 	/**
