fcf91e4fd0e3e6af6019e0c4e96c6c8390719d59
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-17 17:26:32 +0300

    Моделька на связку

---
+++ models/relations/RelUsersGroupsRoles.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models\relations;
+
+use Yii;
+
+/**
+ * This is the model class for table "rel_users_groups_roles".
+ *
+ * @property int $user_group_id ID связки пользователь/группа
+ * @property int $role Роль
+ */
+class RelUsersGroupsRoles extends \yii\db\ActiveRecord {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName() {
+		return 'rel_users_groups_roles';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules() {
+		return [
+			[['user_group_id', 'role'], 'required'],
+			[['user_group_id', 'role'], 'integer'],
+			[['user_group_id', 'role'], 'unique', 'targetAttribute' => ['user_group_id', 'role']],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'user_group_id' => 'ID связки пользователь/группа',
+			'role' => 'Роль',
+		];
+	}
+}
