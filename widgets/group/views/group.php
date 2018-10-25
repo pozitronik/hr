cb1e6b466b95a987895e8e77bad7ff95f6e66acf
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-25 11:01:34 +0300

    Прототип панелек в группах

--- widgets/group/views/group.php
+++ widgets/group/views/group.php
@@ -17,15 +17,31 @@ if (null === $users) $users = $group->relUsers;
 
 <div class="panel">
 	<div class="panel-heading">
+		<div class="panel-control">
+			<ul class="nav nav-tabs">
+				<li class="active"><a href="#tab1-<?= $group->id ?>" data-toggle="tab">Пользователи</a></li>
+				<li><a href="#tab2-<?= $group->id ?>" data-toggle="tab">Описание</a></li>
+			</ul>
+		</div>
 		<h3 class="panel-title"><?= $group->name; ?></h3>
 	</div>
 	<div class="panel-body">
-		<?php foreach ($users as $user): ?>
-			<?= UserWidget::widget([
-				'user' => $user,
-				'mode' => $group->isLeader($user)?'boss':'user'
-			]) ?>
-		<?php endforeach; ?>
+		<div class="tab-content">
+			<div class="tab-pane fade in active" id="tab1-<?= $group->id ?>">
+				<?php foreach ($users as $user): ?>
+					<?= UserWidget::widget([
+						'user' => $user,
+						'mode' => $group->isLeader($user)?'boss':'user'
+					]); ?>
+				<?php endforeach; ?>
+			</div>
+			<div class="tab-pane fade" id="tab2-<?= $group->id ?>">
+				<?= $group->comment ?>
+			</div>
+		</div>
+
+
+
 	</div>
 </div>
 
