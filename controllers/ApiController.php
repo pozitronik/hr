ca8a6aaceb22151cb127118e67d793e9d3fed558
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-10-31 12:24:44 +0300

    Стаб API-контроллера

---
+++ controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\controllers;
+
+use Yii;
+use yii\base\Response;
+use yii\web\Controller;
+use yii\web\ErrorAction;
+
+/**
+ * Class ApiController
+ * @package app/controllers;
+ */
+class ApiController extends Controller {
+
+	/**
+	 * @return string|Response
+	 */
+	public function actionIndex() {
+		//TODO
+	}
+
+}
