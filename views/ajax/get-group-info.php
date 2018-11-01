4b8a4310e5cec2b9ffa643773f523bf6262a7c25
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-01 16:50:23 +0300

    Потешный показометр

--- views/ajax/get-group-info.php
+++ views/ajax/get-group-info.php
@@ -10,6 +10,11 @@ use app\helpers\ArrayHelper;
  * @var View $this
  * @var Groups $group
  **/
+
+$random1 = random_int(1, 100000);
+$random2 = random_int(1, 100000);
+$leaderName = implode(", ", ArrayHelper::getColumn($group->leaders, 'username'));
+$leaderName = empty($leaderName)?'Не указан':$leaderName;
 ?>
 <?= DetailView::widget([
 	'model' => $group,
@@ -22,9 +27,27 @@ use app\helpers\ArrayHelper;
 		],
 		[
 			'label' => 'Лидер',
-			'value' => implode(", ", ArrayHelper::getColumn($group->leaders, 'username'))
+			'value' => $leaderName
 		],
-		'comment'
+		[
+			'attribute' => 'comment'
+		]
 	]
 ]); ?>
+
+<div class="panel panel-info panel-colorful">
+	<div class="pad-all">
+		<p class="text-lg text-semibold"><i class="glyphicon glyphicon-ruble"></i> Показометр</p>
+		<p class="mar-no">
+			<span class="pull-right text-bold">
+				<?= $random1 ?>&#8381;</span>
+			Заработано
+		</p>
+		<p class="mar-no">
+			<span class="pull-right text-bold">
+				<?= $random2 ?>&#8381;</span>
+			Истрачено
+		</p>
+	</div>
+</div>
 
