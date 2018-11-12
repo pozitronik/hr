5147f42db77c9a0345a0d41f775b7b390819d6a5
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-12 11:46:18 +0300

    Модальная вьюха на будущее (просто жалко выкидывать)

---
+++ widgets/bookmarks/views/bookmarks_modal.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * Пока неиспользуемая модальная форма добавления (с редактором)
+ * @var View $this
+ * @var Users $user
+ */
+
+use app\models\users\Bookmarks;
+use app\models\users\Users;
+use yii\web\View;
+use yii\bootstrap\Modal;
+use kartik\form\ActiveForm;
+use kartik\select2\Select2;
+use yii\helpers\Html;
+
+$model = new Bookmarks([
+	'route' => Yii::$app->requestedRoute,
+	'name' => $this->title,
+	'type' => Bookmarks::TYPE_DEFAULT
+]);
+
+?>
+<?= $this->render('list', [
+	'bookmarks' => $user->options->bookmarks,
+	'model' => $model
+]); ?>
+<?php Modal::begin([
+	'header' => 'Добавление закладки',
+	'toggleButton' => [
+		'class' => 'btn btn-default btn-xs pull-right',
+		'label' => 'Добавить'
+	]
+]);
+?>
+
+<?php $form = ActiveForm::begin(); ?>
+
+<?= $form->field($model, 'route')->textInput() ?>
+<?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+<?= $form->field($model, 'type')->widget(Select2::class, [
+	'data' => Bookmarks::Types
+]); ?>
+
+	<div class="form-group">
+		<?= Html::a('Добавить', "javascript:add_bookmark('{$model->route}','{$model->name}','{$model->type}')", ['class' => 'btn btn-primary', 'name' => 'add-bookmark']) ?>
+	</div>
+
+<?php ActiveForm::end(); ?>
+<?php Modal::end(); ?>
