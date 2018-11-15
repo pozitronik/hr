349415f40cf8630b5f08538606a0f75218c6a5a9
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-15 12:42:44 +0300

    Фикс навигатора

--- views/admin/groups/tree.php
+++ views/admin/groups/tree.php
@@ -10,9 +10,10 @@ use app\models\groups\Groups;
 use app\widgets\structure\StructureWidget;
 use yii\web\View;
 
-$this->title = 'Граф связей команды '.Groups::findModel($id)->name;
+$this->title = 'Граф связей';
 $this->params['breadcrumbs'][] = ['label' => 'Управление', 'url' => ['/admin']];
 $this->params['breadcrumbs'][] = ['label' => 'Команды', 'url' => ['/admin/groups']];
+$this->params['breadcrumbs'][] = ['label' => Groups::findModel($id)->name, 'url' => ['/admin/groups/update', 'id' => $id]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
