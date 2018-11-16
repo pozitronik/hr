82c0a115b346099ef12df55674898ea6f56cc2c1
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-16 11:31:09 +0300

    Миграция на таблицу структуры компетенции

---
+++ migrations/m181116_082318_sys_competency.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181116_082318_sys_competency
+ */
+class m181116_082318_sys_competency extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable('sys_competencies', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(255)->notNull()->comment('Название компетенции'),
+			'category' => $this->integer()->null()->comment('Категория'),
+			'daddy' => $this->integer()->null()->comment('Создатель'),
+			'create_date' => $this->dateTime()->comment('Дата создания'),
+			'structure' => $this->json()->notNull()->comment('Структура'),
+			'deleted' => $this->boolean()->defaultValue(false)->notNull()->comment('Флаг удаления')
+		]);
+		$this->createIndex('name', 'sys_competencies', 'name');
+		$this->createIndex('category', 'sys_competencies', 'category');
+
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable('sys_competencies');
+	}
+
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m181116_082318_sys_competency cannot be reverted.\n";
+
+		return false;
+	}
+	*/
+}
