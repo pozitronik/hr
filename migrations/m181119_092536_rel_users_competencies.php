363037458593d1c1f807db5223e7727789ba8c34
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-19 12:32:36 +0300

    Миграция на таблицу связей пользователей и компетенции

---
+++ migrations/m181119_092536_rel_users_competencies.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181119_092536_rel_users_competencies
+ */
+class m181119_092536_rel_users_competencies extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable('rel_users_competencies', [
+			'id' => $this->primaryKey(),
+			'user_id' => $this->integer()->notNull(),
+			'competency_id' => $this->integer()->notNull()
+		]);
+
+		$this->createIndex('user_id', 'rel_users_competencies', 'user_id');
+		$this->createIndex('competency_id', 'rel_users_competencies', 'competency_id');
+		$this->createIndex('user_id_competency_id', 'rel_users_competencies', ['user_id', 'competency_id']);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable('rel_users_competencies');
+	}
+
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m181119_092536_rel_users_competencies cannot be reverted.\n";
+
+		return false;
+	}
+	*/
+}
