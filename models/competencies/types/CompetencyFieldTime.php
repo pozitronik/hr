d4ec900b528033d4295bdf9fad3c7fb1d220d1d0
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-19 17:47:12 +0300

    Забыл модель

---
+++ models/competencies/types/CompetencyFieldTime.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\competencies\types;
+
+/**
+ * This is the model class for table "sys_competencies_time".
+ *
+ * @property int $id
+ * @property int $competency_id ID компетенции
+ * @property int $field_id ID поля
+ * @property int $user_id ID пользователя
+ * @property string $value Значение
+ */
+class CompetencyFieldTime extends CompetencyFieldDefault {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'sys_competencies_time';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['competency_id', 'field_id', 'user_id'], 'required'],
+			[['competency_id', 'field_id', 'user_id'], 'integer'],
+			[['value'], 'safe'],
+			[['competency_id', 'field_id', 'user_id'], 'unique', 'targetAttribute' => ['competency_id', 'field_id', 'user_id']]
+		];
+	}
+
+}
