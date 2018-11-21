f74ff1d8dabc925a8be49aaa8f7cf11f98e217f9
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-11-21 12:18:19 +0300

    Фиксанчик

--- views/admin/competencies/field/update.php
+++ views/admin/competencies/field/update.php
@@ -11,13 +11,14 @@ use app\models\competencies\Competencies;
 use app\models\competencies\CompetencyField;
 use yii\web\View;
 
-$this->title = "Редактирование поля {$model->name} компетенции { $competency->name}";
+$this->title = "Редактирование поля {$model->name}";
 $this->params['breadcrumbs'][] = ['label' => 'Управление', 'url' => ['/admin']];
 $this->params['breadcrumbs'][] = ['label' => 'Компетенции', 'url' => ['/admin/competencies']];
+$this->params['breadcrumbs'][] = ['label' => $competency->name, 'url' => ['/admin/competencies/update', 'id' => $competency->id]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <?= $this->render('_form', [
-	'model' => $model]
+		'model' => $model]
 );
 ?>
