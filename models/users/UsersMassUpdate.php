613294a175818a4f1e35ef8e5a38641a8ea41064
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-04 12:44:54 +0300

    Модель массового редактирования пользюков

--- models/users/UsersMassUpdate.php
+++ models/users/UsersMassUpdate.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 namespace app\models\users;
 
+use app\helpers\ArrayHelper;
+use app\models\groups\Groups;
+use app\widgets\alert\AlertModel;
 use yii\base\Model;
 
 /**
@@ -12,10 +15,12 @@ use yii\base\Model;
  *
  * @property int[] $usersId
  * @property Users $virtualUser
+ * @property-read Users[] $users
  */
 class UsersMassUpdate extends Model {
 	private $usersId = [];
 	private $virtualUser;
+	private $relCompetencies; //Поскольку модель пользователя сразу применяет переданные компетенции, то в вирутальную модель вводим переменную для хранения.
 
 	/**
 	 * @inheritdoc
@@ -35,29 +40,90 @@ class UsersMassUpdate extends Model {
 	}
 
 	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels():array {
+		return [
+			'usersId' => 'Пользователи'
+		];
+	}
+
+	/**
 	 * Нужно подгрузить переданные постом свойства
 	 * @param array $data
 	 * @param null|string $formName
 	 * @return bool
 	 */
 	public function load($data, $formName = null):bool {
-		return parent::load($data, $formName) && $this->virtualUser->load($data[$this->virtualUser->classNameShort], '');
+		if (parent::load($data, $formName) && $this->virtualUser->load($data[$this->virtualUser->classNameShort], '')) {
+			/*Параметры, которые в модели пользователя применяются без промежуточного сохранения нам нужно всё-таки хранить*/
+			$this->relCompetencies = ArrayHelper::getValue($data, "{$this->virtualUser->classNameShort}.relCompetencies");
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	 * Применяет загруженные данные к пользователям, возвращает массив результатов
 	 * return array
 	 */
-	public function apply():array{
+	public function apply():array {
+		$statistic = [];
+		$paramsArray = [];
+		if (!empty($this->relCompetencies)) {
+			$paramsArray['relCompetencies'] = $this->relCompetencies;
+		}
 
+		foreach ($this->usersId as $userId) {
+			if (false !== $user = Users::findModel($userId)) {
+				if ($user->updateUser($paramsArray)) {
+					$statistic[] = [
+						'id' => $userId,
+						'username' => $user->username,
+						'status' => "Пользователь {$user->username} изменён",
+						'error' => false
+					];
+				} else {
+					$statistic[] = [
+						'id' => $userId,
+						'username' => $user->username,
+						'status' => "Пользователь {$user->username} не изменён, ошибки: ".AlertModel::ArrayErrors2String($user->errors),
+						'error' => true
+					];
+				}
+			} else {
+				$statistic[] = [
+					'id' => $userId,
+					'username' => false,
+					'status' => "Пользователь id={$userId} не найден",
+					'error' => true
+				];
+			}
+		}
+		return $statistic;
+	}
+
+	/**
+	 * Пытается загрузить пользователей группы для массовой обработки + генерирует доступные наборы параметров
+	 * @param int $groupId
+	 */
+	public function loadGroupSelection(int $groupId) {
+		if (false !== $group = Groups::findModel($groupId)) {
+			$this->usersId = ArrayHelper::getColumn($group->relUsers, 'id');
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	 * Пытается загрузить массив id пользователей для массовой обработки + генерирует доступные наборы параметров
 	 * @param int[] $selection
 	 */
-	public function loadSelection(array $selection):void {
-
+	public function loadSelection(array $selection):bool {
+		if ([] !== $users = Users::findModels($selection)) {
+			$this->usersId = $selection;
+		}
+		return false;
 	}
 
 	/**
@@ -87,4 +153,12 @@ class UsersMassUpdate extends Model {
 	public function setVirtualUser($virtualUser):void {
 		$this->virtualUser = $virtualUser;
 	}
+
+	/**
+	 * @return Users[]
+	 */
+	public function getUsers():array {
+		return Users::findModels($this->usersId);
+	}
+
 }
