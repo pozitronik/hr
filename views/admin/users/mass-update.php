b52009bf01122ecdca1b02f0384959ca1302becd
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-04 12:38:49 +0300

    Вьюха группового редактирования пользователей

--- views/admin/users/mass-update.php
+++ views/admin/users/mass-update.php
@@ -1,10 +1,113 @@
 <?php
 declare(strict_types = 1);
 
-use app\models\users\UsersMassUpdate;
-use yii\data\ArrayDataProvider;
-use yii\web\View;
+/**
+ * Шаблон страницы группового изменения пользователя
+ * @var View $this
+ * @var UsersMassUpdate $massUpdateModel
+ * @var ArrayDataProvider|null $statistics Статистика предыдущей операции
+ * @var array $competenciesData
+ * @var false|Groups $group
+ */
 
-/* @var UsersMassUpdate $massUpdateModel */
-/* @var View $this */
-/* @var null|ArrayDataProvider $statistics */
+use app\models\groups\Groups;
+use app\models\users\UsersMassUpdate;
+use kartik\select2\Select2;
+use app\helpers\ArrayHelper;
+use yii\data\ArrayDataProvider;
+use yii\helpers\Html;
+use yii\web\View;
+use yii\widgets\ActiveForm;
+use yii\grid\GridView;
+
+$this->title = 'Групповое изменение пользователей';
+$this->params['breadcrumbs'][] = ['label' => 'Управление', 'url' => ['/admin']];
+
+if ((false === $group)) {
+	$this->params['breadcrumbs'][] = ['label' => 'Люди', 'url' => ['/admin/users']];
+} else {
+	$this->params['breadcrumbs'][] = ['label' => 'Группы', 'url' => ['/admin/groups']];
+	$this->params['breadcrumbs'][] = ['label' => $group->name, 'url' => ['/admin/groups/update', 'id' => $group->id]];
+}
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+	<div class="row">
+		<div class="col-xs-12">
+			<?php $form = ActiveForm::begin(); ?>
+			<div class="panel">
+				<div class="panel-heading">
+					<h3 class="panel-title"><?= $this->title; ?></h3>
+				</div>
+				<div class="panel-body">
+					<div class="form-group">
+						<div class="row">
+							<div class="col-md-12">
+								<?= $form->field($massUpdateModel, 'usersId')->widget(Select2::class, [
+									'data' => ArrayHelper::map($massUpdateModel->users, 'id', 'username'),
+									'value' => $massUpdateModel->usersId,
+									'pluginOptions' => [
+										'allowClear' => false,
+										'multiple' => true
+									]
+								]); ?>
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="row">
+							<div class="col-md-12">
+								<?= $this->render('competencies/index', [
+									'user' => $massUpdateModel->virtualUser,
+									'data' => $competenciesData
+								]); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">
+					<?= Html::submitButton('Применить', ['class' => 'btn btn-success']); ?>
+				</div>
+			</div>
+		</div>
+		<?php ActiveForm::end(); ?>
+	</div>
+
+<?php if (null !== $statistics): ?>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="panel">
+				<div class="panel-heading">
+					<h3 class="panel-title">Результат обработки</h3>
+				</div>
+				<div class="panel-body">
+					<?= GridView::widget([
+						'dataProvider' => $statistics,
+						'columns' => [
+							[
+								'attribute' => 'id',
+								'label' => 'id пользователя',
+								'value' => function($array) {
+									return Html::a(ArrayHelper::getValue($array, 'username'), ['admin/users/update', 'id' => ArrayHelper::getValue($array, 'id')]);
+								},
+								'format' => 'raw'
+							],
+							[
+								'attribute' => 'status',
+								'label' => 'Статус'
+							],
+							[
+								'attribute' => 'error',
+								'label' => 'Ошибка',
+								'format' => 'boolean'
+							]
+						],
+						'rowOptions' => function($array) {
+							return ['class' => ArrayHelper::getValue($array, 'error')?'danger':'success'];
+						}
+					]); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
