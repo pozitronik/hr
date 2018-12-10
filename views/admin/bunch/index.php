8c669dbc12ad3c028dc0704f0c21bbc5b83f4083
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-10 17:49:33 +0300

    Масс-редактор: контролы удаления свойств во вьюхах (пока без логики)

--- views/admin/bunch/index.php
+++ views/admin/bunch/index.php
@@ -12,10 +12,8 @@ declare(strict_types = 1);
 
 use app\models\groups\Groups;
 use app\models\users\UsersMassUpdate;
-use app\widgets\group_select\GroupSelectWidget;
 use kartik\select2\Select2;
 use app\helpers\ArrayHelper;
-use yii\data\ActiveDataProvider;
 use yii\data\ArrayDataProvider;
 use yii\helpers\Html;
 use yii\web\View;
@@ -57,27 +55,57 @@ $this->params['breadcrumbs'][] = $this->title;
 					</div>
 					<div class="form-group">
 						<div class="row">
-							<div class="col-md-12">
-								<?= GroupSelectWidget::widget([
+							<div class="col-md-6">
+								<?= Select2::widget([
 									'model' => $massUpdateModel->virtualUser,
 									'attribute' => 'relGroups',
-									'notData' => $massUpdateModel->virtualUser->relGroups,
-									'multiple' => true
-								]) ?>
+									'name' => 'relGroups',
+									'data' => ArrayHelper::map(Groups::find()->all(), 'id', 'name'),
+									'options' => [
+										'multiple' => true,
+										'placeholder' => 'Добавить в группы'
+									]
+								]); ?>
+
+							</div>
+							<div class="col-md-6">
+								<?= Select2::widget([
+									'model' => $massUpdateModel->virtualUser,
+									'attribute' => 'dropGroups',
+									'name' => 'dropGroups',
+									'data' => ArrayHelper::map(Groups::find()->all(), 'id', 'name'),
+									'options' => [
+										'multiple' => true,
+										'placeholder' => 'Убрать из групп'
+									]
+								]); ?>
+
 							</div>
 						</div>
 					</div>
 					<div class="form-group">
 						<div class="row">
-							<div class="col-md-12">
-								<?=  Select2::widget([
+							<div class="col-md-6">
+								<?= Select2::widget([
 									'model' => $massUpdateModel->virtualUser,
 									'attribute' => 'relCompetencies',
-									'name' => 'competency_id',
+									'name' => 'relCompetencies',
 									'data' => $competenciesData,
 									'options' => [
 										'multiple' => true,
-										'placeholder' => 'Добавить компетенцию'
+										'placeholder' => 'Добавить компетенции'
+									]
+								]) ?>
+							</div>
+							<div class="col-md-6">
+								<?= Select2::widget([
+									'model' => $massUpdateModel->virtualUser,
+									'attribute' => 'dropCompetencies',
+									'name' => 'dropCompetencies',
+									'data' => $competenciesData,
+									'options' => [
+										'multiple' => true,
+										'placeholder' => 'Удалить компетенции'
 									]
 								]) ?>
 							</div>
