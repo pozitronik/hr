bbf2497d784bc263930a67beb66d093e83b92c57
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-12 11:02:07 +0300

    changing type of field

--- migrations/m181212_073756_refuserrolesnewfields.php
+++ migrations/m181212_073756_refuserrolesnewfields.php
@@ -11,7 +11,7 @@ class m181212_073756_refuserrolesnewfields extends Migration {
 	 */
 	public function safeUp() {
 		$this->addColumn('ref_user_roles', 'boss_flag', $this->boolean()->notNull()->defaultValue(false)->comment('Флаг лидера группы'));
-		$this->addColumn('ref_user_roles', 'color', $this->integer()->null()->comment('Цветокод'));
+		$this->addColumn('ref_user_roles', 'color', $this->string(255)->null()->comment('Цветокод'));
 		$this->createIndex('boss_flag', 'ref_user_roles', 'boss_flag');
 		$this->createIndex('color', 'ref_user_roles', 'color');
 
