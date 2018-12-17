df1d81067c0d43d71b7c7d0df43534fc16198880
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-17 11:10:44 +0300

    Прототип модели областей видимости

---
+++ models/prototypes/Scopes.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes;
+
+use yii\base\Model;
+
+/**
+ * Class Scopes
+ * Прототипируем области видимости
+ */
+class Scopes extends Model {
+
+	public function DefineScope() {
+
+	}
+}
