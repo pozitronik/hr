fd6c0ecb089df500198d1ef8a7ec35c5bae85a0e
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-17 11:03:53 +0300

    Прототип модели разрешений

---
+++ models/prototypes/Permissions.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes;
+
+use app\models\groups\Groups;
+use app\models\users\Users;
+use yii\base\Model;
+
+/**
+ * Class Permissions
+ * Определение доступов пользователя по отношению к группам пользователей.
+ * Право на чтение группы (read): видит всех пользователей (без права изменения). Предполагается, что все видят всех, поэтому есть как есть. Возможно, потом сделаю разделение по владельцам, как в linux
+ * (это не будет эффективно с точки зрения SQL, поэтому пока можно без - не делаю).
+ * Право на изменение (write): может редактировать пользователей в группе. НЕ РАСПРОСТРАНЯЕТСЯ по дереву
+ * Право на полное изменение (full-write): может редактировать пользователей в группе и всех входящих в неё (распространяется по дереву).
+ * Право на управление группой (master): может менять положение группы в структуре и её атрибуты.
+ *
+ * @property-write Users $user (проверяемый пользователь).
+ */
+class Permissions extends Model {
+	private $user;
+
+	/**
+	 * @param Groups $group
+	 * @return bool
+	 */
+	public function isGroupWritable(Groups $group):bool {
+
+	}
+
+	/**
+	 * @param Users $user
+	 * @return bool
+	 */
+	public function isUserWritable(Users $user):bool {
+
+	}
+
+	/**
+	 * @param Groups $group
+	 * @return bool
+	 */
+	public function isGroupSubmitted(Groups $group):bool {
+
+	}
+
+	/**
+	 * @param Users $user
+	 */
+	public function setUser(Users $user):void {
+		$this->user = $user;
+	}
+}
