2fc5e9f3bdf20e158348c267ac929ab64cfc1794
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-24 15:36:41 +0300

    Всякие протитипирования иконок.

--- helpers/Icons.php
+++ helpers/Icons.php
@@ -14,6 +14,50 @@ class Icons {
 	 * @return string
 	 */
 	public static function trash():string {
-		return "<i class='fa fa-trash'></i>";
+		return "<i class='fa fa-trash' title='Отметить к удалению'></i>";
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function users():string {
+		return "<i class='fa fa-user-friends' title='Пользователи'></i>";
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function subgroups():string {
+		return "<i class='fa fa-users' title='Подгруппы'></i>";
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function network():string {
+		return "<i class='fa fa-chart-network'></i>";
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function menu():string {
+		return "<i class='fa fa-bars'></i>";
+	}
+	/**
+	 * @return string
+	 */
+	public static function menu_caret():string {
+		return "<i class='fa chevron-circle-down'></i>";
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function users_edit():string {
+		return "<span class='fa-stack fa-2x'>
+		<i class='fa fa-users'></i>
+		<i class='fa fa-edit fa-stack-1x fa-inverse'></i>
+		</span>";
 	}
 }
