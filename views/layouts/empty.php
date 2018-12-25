33fbda579273e200eb36732e913b5c85680840d0
Pavel Dubrovsky <antikillerxxl@gmail.com>
2018-12-25 11:55:26 +0300

    Import

--- views/layouts/empty.php
+++ views/layouts/empty.php
@@ -3,8 +3,9 @@ declare(strict_types = 1);
 //Лейаут для просмотра внешних хтмлок, присланных, например, аттачами в письмах
 
 /**
- * @var \yii\web\View $this
+ * @var View $this
  * @var string $content
  */
-?>
+
+use yii\web\View; ?>
 <?= $content; ?>
