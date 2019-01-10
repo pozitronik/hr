348ed7cee66fb17741b8285d3f0dbd58120ca20c
Pozitronik <antikillerxxl@gmail.com>
2019-01-10 10:39:46 +0300

    Базовые свойства для связи привилегий и прав (R/W)

--- models/user_rights/Privileges.php
+++ models/user_rights/Privileges.php
@@ -8,6 +8,7 @@ use app\helpers\Date;
 use app\models\core\LCQuery;
 use app\models\core\Magic;
 use app\models\core\traits\ARExtended;
+use app\models\relations\RelPrivilegesRights;
 use app\models\user\CurrentUser;
 use app\widgets\alert\AlertModel;
 use RecursiveDirectoryIterator;
@@ -15,6 +16,7 @@ use RecursiveIteratorIterator;
 use ReflectionException;
 use Yii;
 use yii\base\UnknownClassException;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\db\Exception;
 
@@ -22,10 +24,15 @@ use yii\db\Exception;
  * Class Privileges
  * Набор прав пользователя
  * @package app\models\user_rights\rights
+ * @property int $id
  * @property string name
  * @property int $daddy
  * @property string $create_date
  * @property bool $deleted
+ *
+ * @property ActiveQuery|LCQuery|RelPrivilegesRights[] $relPrivilegesRights
+ * @property string[] $userRightsNames
+ * @property UserRightInterface[] $userRights
  */
 class Privileges extends ActiveRecord {
 	use ARExtended;
@@ -50,10 +57,11 @@ class Privileges extends ActiveRecord {
 	 */
 	public function rules():array {
 		return [
-			[['daddy'], 'integer'],
+			[['id', 'daddy'], 'integer'],
 			[['create_date'], 'safe'],
 			[['deleted'], 'boolean'],
 			[['name'], 'string', 'max' => 256],
+			[['userRightsNames'], 'safe']
 		];
 	}
 
@@ -101,26 +109,64 @@ class Privileges extends ActiveRecord {
 	/**
 	 * Возвращает массив всех возможных прав
 	 * @param string $path
-	 * @return array
+	 * @return UserRightInterface[]
 	 * @throws ReflectionException
 	 * @throws UnknownClassException
 	 */
-	public static function GetRightsList(string $path = self::RIGHTS_DIRECTORY):array {
+	public static function GetRightsList(string $path = self::RIGHTS_DIRECTORY, array $exludedRights = []):array {
 		$result = [];
 
 		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(Yii::getAlias($path)), RecursiveIteratorIterator::SELF_FIRST);
 		/** @var RecursiveDirectoryIterator $file */
 		foreach ($files as $file) {
-			if ($file->isFile() && 'php' === $file->getExtension() && false !== $model = Magic::GetUserRightModel($file->getRealPath())) {
-				$result[] = $model;
+			if ($file->isFile() && 'php' === $file->getExtension() && null !== $model = Magic::GetUserRightModel($file->getRealPath())) {
+				if (!in_array($model->formName(), $exludedRights)) $result[] = $model;
 			}
 		}
 		return $result;
 	}
 
-	public function getRelUserRights() {
+	/**
+	 * Связь с именами классов
+	 * @return ActiveQuery|LCQuery|RelPrivilegesRights[]
+	 */
+	public function getRelPrivilegesRights() {
+		return $this->hasMany(RelPrivilegesRights::class, ['privilege' => 'id']);
 	}
 
-	public function setRelUsers($relGroupsUsers):void {
+	/**
+	 * @return string[]
+	 */
+	public function getUserRightsNames():array {
+		return ArrayHelper::getColumn($this->relPrivilegesRights, 'right');
 	}
+
+	/**
+	 * @param string[] $userRightsNames
+	 */
+	public function setUserRightsNames(array $userRightsNames):void {
+		if ($this->isNewRecord) return;//Обработчик сохранения перевызовет метод после сохранения основной модели
+		foreach ($userRightsNames as $className) {
+			$relRight = new RelPrivilegesRights([
+				'privilege' => $this->id,
+				'right' => $className
+			]);
+			$relRight->save();
+		}
+	}
+
+	/**
+	 * @return UserRightInterface[]
+	 * @throws ReflectionException
+	 * @throws UnknownClassException
+	 */
+	public function getUserRights():array {
+		$classNames = $this->userRightsNames;
+		$result = [];
+		foreach ($classNames as $className) {
+			if (null !== $class = Magic::LoadClassByName($className, UserRightInterface::class)) $result[] = $class;
+		}
+		return $result;
+	}
+
 }
