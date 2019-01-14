98002a2c8660aaf4d583ade7f00b3e0a61d4d5ed
Pozitronik <antikillerxxl@gmail.com>
2019-01-14 18:15:08 +0300

    Лишние блоки

--- views/admin/users/_form.php
+++ views/admin/users/_form.php
@@ -22,113 +22,109 @@ use kartik\file\FileInput;
 
 ?>
 
-<div class="row">
-	<div class="col-xs-12">
-		<?php $form = ActiveForm::begin(); ?>
-		<div class="panel panel-primary">
-			<div class="panel-heading">
-				<div class="panel-control">
-					<?php if (!$model->isNewRecord): ?>
-						<?= Html::a('Новый пользователь', 'create', ['class' => 'btn btn-success']); ?>
-					<?php endif; ?>
-				</div>
-				<h3 class="panel-title"><?= Html::encode($this->title); ?></h3>
+<?php $form = ActiveForm::begin(); ?>
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<div class="panel-control">
+				<?php if (!$model->isNewRecord): ?>
+					<?= Html::a('Новый пользователь', 'create', ['class' => 'btn btn-success']); ?>
+				<?php endif; ?>
 			</div>
+			<h3 class="panel-title"><?= Html::encode($this->title); ?></h3>
+		</div>
 
-			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-3">
-						<?= $form->field($model, 'upload_image')->widget(FileInput::class, [
-							'options' => [
-								'accept' => 'image/*',
-								'multiple' => false
-							],
-							'pluginOptions' => [
-								'initialPreview' => !empty($model->profile_image)?[
-									$model->avatar
-								]:false,
-								'initialPreviewAsData' => true,
-								'browseClass' => 'btn btn-primary pull-right',
-								'browseIcon' => '<i class="glyphicon glyphicon-camera"></i> ',
-								'browseLabel' => 'Выберите изображение',
-								'showCaption' => false
-							]
-						]) ?>
-					</div>
-					<div class="col-md-9">
-						<div class="row">
-							<div class="col-md-12">
-								<?= $form->field($model, 'username')->textInput(['maxlength' => 50]); ?>
-							</div>
-							<div class="col-md-6">
-								<?= $form->field($model, 'login')->textInput(['maxlength' => 50]); ?>
-							</div>
-							<div class="col-md-6">
-								<?php if ($model->isNewRecord): ?>
-									<?= $form->field($model, 'password')->textInput(['maxlength' => 50])->hint('При входе пользователю будет предложено сменить пароль.'); ?>
-								<?php else: ?>
-									<?= $form->field($model, 'update_password')->textInput(['maxlength' => 50, 'value' => false])->hint('Пароль пользователя будет сброшен на введённый.'); ?>
-								<?php endif; ?>
-							</div>
-							<div class="col-md-12">
-								<?= $form->field($model, 'email')->textInput(['maxlength' => 50]); ?>
-							</div>
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-md-3">
+					<?= $form->field($model, 'upload_image')->widget(FileInput::class, [
+						'options' => [
+							'accept' => 'image/*',
+							'multiple' => false
+						],
+						'pluginOptions' => [
+							'initialPreview' => !empty($model->profile_image)?[
+								$model->avatar
+							]:false,
+							'initialPreviewAsData' => true,
+							'browseClass' => 'btn btn-primary pull-right',
+							'browseIcon' => '<i class="glyphicon glyphicon-camera"></i> ',
+							'browseLabel' => 'Выберите изображение',
+							'showCaption' => false
+						]
+					]) ?>
+				</div>
+				<div class="col-md-9">
+					<div class="row">
+						<div class="col-md-12">
+							<?= $form->field($model, 'username')->textInput(['maxlength' => 50]); ?>
+						</div>
+						<div class="col-md-6">
+							<?= $form->field($model, 'login')->textInput(['maxlength' => 50]); ?>
+						</div>
+						<div class="col-md-6">
+							<?php if ($model->isNewRecord): ?>
+								<?= $form->field($model, 'password')->textInput(['maxlength' => 50])->hint('При входе пользователю будет предложено сменить пароль.'); ?>
+							<?php else: ?>
+								<?= $form->field($model, 'update_password')->textInput(['maxlength' => 50, 'value' => false])->hint('Пароль пользователя будет сброшен на введённый.'); ?>
+							<?php endif; ?>
+						</div>
+						<div class="col-md-12">
+							<?= $form->field($model, 'email')->textInput(['maxlength' => 50]); ?>
+						</div>
 
-							<div class="col-md-12">
-								<?= $form->field($model, 'comment')->label('Комментарий пользователя'); ?>
-							</div>
+						<div class="col-md-12">
+							<?= $form->field($model, 'comment')->label('Комментарий пользователя'); ?>
+						</div>
 
-							<div class="col-md-6">
-								<?= $form->field($model, 'position')->widget(Select2::class, [
-									'data' => RefUserPositions::mapData(),
-									'options' => ['placeholder' => 'Выберите роль'],
-									'pluginOptions' => [
-										'allowClear' => true
-									]
-								]); ?>
-							</div>
-							<div class="col-md-6">
-								<?= $form->field($model, 'relPrivileges')->widget(Select2::class, [
-									'data' => ArrayHelper::map(Privileges::find()->active()->all(),'id','name'),
-									'options' => ['placeholder' => 'Выберите привилегии пользователя'],
-									'pluginOptions' => [
-										'multiple' => true,
-										'allowClear' => true
-									]
-								]); ?>
-							</div>
+						<div class="col-md-6">
+							<?= $form->field($model, 'position')->widget(Select2::class, [
+								'data' => RefUserPositions::mapData(),
+								'options' => ['placeholder' => 'Выберите роль'],
+								'pluginOptions' => [
+									'allowClear' => true
+								]
+							]); ?>
+						</div>
+						<div class="col-md-6">
+							<?= $form->field($model, 'relPrivileges')->widget(Select2::class, [
+								'data' => ArrayHelper::map(Privileges::find()->active()->all(), 'id', 'name'),
+								'options' => ['placeholder' => 'Выберите привилегии пользователя'],
+								'pluginOptions' => [
+									'multiple' => true,
+									'allowClear' => true
+								]
+							]); ?>
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<?= $this->render('groups/index', [
-							'model' => $model,
-							'provider' => new ActiveDataProvider(['query' => $model->getRelGroups()->orderBy('name')->active()])
-						]); ?>
-					</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<?= $this->render('groups/index', [
+						'model' => $model,
+						'provider' => new ActiveDataProvider(['query' => $model->getRelGroups()->orderBy('name')->active()])
+					]); ?>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<?= $this->render('attributes/index', [
-							'user' => $model,
-							'data' => $attributesData,
-							'provider' => new ActiveDataProvider(['query' => $model->getRelDynamicAttributes()->orderBy('name')->active()])
-						]); ?>
-					</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<?= $this->render('attributes/index', [
+						'user' => $model,
+						'data' => $attributesData,
+						'provider' => new ActiveDataProvider(['query' => $model->getRelDynamicAttributes()->orderBy('name')->active()])
+					]); ?>
 				</div>
-
 			</div>
 
-			<div class="panel-footer">
-				<div class="btn-group">
-					<?= Html::submitButton($model->isNewRecord?'Сохранить':'Изменить', ['class' => $model->isNewRecord?'btn btn-success':'btn btn-primary']); ?>
-					<?php if ($model->isNewRecord): ?>
-						<?= Html::input('submit', 'more', 'Сохранить и добавить ещё', ['class' => 'btn btn-primary']); ?>
-					<?php endif ?>
-				</div>
+		</div>
+
+		<div class="panel-footer">
+			<div class="btn-group">
+				<?= Html::submitButton($model->isNewRecord?'Сохранить':'Изменить', ['class' => $model->isNewRecord?'btn btn-success':'btn btn-primary']); ?>
+				<?php if ($model->isNewRecord): ?>
+					<?= Html::input('submit', 'more', 'Сохранить и добавить ещё', ['class' => 'btn btn-primary']); ?>
+				<?php endif ?>
 			</div>
 		</div>
-		<?php ActiveForm::end(); ?>
 	</div>
-</div>
+<?php ActiveForm::end(); ?>
