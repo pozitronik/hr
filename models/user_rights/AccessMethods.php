b0bbc5b6da4bbd632b28932dc7cfbcb4b426d920
Pozitronik <antikillerxxl@gmail.com>
2019-01-14 19:10:09 +0300

    declare public

--- models/user_rights/AccessMethods.php
+++ models/user_rights/AccessMethods.php
@@ -11,11 +11,11 @@ namespace app\models\user_rights;
 abstract class AccessMethods {
 	public const __default = self::any;
 
-	const any = null;
-	const view = 0;
-	const create = 1;
-	const update = 2;
-	const delete = 3;
+	public const any = null;
+	public const view = 0;
+	public const create = 1;
+	public const update = 2;
+	public const delete = 3;
 	/*et cetera*/
 //	const take_ownership = 300;
 //	const prune = 100;
