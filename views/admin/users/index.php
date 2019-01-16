1d692086cd9a9b498c2b6727da6bfe6a2421542e
Pozitronik <antikillerxxl@gmail.com>
2019-01-16 13:30:39 +0300

    Очевидно лишнее форматирование, унаследованное от лайткаба

--- views/admin/users/index.php
+++ views/admin/users/index.php
@@ -26,139 +26,135 @@ $this->params['breadcrumbs'][] = ['label' => 'Управление', 'url' => ['
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="row">
-	<div class="col-xs-12">
-		<?= /** @noinspection MissedFieldInspection */
-		GridView::widget([
-			'dataProvider' => $dataProvider,
-			'filterModel' => $searchModel,
-			'panel' => [
-				'heading' => $this->title.(($dataProvider->totalCount > 0)?" (".Utils::pluralForm($dataProvider->totalCount, ['пользователь', 'пользователя', 'пользователей']).")":" (нет пользователей)")
+<?= /** @noinspection MissedFieldInspection */
+GridView::widget([
+	'dataProvider' => $dataProvider,
+	'filterModel' => $searchModel,
+	'panel' => [
+		'heading' => $this->title.(($dataProvider->totalCount > 0)?" (".Utils::pluralForm($dataProvider->totalCount, ['пользователь', 'пользователя', 'пользователей']).")":" (нет пользователей)")
+	],
+	'summary' => Html::a('Новый пользователь', 'create', ['class' => 'btn btn-success summary-content']),
+	'toolbar' => false,
+	'export' => false,
+	'resizableColumns' => true,
+	'responsive' => true,
+	'columns' => [
+		[
+			'class' => ActionColumn::class,
+			'header' => Icons::menu(),
+			'dropdown' => true,
+			'dropdownButton' => [
+				'label' => Icons::menu(),
+				'caret' => ''
 			],
-			'summary' => Html::a('Новый пользователь', 'create', ['class' => 'btn btn-success summary-content']),
-			'toolbar' => false,
-			'export' => false,
-			'resizableColumns' => true,
-			'responsive' => true,
-			'columns' => [
-				[
-					'class' => ActionColumn::class,
-					'header' => Icons::menu(),
-					'dropdown' => true,
-					'dropdownButton' => [
-						'label' => Icons::menu(),
-						'caret' => ''
-					],
-					'template' => '{update} {delete}',
-					'buttons' => [
-						'update' => function($url, $model) {
-							/** @var UsersSearch $model */
-							return Html::tag('li', Html::a(Icons::update().'Изменение', ['update', 'id' => $model->id]));
-						},
-						'delete' => function($url, $model) {
-							/** @var UsersSearch $model */
-							return Html::tag('li', Html::a(Icons::delete().'Удаление', ['delete', 'id' => $model->id], [
-								'title' => 'Удалить запись',
-								'data' => [
-									'confirm' => $model->deleted?'Вы действительно хотите восстановить запись?':'Вы действительно хотите удалить запись?',
-									'method' => 'post'
-								]
-							]));
-						}
-					]
-				],
-				[
-					'attribute' => 'id',
-					'options' => [
-						'style' => 'width:36px'
-					]
-				],
-				[
-					'value' => function($model) {
-						/** @var UsersSearch $model */
-						return Html::img($model->avatar, ['class' => 'img-circle img-xs']);
-					},
-					'label' => 'Аватар',
-					'format' => 'raw',
-					'contentOptions' => ['class' => 'text-center'],
-					'options' => [
-						'style' => 'width: 40px;'
-					]
-				],
-				[
-					'attribute' => 'username',
-					'value' => function($model) {
-						/** @var UsersSearch $model */
-						return Html::a($model->username, ['admin/users/update', 'id' => $model->id]);
-					},
-					'format' => 'raw'
-				],
-				[
-					'attribute' => 'groupName',
-					'label' => 'Группы',
-					'value' => function($model) {
-						/** @var UsersSearch $model */
-						return BadgeWidget::widget([
-							'data' => $model->relGroups,
-							'useBadges' => false,
-							'attribute' => 'name',
-							'linkScheme' => ['admin/groups/update', 'id' => 'id']
-						]);
-					},
-					'format' => 'raw'
-				],
-				[
-					'attribute' => 'roles',
-					'filterType' => GridView::FILTER_SELECT2,
-					'filter' => RefUserRoles::mapData(),
-					'filterInputOptions' => ['placeholder' => 'Выберите роль'],
-					'filterWidgetOptions' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
-
-					'label' => 'Роли',
-					'value' => function($model) {
-						/** @var UsersSearch $model */
-						$options = ArrayHelper::map(RefUserRoles::find()->active()->all(), 'id', 'color');
-						array_walk($options, function(&$value, $key) {
-							if (!empty($value)) {
-								$value = [
-									'style' => "background: $value;"
-								];
-							}
-						});
-						return BadgeWidget::widget([
-							'data' => $model->getRelRefUserRoles()->all(),//здесь нельзя использовать свойство, т.к. фреймворк не подгружает все релейшены в $_related сразу. Выяснено экспериментально, на более подробные разбирательства нет времени
-							'useBadges' => true,
-							'attribute' => 'name',
-							'unbadgedCount' => 6,
-							"itemsSeparator" => false,
-							"optionsMap" => $options
-						]);
-					},
-					'format' => 'raw'
-				],
-				[
-					'attribute' => 'privileges',
-					'filterType' => GridView::FILTER_SELECT2,
-					'filter' => ArrayHelper::map(Privileges::find()->active()->all(), 'id', 'name'),
-					'filterInputOptions' => ['placeholder' => 'Выберите привилегии'],
-					'filterWidgetOptions' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
-
-					'label' => 'Привилегии',
-					'value' => function($model) {
-						/** @var UsersSearch $model */
-						return BadgeWidget::widget([
-							'data' => $model->getRelPrivileges()->all(),//здесь нельзя использовать свойство, т.к. фреймворк не подгружает все релейшены в $_related сразу. Выяснено экспериментально, на более подробные разбирательства нет времени
-							'useBadges' => true,
-							'attribute' => 'name',
-							'unbadgedCount' => 6,
-							"itemsSeparator" => false
-						]);
-					},
-					'format' => 'raw'
-				],
-
-				'email:email'
+			'template' => '{update} {delete}',
+			'buttons' => [
+				'update' => function($url, $model) {
+					/** @var UsersSearch $model */
+					return Html::tag('li', Html::a(Icons::update().'Изменение', ['update', 'id' => $model->id]));
+				},
+				'delete' => function($url, $model) {
+					/** @var UsersSearch $model */
+					return Html::tag('li', Html::a(Icons::delete().'Удаление', ['delete', 'id' => $model->id], [
+						'title' => 'Удалить запись',
+						'data' => [
+							'confirm' => $model->deleted?'Вы действительно хотите восстановить запись?':'Вы действительно хотите удалить запись?',
+							'method' => 'post'
+						]
+					]));
+				}
 			]
-		]); ?>
-	</div>
-</div>
+		],
+		[
+			'attribute' => 'id',
+			'options' => [
+				'style' => 'width:36px'
+			]
+		],
+		[
+			'value' => function($model) {
+				/** @var UsersSearch $model */
+				return Html::img($model->avatar, ['class' => 'img-circle img-xs']);
+			},
+			'label' => 'Аватар',
+			'format' => 'raw',
+			'contentOptions' => ['class' => 'text-center'],
+			'options' => [
+				'style' => 'width: 40px;'
+			]
+		],
+		[
+			'attribute' => 'username',
+			'value' => function($model) {
+				/** @var UsersSearch $model */
+				return Html::a($model->username, ['admin/users/update', 'id' => $model->id]);
+			},
+			'format' => 'raw'
+		],
+		[
+			'attribute' => 'groupName',
+			'label' => 'Группы',
+			'value' => function($model) {
+				/** @var UsersSearch $model */
+				return BadgeWidget::widget([
+					'data' => $model->relGroups,
+					'useBadges' => false,
+					'attribute' => 'name',
+					'linkScheme' => ['admin/groups/update', 'id' => 'id']
+				]);
+			},
+			'format' => 'raw'
+		],
+		[
+			'attribute' => 'roles',
+			'filterType' => GridView::FILTER_SELECT2,
+			'filter' => RefUserRoles::mapData(),
+			'filterInputOptions' => ['placeholder' => 'Выберите роль'],
+			'filterWidgetOptions' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
+
+			'label' => 'Роли',
+			'value' => function($model) {
+				/** @var UsersSearch $model */
+				$options = ArrayHelper::map(RefUserRoles::find()->active()->all(), 'id', 'color');
+				array_walk($options, function(&$value, $key) {
+					if (!empty($value)) {
+						$value = [
+							'style' => "background: $value;"
+						];
+					}
+				});
+				return BadgeWidget::widget([
+					'data' => $model->getRelRefUserRoles()->all(),//здесь нельзя использовать свойство, т.к. фреймворк не подгружает все релейшены в $_related сразу. Выяснено экспериментально, на более подробные разбирательства нет времени
+					'useBadges' => true,
+					'attribute' => 'name',
+					'unbadgedCount' => 6,
+					"itemsSeparator" => false,
+					"optionsMap" => $options
+				]);
+			},
+			'format' => 'raw'
+		],
+		[
+			'attribute' => 'privileges',
+			'filterType' => GridView::FILTER_SELECT2,
+			'filter' => ArrayHelper::map(Privileges::find()->active()->all(), 'id', 'name'),
+			'filterInputOptions' => ['placeholder' => 'Выберите привилегии'],
+			'filterWidgetOptions' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
+
+			'label' => 'Привилегии',
+			'value' => function($model) {
+				/** @var UsersSearch $model */
+				return BadgeWidget::widget([
+					'data' => $model->getRelPrivileges()->all(),//здесь нельзя использовать свойство, т.к. фреймворк не подгружает все релейшены в $_related сразу. Выяснено экспериментально, на более подробные разбирательства нет времени
+					'useBadges' => true,
+					'attribute' => 'name',
+					'unbadgedCount' => 6,
+					"itemsSeparator" => false
+				]);
+			},
+			'format' => 'raw'
+		],
+
+		'email:email'
+	]
+]); ?>
