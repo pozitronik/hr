1292e8d534b9a2f452bcbccd1fd93b26e0c8a724
Pozitronik <antikillerxxl@gmail.com>
2019-01-17 17:37:53 +0300

    Боллее удобное разбитие по шагам

--- views/admin/import/decompose.php
+++ views/admin/import/decompose.php
@@ -12,7 +12,19 @@ use app\helpers\Utils;
 use yii\helpers\Html;
 use yii\web\View;
 
-Utils::log($messages);
 ?>
 
-<?= (3 !== $step)?Html::a('Следующий шаг', ['decompose', 'step' => $step+1, 'domain' => $domain], ['class' => 'btn btn-default']):Html::a('К результату', ['result', 'domain' => $domain], ['class' => 'btn btn-success']); ?>
+<?php if ([] === $messages): ?>
+	<?= Html::label('Нет ошибок'); ?>
+	<br/>
+	<?= Html::a('Повторить', ['decompose', 'step' => $step, 'domain' => $domain], ['class' => 'btn btn-warning pull-right']); ?>
+	<?= (3 !== $step)?Html::a('Следующий шаг', ['decompose', 'step' => $step + 1, 'domain' => $domain], ['class' => 'btn btn-default']):Html::a('К результату', ['result', 'domain' => $domain], ['class' => 'btn btn-success']); ?>
+<?php else: ?>
+	<?= Html::label('Ошибки:'); ?>
+	<br/>
+	<?php Utils::log($messages); ?>
+	<?= Html::a('Повторить', ['decompose', 'step' => $step, 'domain' => $domain], ['class' => 'btn btn-warning pull-right']); ?>
+	<?= Html::a('К началу', 'import', ['class' => 'btn btn-default pull-left']); ?>
+<?php endif; ?>
+
+
