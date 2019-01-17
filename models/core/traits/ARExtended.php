ce72648712017ab9dc1979495629f938510b1396
Pozitronik <antikillerxxl@gmail.com>
2019-01-17 13:34:07 +0300

    fixed getInstance code

--- models/core/traits/ARExtended.php
+++ models/core/traits/ARExtended.php
@@ -53,13 +53,13 @@ trait ARExtended {
 	/**
 	 * Возвращает существующую запись в ActiveRecord-модели, найденную по условию, если же такой записи нет - возвращает новую модель
 	 * @param array|string $searchCondition
-	 * @return array|null|ActiveRecord|self
+	 * @return ActiveRecord|self
 	 * @todo: не работает с наследуемыми моделями, вроде референсов, разобраться.
 	 */
-	public static function getInstance($searchCondition) {
+	public static function getInstance($searchCondition):self {
 		/** @noinspection PhpUndefinedMethodInspection */
 		$instance = self::find()->where($searchCondition)->one();
-		return $instance?:new self;
+		return $instance??new self;
 	}
 
 	/**
