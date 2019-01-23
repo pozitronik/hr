173c5dc657ac1a43a22120f48bf465e62a8a49bc
Pozitronik <antikillerxxl@gmail.com>
2019-01-23 15:54:58 +0300

    Переименование колонок

--- migrations/m190123_123508_fos_import_rename_column.php
+++ migrations/m190123_123508_fos_import_rename_column.php
@@ -11,6 +11,12 @@ class m190123_123508_fos_import_rename_column extends Migration {
 	 */
 	public function safeUp() {
 		$this->renameColumn('import_fos', 'user_id', 'user_tn');
+
+		$this->renameColumn('import_fos', 'tribe_leader_id', 'tribe_leader_tn');
+		$this->renameColumn('import_fos', 'tribe_leader_it_id', 'tribe_leader_it_tn');
+		$this->renameColumn('import_fos', 'cluster_product_leader_id', 'cluster_product_leader_tn');
+		$this->renameColumn('import_fos', 'chapter_leader_id', 'chapter_leader_tn');
+		$this->renameColumn('import_fos', 'chapter_couch_id', 'chapter_couch_tn');
 	}
 
 	/**
@@ -18,6 +24,12 @@ class m190123_123508_fos_import_rename_column extends Migration {
 	 */
 	public function safeDown() {
 		$this->renameColumn('import_fos', 'user_tn', 'user_id');
+
+		$this->renameColumn('import_fos', 'tribe_leader_tn', 'tribe_leader_id');
+		$this->renameColumn('import_fos', 'tribe_leader_it_tn', 'tribe_leader_it_id');
+		$this->renameColumn('import_fos', 'cluster_product_leader_tn', 'cluster_product_leader_id');
+		$this->renameColumn('import_fos', 'chapter_leader_tn', 'chapter_leader_id');
+		$this->renameColumn('import_fos', 'chapter_couch_tn', 'chapter_couch_id');
 	}
 
 	/*
