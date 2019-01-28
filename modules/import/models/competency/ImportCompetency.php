c088d694cccabfc2d108f68100564dc58353a528
Pozitronik <antikillerxxl@gmail.com>
2019-01-28 13:32:27 +0300

    Исправление получаемого куска оценок

--- modules/import/models/competency/ImportCompetency.php
+++ modules/import/models/competency/ImportCompetency.php
@@ -82,7 +82,7 @@ class ImportCompetency extends Model {
 					$currentCompetencyFieldName = $newCompetencyFieldName;
 				}
 				foreach ($userIdIndexes as $usersCount => $usersCountValue) {
-					$userScoreSliceBlock = array_slice($importRow, 2 + ($usersCount * 6), 6 + ($usersCount * 6));//вырезаем кусок оценок
+					$userScoreSliceBlock = array_slice($importRow, 2 + ($usersCount * 6), 6);//вырезаем кусок оценок
 					$this->addScores($userIdIndexes[$usersCount], $currentCompetencyName, $currentCompetencyFieldName, $userScoreNamesIndexes, $userScoreSliceBlock);
 				}
 
