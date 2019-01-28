cfb3a19148b22a737cb2bad5f914f7a9f7407395
Pozitronik <antikillerxxl@gmail.com>
2019-01-28 11:06:14 +0300

    done view stub

---
+++ modules/import/views/competency/done.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var ActiveRecord $searchModel
+ * @var ActiveDataProvider $dataProvider
+ * @var int $domain
+ */
+
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveRecord;
+use yii\web\View;
+
+?>
+
+Done!
