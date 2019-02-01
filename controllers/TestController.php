21e407527e14e9f5946ba12fb16223ee86918c86
Pozitronik <antikillerxxl@gmail.com>
2019-02-01 12:21:16 +0300

    Тестовый код (разбираюсь с поведением наследования классов в бутстрапе)

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -1,10 +1,11 @@
 <?php
-/** @noinspection ALL */
 declare(strict_types = 1);
 
 namespace app\controllers;
 
+use app\helpers\ArrayHelper;
 use app\helpers\Utils;
+use app\models\dynamic_attributes\DynamicAttributes;
 use app\models\groups\Groups;
 use app\models\references\refs\RefGroupTypes;
 use app\models\references\refs\RefUserRoles;
@@ -15,6 +16,7 @@ use app\widgets\alert\Alert;
 use app\widgets\alert\AlertModel;
 use kartik\growl\Growl;
 use yii\base\Response;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 
 /**
@@ -27,16 +29,22 @@ class TestController extends Controller {
 	 * @return string|Response
 	 */
 	public function actionIndex() {
-		$this->layout = 'empty';
 		return $this->render('index');
 
 	}
 
-	public function actionFlash() {
-//		AlertPrototype::SuccessNotify();
-		AlertModel::SuccessNotify();
-		return $this->render('flash');
+	/**
+	 * @return string|null
+	 * @throws \Throwable
+	 */
+	public function actionUser() {
+		if (null === $user = Users::findModel(1)) return null;
 
+
+
+		return $this->render('/admin/users/attributes/test_widgets', [
+			'user' => $user,
+		]);
 	}
 
 }
--- views/test/index.php
+++ views/test/index.php
@@ -1,9 +1,67 @@
 <?php
 declare(strict_types = 1);
 
-use yii\web\View;
-
-\app\assets\FontAwesomeProAsset::register($this);
 /* @var View $this */
 
-echo 'icon: '."<i class='far fa-user' title='Пользователи'></i>";
+use yii\web\View;
+
+?>
+
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">Заголовок</h3>
+	</div>
+
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title">Заголовок</h3>
+					</div>
+
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-6">
+								раз
+							</div>
+							<div class="col-md-6">
+								два
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h3 class="panel-title">Заголовок</h3>
+					</div>
+
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-6">
+								раз
+							</div>
+							<div class="col-md-6">
+								<div class="panel panel-mint panel-score">
+									<div class="panel-heading">
+										<div class="panel-title">пук</div>
+									</div>
+
+									<div class="panel-body">
+										<div class="row">
+											<div class="col-md-12">
+												жепка
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
