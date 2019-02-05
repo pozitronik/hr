90f8f0d73ba03a13975268b70823f44fa51f787f
Pozitronik <antikillerxxl@gmail.com>
2019-02-05 16:27:08 +0300

    Исправление ссылки

--- widgets/navbar/views/dropdown.php
+++ widgets/navbar/views/dropdown.php
@@ -32,7 +32,7 @@ use app\models\users\Users;
 				<ul class="list-unstyled">
 					<li class="dropdown-header">Навигация</li>
 					<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no">Группы</p><small class="text-muted">Рабочие группы в сфере моей ответственности</small></div>', ["/home/index"]); ?></li>
-					<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no">Атрибуты</p><small class="text-muted">Поиск сотрудников по атрибутам</small></div>', ["/admin/attributes/search"]); ?></li>
+					<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no">Атрибуты</p><small class="text-muted">Поиск сотрудников по атрибутам</small></div>', ["/attributes/attributes/search"]); ?></li>
 					<?php if (UserAccess::GetFlag(ServiceAccess::FLAG_SERVICE)): ?>
 						<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no"><i class="fa fa-radiation-alt"></i> Ядерный пепел <i class="fa fa-radiation-alt"></i></p><small class="text-muted">Сброс сервиса в исходное состояние</small></div>', ["/service/service/index"]); ?></li>
 					<?php endif; ?>
