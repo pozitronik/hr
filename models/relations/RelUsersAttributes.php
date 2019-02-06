8ba08843c59963fbebf563a1cfd8bf107ef0b5ca
Pozitronik <antikillerxxl@gmail.com>
2019-02-06 12:09:39 +0300

    +релейшены меж таблицами

--- models/relations/RelUsersAttributes.php
+++ models/relations/RelUsersAttributes.php
@@ -3,6 +3,9 @@ declare(strict_types = 1);
 
 namespace app\models\relations;
 
+use app\models\core\traits\ARExtended;
+use app\models\references\refs\RefAttributesTypes;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 
 /**
@@ -11,9 +14,13 @@ use yii\db\ActiveRecord;
  * @property int $id
  * @property int $user_id
  * @property int $attribute_id
+ * @property ActiveQuery|RelUsersAttributesTypes[] $relUsersAttributesTypes соответствия в таблице типов аттрибута, ассоциированные к этой связке
+ * @property ActiveQuery|RefAttributesTypes[] $refAttributesTypes Значения из справочника аттрибутов, ассоциированные с этой связкой
  */
 class RelUsersAttributes extends ActiveRecord {
 	use Relations;
+	use ARExtended;
+
 	/**
 	 * {@inheritdoc}
 	 */
@@ -42,4 +49,19 @@ class RelUsersAttributes extends ActiveRecord {
 			'attribute_id' => 'Attribute ID'
 		];
 	}
+
+	/**
+	 * @return RelUsersAttributesTypes[]|ActiveQuery
+	 */
+	public function getRelUsersAttributesTypes() {
+		return $this->hasMany(RelUsersAttributesTypes::class, ['user_attribute_id' => 'id']);
+	}
+
+	/**
+	 * @return RefAttributesTypes[]|ActiveQuery
+	 */
+	public function getRefAttributesTypes() {
+		return $this->hasMany(RefAttributesTypes::class, ['id' => 'type'])->via('relUsersAttributesTypes');
+	}
+
 }
