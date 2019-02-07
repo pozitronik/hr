245a13d671774fe79ea7962bd8b62976b62e9ed4
Pozitronik <antikillerxxl@gmail.com>
2019-02-07 08:12:28 +0300

    Схоронил текущую вьюху атрибутов отдельно (базовую переделываю)

---
+++ modules/dynamic_attributes/views/user/panel.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var Users $user
+ */
+
+use app\helpers\ArrayHelper;
+use app\models\relations\RelUsersAttributes;
+use app\modules\dynamic_attributes\models\DynamicAttributes;
+use app\models\users\Users;
+use app\modules\dynamic_attributes\widgets\user_attribute\UserAttributeWidget;
+use kartik\select2\Select2;
+use yii\web\View;
+
+?>
+
+<div class="panel panel-attribute">
+	<div class="kv-panel-before">
+		<?= Select2::widget([
+			'model' => $user,
+			'attribute' => 'relDynamicAttributes',
+			'name' => 'attribute_id',
+			'data' => ArrayHelper::map($user->isNewRecord?DynamicAttributes::find()->active()->all():DynamicAttributes::find()->active()->where(['not in', 'id', ArrayHelper::getColumn($user->relDynamicAttributes, 'id')])->all(), 'id', 'name'),
+			'options' => [
+				'multiple' => true,
+				'placeholder' => 'Добавить атрибут'
+			]
+		]) ?>
+	</div>
+
+	<?php foreach (RelUsersAttributes::getUserAttributes($user->id) as $model): ?>
+		<?= UserAttributeWidget::widget([
+			'user_id' => $user->id,
+			'attribute_id' => $model->attribute_id
+		]); ?>
+
+	<?php endforeach; ?>
+</div>
+
+
+
