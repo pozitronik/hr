cd7bc79def73938e6937d793d2776695b6542bf5
Pozitronik <antikillerxxl@gmail.com>
2019-02-11 14:51:43 +0300

    Интерфейс виджета-выбиралки

---
+++ models/core/SelectionWidget.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\core;
+
+/**
+ * Интерфейс виджетов-выбиралок (несущественный, просто вынос констант. Вохможно, будет дополнен методами геттерами-сеттерами атрибутов
+ * Interface SelectionWidget
+ * @package app\models\core
+ */
+interface SelectionWidget {
+	public const MODE_FIELD = 0;
+	public const MODE_FORM = 1;
+	public const MODE_AJAX = 2;
+}
