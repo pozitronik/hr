d38f6d1c927dc48b66035999f9013c0a6962ae64
Pozitronik <antikillerxxl@gmail.com>
2019-02-11 14:01:15 +0300

    Не показываем undefined при неопределённом типе группы

--- widgets/group_select/assets/js/group_select.js
+++ widgets/group_select/assets/js/group_select.js
@@ -8,5 +8,6 @@ function formatGroup(item) {
 	}
 	var logo = $(item.element).data('logo');
 	var typename = $(item.element).data('typename');
+	if (!typename) typename = '';
 	return '<div class="select-item"><div class="row"><div class="col-sm-8"><img src="' + logo + '" class="group-logo" alt="logo">' + item.text + '</div><div class="col-sm-4 text-overflow" ' + typecolor + '>' + typename + '</div></div></div>';
 }
