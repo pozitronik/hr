25ccba3a86ca6fac2c915f9a68525cfe89639856
Pozitronik <antikillerxxl@gmail.com>
2019-02-12 10:59:59 +0300

    Вьюха пользователей группы + фиксы сабвьюхи

--- modules/groups/views/groups/users/index.php
+++ modules/groups/views/groups/users/index.php
@@ -27,79 +27,75 @@ $provider = new ActiveDataProvider([
 	'query' => $model->getRelUsers()->active()
 ])
 ?>
-<div class="row">
-	<div class="col-xs-12">
-		<?= GridView::widget([
-			'dataProvider' => $provider,
-			'panel' => [
-				'type' => GridView::TYPE_DEFAULT,
-				'after' => false,
-				'heading' => $heading.(($provider->totalCount > 0)?" (".Utils::pluralForm($provider->totalCount, ['пользователь', 'пользователя', 'пользователей']).")":" (нет пользователей)"),
-				'footer' => $provider->totalCount > $provider->pagination->pageSize?null:false,
-				'before' => $selectorInPanel?UserSelectWidget::widget([
-					'model' => $model,
-					'attribute' => 'relUsers',
-					'notData' => $model->relUsers,
-					'multiple' => true
-				]):false
+<?= GridView::widget([
+	'dataProvider' => $provider,
+	'panel' => [
+		'type' => GridView::TYPE_INFO,
+		'after' => false,
+		'heading' => $heading.(($provider->totalCount > 0)?" (".Utils::pluralForm($provider->totalCount, ['пользователь', 'пользователя', 'пользователей']).")":" (нет пользователей)"),
+		'footer' => $provider->totalCount > $provider->pagination->pageSize?null:false,
+		'before' => $selectorInPanel?UserSelectWidget::widget([
+			'model' => $model,
+			'attribute' => 'relUsers',
+			'notData' => $model->relUsers,
+			'multiple' => true
+		]):false
+	],
+	'toolbar' => false,
+	'summary' => ButtonDropdown::widget([
+		'label' => Icons::menu(),
+		'encodeLabel' => false,
+		'options' => [
+			'class' => 'summary-content'
+		],
+		'dropdown' => [
+			'options' => [
+				'class' => 'pull-right'
 			],
-			'toolbar' => false,
-			'summary' => ButtonDropdown::widget([
-				'label' => Icons::menu(),
-				'encodeLabel' => false,
-				'options' => [
-					'class' => 'summary-content'
-				],
-				'dropdown' => [
-					'options' => [
-						'class' => 'pull-right'
-					],
-					'encodeLabels' => false,
-					'items' => [
-						[
-							'label' => Icons::hierarchy().'Иерархия',
-							'url' => ['/groups/groups/users-hierarchy', 'id' => $model->id]
-						],
-						[
-							'label' => Icons::hierarchy_red().'Иерархия (с ролями)',
-							'url' => ['/groups/groups/users-hierarchy', 'id' => $model->id, 'showRolesSelector' => true]
-						]
-					]
-				]
-			]),
-
-			'export' => false,
-			'resizableColumns' => true,
-			'responsive' => true,
-			'columns' => [
+			'encodeLabels' => false,
+			'items' => [
 				[
-					'format' => 'raw',
-					'attribute' => 'username',
-					'value' => function($user) {
-						/** @var Users $user */
-						return Html::a($user->username, ['/users/users/profile', 'id' => $user->id]);
-					}
+					'label' => Icons::hierarchy().'Иерархия',
+					'url' => ['/groups/groups/users-hierarchy', 'id' => $model->id]
 				],
 				[
-					'label' => 'Роли в группе',
-					'value' => function($user) use ($model) {
-						/** @var Groups $model */
-						return RolesSelectWidget::widget([
-							'groupId' => $model->id,
-							'userId' => $user->id
-						]);
-					},
-					'format' => 'raw',
-					'visible' => $showRolesSelector
-				],
-				[
-					'class' => CheckboxColumn::class,
-					'headerOptions' => ['class' => 'kartik-sheet-style'],
-					'header' => Icons::trash(),
-					'name' => $model->formName().'[dropUsers]',
-					'visible' => $showDropColumn
+					'label' => Icons::hierarchy_red().'Иерархия (с ролями)',
+					'url' => ['/groups/groups/users-hierarchy', 'id' => $model->id, 'showRolesSelector' => true]
 				]
 			]
-		]); ?>
-	</div>
-</div>
+		]
+	]),
+
+	'export' => false,
+	'resizableColumns' => true,
+	'responsive' => true,
+	'columns' => [
+		[
+			'format' => 'raw',
+			'attribute' => 'username',
+			'value' => function($user) {
+				/** @var Users $user */
+				return Html::a($user->username, ['/users/users/profile', 'id' => $user->id]);
+			}
+		],
+		[
+			'label' => 'Роли в группе',
+			'value' => function($user) use ($model) {
+				/** @var Groups $model */
+				return RolesSelectWidget::widget([
+					'groupId' => $model->id,
+					'userId' => $user->id
+				]);
+			},
+			'format' => 'raw',
+			'visible' => $showRolesSelector
+		],
+		[
+			'class' => CheckboxColumn::class,
+			'headerOptions' => ['class' => 'kartik-sheet-style'],
+			'header' => Icons::trash(),
+			'name' => $model->formName().'[dropUsers]',
+			'visible' => $showDropColumn
+		]
+	]
+]); ?>
