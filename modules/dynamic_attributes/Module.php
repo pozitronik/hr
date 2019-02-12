fb2ab3c2108ff781ca94334c4ce8302c58c81627
Pozitronik <antikillerxxl@gmail.com>
2019-02-12 16:33:06 +0300

    Замена родительской модели модулей на CoreModule

--- modules/dynamic_attributes/Module.php
+++ modules/dynamic_attributes/Module.php
@@ -3,16 +3,11 @@ declare(strict_types = 1);
 
 namespace app\modules\dynamic_attributes;
 
-use yii\base\Module as BaseModule;
+use app\models\core\core_module\CoreModule;
 
 /**
  * Class Module
  * @package app\modules\import
  */
-class Module extends BaseModule {
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public $controllerNamespace = 'app\modules\dynamic_attributes\controllers';
+class Module extends CoreModule {
 }
