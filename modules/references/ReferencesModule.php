42988f16e4f7822f9e5b0485dc31d95ab15ddcf8
Pozitronik <antikillerxxl@gmail.com>
2019-02-14 12:21:58 +0300

    Стаб под модуль справочников

---
+++ modules/references/ReferencesModule.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\references;
+
+use app\models\core\core_module\CoreModule;
+
+/** @noinspection EmptyClassInspection */
+
+/**
+ * Class ReferencesModule
+ * @package app\modules\references
+ */
+class ReferencesModule extends CoreModule {
+
+}
