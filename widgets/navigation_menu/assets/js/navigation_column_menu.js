fb645bdee06d84a844f6e1aa51a4e0ceae1de8f5
Pozitronik <antikillerxxl@gmail.com>
2019-02-21 14:11:03 +0300

    Позиционирование всплывающего меню учитывает прокрутку

--- widgets/navigation_menu/assets/js/navigation_column_menu.js
+++ widgets/navigation_menu/assets/js/navigation_column_menu.js
@@ -2,6 +2,8 @@ jQuery(function ($) {
 	$('table.table * .dropdown-menu').parent().on('shown.bs.dropdown', function () {
 		var menu = jQuery(this).find('.dropdown-menu');
 		var position = menu.offset();
+		position.top -= document.documentElement.scrollTop;
+		position.left -= document.documentElement.scrollLeft;
 		menu.css('position', 'fixed').css(position);
 	});
 	$('table.table * .dropdown-menu').parent().on('hidden.bs.dropdown', function () {
