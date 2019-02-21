15535934d819a007e74f91e0ee205bd85ba49403
Pozitronik <antikillerxxl@gmail.com>
2019-02-21 12:16:53 +0300

    Модель для справочника веток должностей

---
+++ modules/references/models/refs/RefUserPositionBranches.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\references\models\refs;
+
+use app\modules\references\models\Reference;
+
+/**
+ * Class RefUserPositionBranches
+ * @package app\modules\references\models\refs
+ */
+class RefUserPositionBranches extends Reference {
+	public $menuCaption = 'Ветки должностей';
+	public $menuIcon = false;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function tableName():string {
+		return 'ref_user_position_branches';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function rules():array {
+		return [
+			[['name'], 'required'],
+			[['name'], 'unique'],
+			[['id', 'deleted'], 'integer'],
+			[['name'], 'string', 'max' => 256]
+		];
+	}
+}
