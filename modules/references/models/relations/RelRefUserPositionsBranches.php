7039003e890a60ab564f40e83b653d0d742ed4aa
Pozitronik <antikillerxxl@gmail.com>
2019-02-21 12:22:50 +0300

    Модели для релейшенов между должностями и типами/ветвями

---
+++ modules/references/models/relations/RelRefUserPositionsBranches.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\references\models\relations;
+
+use app\models\core\traits\ARExtended;
+use app\models\relations\Relations;
+use yii\db\ActiveRecord;
+
+/**
+ * Class RelRefUserPositionsBranches
+ * @package app\modules\references\models\relations
+ */
+class RelRefUserPositionsBranches extends ActiveRecord {
+	use Relations;
+	use ARExtended;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function tableName():string {
+		return 'rel_ref_user_positions_branches';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['position_id', 'position_branch_id'], 'required'],
+			[['position_id', 'position_branch_id'], 'integer'],
+			[['position_id', 'position_branch_id'], 'unique', 'targetAttribute' => ['position_id', 'position_branch_id']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'position_id' => 'Должность',
+			'position_branch_id' => 'Ветвь'
+		];
+	}
+
+}
---
+++ modules/references/models/relations/RelRefUserPositionsTypes.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\references\models\relations;
+
+use app\models\core\traits\ARExtended;
+use app\models\relations\Relations;
+use yii\db\ActiveRecord;
+
+/**
+ * Class RelRefUserPositionsTypes
+ * @package app\modules\references\models\relations
+ */
+class RelRefUserPositionsTypes extends ActiveRecord {
+	use Relations;
+	use ARExtended;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function tableName():string {
+		return 'rel_ref_user_positions_types';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['position_id', 'position_type_id'], 'required'],
+			[['position_id', 'position_type_id'], 'integer'],
+			[['position_id', 'position_type_id'], 'unique', 'targetAttribute' => ['position_id', 'position_type_id']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'position_id' => 'Должность',
+			'position_type_id' => 'Тип'
+		];
+	}
+
+}
