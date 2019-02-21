6119cf0c50bfbcc7e8059a107ed4f6c98c830ae3
Pozitronik <antikillerxxl@gmail.com>
2019-02-21 13:47:17 +0300

    Реализация отношений между должностями, типами должностей и ветвями должностей + форма редактирования должностей с учётом новых полей

--- modules/references/models/refs/RefUserPositions.php
+++ modules/references/models/refs/RefUserPositions.php
@@ -3,8 +3,12 @@ declare(strict_types = 1);
 
 namespace app\modules\references\models\refs;
 
+use app\helpers\ArrayHelper;
 use app\modules\references\models\Reference;
+use app\modules\references\models\relations\RelRefUserPositionsBranches;
+use app\modules\references\models\relations\RelRefUserPositionsTypes;
 use app\modules\users\models\Users;
+use yii\db\ActiveQuery;
 
 /**
  * This is the model class for table "ref_user_positions".
@@ -12,9 +16,23 @@ use app\modules\users\models\Users;
  * @property int $id
  * @property string $name Название
  * @property int $usedCount Количество объектов, использующих это значение справочника
+ * @property string $color//todo
  * @property int $deleted
+ *
+ * @property RelRefUserPositionsBranches|ActiveQuery $relRefUserPositionsBranches
+ * @property RefUserPositionBranches|ActiveQuery $relRefUserPositionBranch
+ *
+ * @property RelRefUserPositionsTypes[]|ActiveQuery $relRefUserPositionsTypes
+ * @property RefUserPositionTypes[]|ActiveQuery $relRefUserPositionTypes
+ *
+ * @property null|int $branch
+ * @property null|int[] $types
+ *
+ * @property-read null|string $branchName
+ * @property-read null|string $typeName
  */
 class RefUserPositions extends Reference {
+	public $color = null;
 	public $menuCaption = 'Должности';
 	public $menuIcon = false;
 
@@ -33,7 +51,8 @@ class RefUserPositions extends Reference {
 			[['name'], 'required'],
 			[['name'], 'unique'],
 			[['id', 'deleted'], 'integer'],
-			[['name'], 'string', 'max' => 256]
+			[['name'], 'string', 'max' => 256],
+			[['branch', 'types'], 'safe'],//relational attributes
 		];
 	}
 
@@ -45,7 +64,11 @@ class RefUserPositions extends Reference {
 			'id' => 'ID',
 			'name' => 'Название',
 			'deleted' => 'Deleted',
-			'usedCount' => 'Использований'
+			'usedCount' => 'Использований',
+			'branchName' => 'Ветвь',
+			'typeName' => 'Тип',
+			'branch' => 'Ветвь',
+			'types' => 'Тип'
 		];
 	}
 
@@ -66,4 +89,78 @@ class RefUserPositions extends Reference {
 	public function getUsedCount():int {
 		return (int)Users::find()->where(['position' => $this->id])->count();
 	}
+
+	/**
+	 * @return RelRefUserPositionsBranches|ActiveQuery
+	 */
+	public function getRelRefUserPositionsBranches() {
+		return $this->hasOne(RelRefUserPositionsBranches::class, ['position_id' => 'id']);
+	}
+
+	/**
+	 * @return RefUserPositionBranches|ActiveQuery
+	 */
+	public function getRelRefUserPositionBranch() {
+		return $this->hasOne(RefUserPositionBranches::class, ['id' => 'position_branch_id'])->via('relRefUserPositionsBranches');
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getBranchName():?string {
+		return ArrayHelper::getValue($this->relRefUserPositionBranch, 'name');
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getTypeName():?string {
+		return '<Тип не указан>';
+	}
+
+	/**
+	 * @return int|null
+	 */
+	public function getBranch():?int {
+		return ArrayHelper::getValue($this->relRefUserPositionBranch, 'id');
+	}
+
+	/**
+	 * @param mixed $branch
+	 * //не можем типизировать null, т.к. может быть передана строка, а делать преобразования бессмысленно
+	 */
+	public function setBranch($branch):void {
+		RelRefUserPositionsBranches::deleteAll(['position_id' => $this->id]);
+		RelRefUserPositionsBranches::linkModel($this->id, $branch);//проверки на пустоту делает метод
+	}
+
+	/**
+	 * @return int[]
+	 */
+	public function getTypes():array {
+		return ArrayHelper::getColumn($this->relRefUserPositionTypes, 'id');
+	}
+
+	/**
+	 * @param mixed $types
+	 */
+	public function setTypes($types):void {
+		RelRefUserPositionsTypes::deleteAll(['position_id' => $this->id]);
+		RelRefUserPositionsTypes::linkModels($this->id, $types);//проверки на пустоту делает метод
+	}
+
+	/**
+	 * @return RelRefUserPositionsTypes[]|ActiveQuery
+	 */
+	public function getRelRefUserPositionsTypes() {
+		return $this->hasMany(RelRefUserPositionsTypes::class, ['position_id' => 'id']);
+	}
+
+	/**
+	 * @return RefUserPositionTypes[]|ActiveQuery
+	 */
+	public function getRelRefUserPositionTypes() {
+		return $this->hasMany(RefUserPositionTypes::class, ['id' => 'position_type_id'])->via('relRefUserPositionsTypes');
+	}
+
 }
---
+++ modules/references/views/references/refuserpositions/_form.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var RefUserRoles $model
+ * @var ActiveForm $form
+ */
+
+use app\modules\references\models\refs\RefUserPositionBranches;
+use app\modules\references\models\refs\RefUserPositionTypes;
+use app\modules\references\models\refs\RefUserRoles;
+use app\modules\references\widgets\reference_select\ReferenceSelectWidget;
+use kartik\color\ColorInput;
+use kartik\switchinput\SwitchInput;
+use yii\web\View;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+?>
+<div class="row">
+	<div class="col-xs-12">
+		<div class="panel">
+			<div class="panel-heading">
+				<div class="panel-control"></div>
+				<h3 class="panel-title"><?= Html::encode($this->title); ?></h3>
+			</div>
+			<?php $form = ActiveForm::begin(); ?>
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-md-5">
+						<?= $form->field($model, 'name')->textInput([
+							'maxlength' => true,
+							'autofocus' => 'autofocus',
+							'spellcheck' => 'true'
+						]); ?>
+					</div>
+
+					<div class="col-md-3">
+						<?= $form->field($model, 'color')->widget(ColorInput::class, [
+							'options' => [
+								'placeholder' => 'Выбрать цвет'
+							],
+							'pluginOptions' => [
+								'showAlpha' => false,
+								'preferredFormat' => 'rgb'
+							]
+						]) ?>
+					</div>
+					<div class="col-md-2">
+						<?= $form->field($model, 'types')->widget(ReferenceSelectWidget::class, [
+							'referenceClass' => RefUserPositionTypes::class,
+							'options' => ['placeholder' => 'Выберите тип'],
+							'pluginOptions' => [
+								'multiple' => true,
+								'allowClear' => true
+							]
+						]); ?>
+					</div>
+					<div class="col-md-2">
+						<?= $form->field($model, 'branch')->widget(ReferenceSelectWidget::class, [
+							'referenceClass' => RefUserPositionBranches::class,
+							'options' => ['placeholder' => 'Выберите ветвь'],
+							'pluginOptions' => [
+								'allowClear' => true
+							]
+						]); ?>
+					</div>
+				</div>
+
+			</div>
+			<div class="panel-footer">
+				<?= Html::submitButton($model->isNewRecord?'Создать':'Изменить', ['class' => $model->isNewRecord?'btn btn-success':'btn btn-primary']); ?>
+				<?php if ($model->isNewRecord): ?>
+					<?= Html::input('submit', 'more', 'Сохранить и добавить ещё', ['class' => 'btn btn-primary']); ?>
+				<?php endif ?>
+			</div>
+			<?php ActiveForm::end(); ?>
+		</div>
+	</div>
+</div>
