41d645260e48c65cfff294699c08776f43d53757
Pozitronik <antikillerxxl@gmail.com>
2019-02-21 16:50:28 +0300

    Модель соответствий герйдов и должностей

---
+++ modules/grades/models/GradesPositionsRules.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\grades\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "grades_positions_rules".
+ *
+ * @property int $id
+ * @property int $grade_id
+ * @property int $position_id
+ */
+class GradesPositionsRules extends ActiveRecord {
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'grades_positions_rules';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['grade_id', 'position_id'], 'required'],
+			[['grade_id', 'position_id'], 'integer'],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'grade_id' => 'Grade ID',
+			'position_id' => 'Position ID',
+		];
+	}
+}
