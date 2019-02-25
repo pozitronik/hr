9808f8a2181e126c74fda6e3f51e54c7de8fe984
Pozitronik <antikillerxxl@gmail.com>
2019-02-25 09:48:57 +0300

    Модель справочника групп премирования

---
+++ modules/grades/models/references/RefGradePremiumGroups.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\grades\models\references;
+
+use app\modules\references\models\Reference;
+use yii\helpers\Html;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $color
+ * @property int $deleted
+ */
+class RefGradePremiumGroups extends Reference {
+	public $menuCaption = 'Типы отношений атрибутов';
+	public $menuIcon = false;
+
+	protected $_dataAttributes = ['color'];
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'ref_grade_premium_groups';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['id'], 'integer'],
+			[['name'], 'required'],
+			[['deleted'], 'integer'],
+			[['name', 'color'], 'string', 'max' => 255]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'name' => 'Название',
+			'deleted' => 'Deleted',
+			'color' => 'Цвет',
+			'usedCount' => 'Использований'
+		];
+	}
+
+	/**
+	 * Набор колонок для отображения на главной
+	 * @return array
+	 */
+	public function getColumns():array {
+		return [
+			[
+				'attribute' => 'id',
+				'options' => [
+					'style' => 'width:36px;'
+				]
+			],
+			[
+				'attribute' => 'name',
+				'value' => function($model) {
+					/** @var self $model */
+					return $model->deleted?Html::tag('span', "Удалено:", [
+							'class' => 'label label-danger'
+						]).$model->name:Html::tag('span', Html::a($model->name, ['update', 'class' => $model->formName(), 'id' => $model->id]), [
+						'style' => "background: {$model->color}"
+					]);
+				},
+				'format' => 'raw'
+			],
+			[
+				'attribute' => 'usedCount'
+			]
+
+		];
+	}
+
+}
