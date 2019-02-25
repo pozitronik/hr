12f927502cca7c88dd221e80611e39026e2b20ab
Pozitronik <antikillerxxl@gmail.com>
2019-02-25 15:08:42 +0300

    Модель-прослойка для прозрачной работы со списком справочников

---
+++ modules/references/models/ReferenceLoader.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\references\models;
+
+use app\helpers\ArrayHelper;
+use app\models\core\core_module\PluginsSupport;
+use app\models\core\Magic;
+use Yii;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use yii\base\Model;
+
+/**
+ * Class ReferenceLoader
+ * @package app\modules\references
+ *
+ * @property Reference[] $list [$referenceClassName] => object Reference
+ *
+ */
+class ReferenceLoader extends Model {
+
+	/**
+	 * @return Reference[]
+	 */
+	public static function getList():array {
+		$baseReferences = [];
+
+		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(Yii::getAlias(Reference::REFERENCES_DIRECTORY)), RecursiveIteratorIterator::SELF_FIRST);
+		/** @var RecursiveDirectoryIterator $file */
+		foreach ($files as $file) {
+			if ($file->isFile() && 'php' === $file->getExtension() && null !== $model = Magic::GetReferenceModel($file->getRealPath())) {
+				$baseReferences[$model->formName()] = $model;
+			}
+		}
+		$pluginsReferences = PluginsSupport::GetAllReferences();
+		return array_merge($baseReferences, $pluginsReferences);
+	}
+
+	/**
+	 * @param string $className
+	 * @return Reference|null
+	 */
+	public static function getReferenceByClassName(string $className):?Reference {
+		return ArrayHelper::getValue(self::getList(), $className);
+	}
+}
