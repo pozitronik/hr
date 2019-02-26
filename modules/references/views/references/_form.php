1ca1b9563a2b2d5dc6aa0f1c94b0e9f45a34c048
Pozitronik <antikillerxxl@gmail.com>
2019-02-26 16:54:56 +0300

    Фикс вёрстки

--- modules/references/views/references/_form.php
+++ modules/references/views/references/_form.php
@@ -11,29 +11,27 @@ use app\modules\references\models\Reference;
 use yii\web\View;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+
 ?>
-<div class="row">
-	<div class="col-xs-12">
-		<div class="panel">
-			<div class="panel-heading">
-				<div class="panel-control"></div>
-				<h3 class="panel-title"><?= Html::encode($this->title); ?></h3>
-			</div>
-			<?php $form = ActiveForm::begin(); ?>
-			<div class="panel-body">
-				<?= $form->field($model, 'name')->textInput([
-					'maxlength' => true,
-					'autofocus' => 'autofocus',
-					'spellcheck' => 'true'
-				]); ?>
-			</div>
-			<div class="panel-footer">
-				<?= Html::submitButton($model->isNewRecord?'Создать':'Изменить', ['class' => $model->isNewRecord?'btn btn-success':'btn btn-primary']); ?>
-				<?php if ($model->isNewRecord): ?>
-					<?= Html::input('submit', 'more', 'Сохранить и добавить ещё', ['class' => 'btn btn-primary']); ?>
-				<?php endif ?>
-			</div>
-			<?php ActiveForm::end(); ?>
-		</div>
+
+<div class="panel">
+	<div class="panel-heading">
+		<div class="panel-control"></div>
+		<h3 class="panel-title"><?= Html::encode($this->title); ?></h3>
 	</div>
+	<?php $form = ActiveForm::begin(); ?>
+	<div class="panel-body">
+		<?= $form->field($model, 'name')->textInput([
+			'maxlength' => true,
+			'autofocus' => 'autofocus',
+			'spellcheck' => 'true'
+		]); ?>
+	</div>
+	<div class="panel-footer">
+		<?= Html::submitButton($model->isNewRecord?'Создать':'Изменить', ['class' => $model->isNewRecord?'btn btn-success':'btn btn-primary']); ?>
+		<?php if ($model->isNewRecord): ?>
+			<?= Html::input('submit', 'more', 'Сохранить и добавить ещё', ['class' => 'btn btn-primary']); ?>
+		<?php endif ?>
+	</div>
+	<?php ActiveForm::end(); ?>
 </div>
