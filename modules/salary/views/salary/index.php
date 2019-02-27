258862bdb9ecb6f53e16551259b8e744deca4b6c
Pozitronik <antikillerxxl@gmail.com>
2019-02-27 11:36:56 +0300

    Наброски списка зарплатных вилок

--- modules/salary/views/salary/index.php
+++ modules/salary/views/salary/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection MissedFieldInspection */
 declare(strict_types = 1);
 
 /**
@@ -8,14 +8,13 @@ declare(strict_types = 1);
  */
 
 use app\helpers\Icons;
-use app\modules\privileges\models\Privileges;
+use app\modules\salary\models\SalaryFork;
+use app\modules\salary\widgets\navigation_menu\SalaryForkMenuWidget;
 use yii\data\ActiveDataProvider;
-use yii\data\ArrayDataProvider;
 use yii\db\ActiveRecord;
 use yii\web\View;
 use kartik\grid\GridView;
 use yii\bootstrap\Html;
-use kartik\grid\ActionColumn;
 
 $this->title = 'Зарплатные вилки';
 $this->params['breadcrumbs'][] = $this->title;
@@ -35,79 +34,42 @@ $this->params['breadcrumbs'][] = $this->title;
 	'responsive' => true,
 	'columns' => [
 		[
-			'class' => ActionColumn::class,
+			'filter' => false,
 			'header' => Icons::menu(),
-			'dropdown' => true,
-			'dropdownButton' => [
-				'label' => Icons::menu(),
-				'caret' => ''
+			'mergeHeader' => true,
+			'headerOptions' => [
+				'class' => 'skip-export kv-align-center kv-align-middle'
 			],
-			'template' => '{update} {delete}',
-			'buttons' => [
-				'update' => function($url, $model) {
-					/** @var Privileges $model */
-					return Html::tag('li', Html::a(Icons::update().'Изменение', ['update', 'id' => $model->id]));
-				},
-				'delete' => function($url, $model) {
-					/** @var Privileges $model */
-					return Html::tag('li', Html::a(Icons::delete().'Удаление', ['delete', 'id' => $model->id], [
-						'title' => 'Удалить запись',
-						'data' => [
-							'confirm' => $model->deleted?'Вы действительно хотите восстановить запись?':'Вы действительно хотите удалить запись?',
-							'method' => 'post'
-						]
-					]));
-				}
-			]
-		],
-		[
-			'attribute' => 'name',
-			'value' => function($model) {
-				/** @var Privileges $model */
-				return Html::a($model->name, ['update', 'id' => $model->id]);
-			},
-			'format' => 'raw'
-		],
-		'default:boolean',
-		[
-			'attribute' => 'userRights',
-			'value' => function($model) {
-				/** @var Privileges $model */
-				return GridView::widget([
-					'dataProvider' => new ArrayDataProvider([
-						'allModels' => $model->userRights
-					]),
-					'panel' => false,
-					'summary' => false,
-					'headerRowOptions' => [
-						'style' => 'display:none'
-					],
-					'toolbar' => false,
-					'export' => false,
-					'resizableColumns' => false,
-					'responsive' => true,
-					'options' => [
-						'class' => 'grid_view_cell'
-					],
-					'columns' => [
-						[
-							'attribute' => 'name',
-							'options' => [
-								'style' => 'width:20%'
-							]
-						],
-						'description'
-					]
+			'contentOptions' => [
+				'style' => 'width:50px',
+				'class' => 'skip-export kv-align-center kv-align-middle'
+			],
+			'value' => function(SalaryFork $model) {
+				return SalaryForkMenuWidget::widget([
+					'model' => $model,
+					'mode' => SalaryForkMenuWidget::MODE_ACTION_COLUMN_MENU
 				]);
 			},
 			'format' => 'raw'
 		],
 		[
-			'attribute' => 'usersCount',
-			'value' => function($model) {
-				/** @var Privileges $model */
-				return $model->default?"Все пользователи":$model->usersCount;
-			}
-		]
+			'attribute' => 'refUserPosition.name'
+		],
+		[
+			'attribute' => 'refGrade.name'
+		],
+		[
+			'attribute' => 'refPremiumGroup.name'
+		],
+		[
+			'attribute' => 'refLocation.name'
+		],
+		[
+			'attribute' => 'min'
+		],
+		[
+			'attribute' => 'max'
+		],
+
 	]
 ]); ?>
