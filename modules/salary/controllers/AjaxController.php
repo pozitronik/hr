e5ef09b17af6a64920ea38656f4ef62e0e61c001
Pozitronik <antikillerxxl@gmail.com>
2019-02-27 14:35:17 +0300

    Ajax-контроллер для зарплат + функция отдачи грейдов, привязанных к должности

---
+++ modules/salary/controllers/AjaxController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\salary\controllers;
+
+use app\helpers\ArrayHelper;
+use app\models\core\ajax\BaseAjaxController;
+use app\modules\salary\models\references\RefUserPositions;
+use Throwable;
+use Yii;
+
+/**
+ * Class AjaxController
+ */
+class AjaxController extends BaseAjaxController {
+
+	/**
+	 * Функция для DepDrop-виджета, отдающая список грейдов, привязанных к должности
+	 * @return array
+	 * @throws Throwable
+	 */
+	public function actionGetPositionGrades():array {
+
+		$position_id = ArrayHelper::getValue(Yii::$app->request->post('depdrop_parents'), 0);
+		if ((null === $position = RefUserPositions::findModel($position_id)) || empty($grades = $position->relGrades)) {//Неправильной должности тут быть не может, значит не заданы грейды
+			return ([
+				'output' => [/*['id' => -1, 'name' => 'Не заполнен список грейдов']*/]
+			]);
+		}
+		$out = ArrayHelper::mapEx($grades, ['id' => 'id', 'name' => 'name']);
+		return ['output' => $out, 'selected' => ArrayHelper::getValue($out, '0.id')];
+
+	}
+}
