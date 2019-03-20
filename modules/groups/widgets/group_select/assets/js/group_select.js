26c4d343b6db8bbf7e3f1608f677838dcda80433
Pozitronik <antikillerxxl@gmail.com>
2019-03-20 14:20:05 +0300

    Код вывода форматированного списка групп при аяксовом поиске

--- modules/groups/widgets/group_select/assets/js/group_select.js
+++ modules/groups/widgets/group_select/assets/js/group_select.js
@@ -2,11 +2,22 @@ function formatGroup(item) {
 	if (item.loading || item.hasOwnProperty('children')) {
 		return item.text;
 	}
-	var typecolor = $(item.element).data('typecolor');
+	var typecolor = $(item.element).data('typecolor') || 'inherited';
 	if (typecolor) typecolor = 'style="background: ' + typecolor + ';"';
-	var logo = $(item.element).data('logo');
-	var typename = $(item.element).data('typename');
-	if (!typename) typename = '';
+	var logo = $(item.element).data('logo') || '';
+	var typename = $(item.element).data('typename') || '';
+	return '<div class="select-item"><div class="row"><div class="col-sm-8"><img src="' + logo + '" class="group-logo" alt="logo">' + item.text + '</div><div class="col-sm-4 text-overflow" ' + typecolor + '>' + typename + '</div></div></div>';
+}
+
+function formatGroupAJAX(item) {
+	if (item.loading || item.hasOwnProperty('children')) {
+		return item.text;
+	}
+
+	var typecolor = item.typecolor || 'inherited';
+	if (typecolor) typecolor = 'style="background: ' + typecolor + ';"';
+	var logo = item.logo || '';
+	var typename = item.typename || '';
 	return '<div class="select-item"><div class="row"><div class="col-sm-8"><img src="' + logo + '" class="group-logo" alt="logo">' + item.text + '</div><div class="col-sm-4 text-overflow" ' + typecolor + '>' + typename + '</div></div></div>';
 }
 
