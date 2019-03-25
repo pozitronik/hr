0cd226e5e628405aa69f3ea77dd6e25287929bad
Pozitronik <antikillerxxl@gmail.com>
2019-03-25 13:59:14 +0300

    System logger model

---
+++ models/SysLog.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models;
+
+use app\models\user\CurrentUser;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+
+/**
+ * Class SysLog
+ * @package app\models
+ * @property integer $id
+ * @property-read string $timestamp
+ * @property int $user
+ * @property string $model
+ * @property int $model_key
+ * @property array $old_attributes
+ * @property array $new_attributes
+ *
+ */
+class SysLog extends ActiveRecord {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function rules():array {
+		return [
+			[['user', 'model_key'], 'integer'],
+			[['model'], 'string'],
+			[['old_attributes', 'new_attributes'], 'safe']
+		];
+	}
+
+	/**
+	 * @param ActiveRecord $model
+	 * @param bool $ignoreUnchanged
+	 * @return bool
+	 * @throws InvalidConfigException
+	 */
+	public static function logChanges(ActiveRecord $model, bool $ignoreUnchanged = true):bool {
+		if (([] === $diff = $model->identifyChangedAttributes()) && $ignoreUnchanged) return true;
+		$changedAttributes = array_intersect_key($model->oldAttributes, $diff);
+		self::push($model->formName(), $model->primaryKey, $changedAttributes, $diff);
+		return true;
+	}
+
+	/**
+	 * @param string|null $modelName
+	 * @param int|null $pKey
+	 * @param array $oldAttributes
+	 * @param array $newAttributes
+	 */
+	public static function push(?string $modelName, ?int $pKey, array $oldAttributes, array $newAttributes):void {
+		$log = new self([
+			'user' => CurrentUser::Id(),
+			'model' => $modelName,
+			'model_key' => $pKey,
+			'old_attributes' => $oldAttributes,
+			'new_attributes' => $newAttributes
+		]);
+		$log->save();
+	}
+
+}
