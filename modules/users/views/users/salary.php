cda77b3663dee0ab4624aef86c5f042d51c3397f
Pozitronik <antikillerxxl@gmail.com>
2019-03-25 11:16:33 +0300

    Фикс ошибки при неназначенной должности

--- modules/users/views/users/salary.php
+++ modules/users/views/users/salary.php
@@ -52,7 +52,7 @@ $this->params['breadcrumbs'][] = $this->title;
 					<?= $form->field($model, 'relGrade')->widget(RefDepDrop::class, [
 						'options' => ['placeholder' => 'Выберите грейд'],
 						'referenceClass' => RefGrades::class,
-						'data' => null === $model->relUsersSalary->grade_id?ArrayHelper::map($model->relRefUserPositions->relGrades, 'id', 'name'):[$model->relUsersSalary->grade_id => ArrayHelper::getValue($model, 'relGrade.name')],
+						'data' => null === $model->relUsersSalary->grade_id?ArrayHelper::map(ArrayHelper::getValue($model->relRefUserPositions, 'relGrades'), 'id', 'name'):[$model->relUsersSalary->grade_id => ArrayHelper::getValue($model, 'relGrade.name')],
 						'type' => RefDepDrop::TYPE_REFERENCE_SELECT,
 						'pluginOptions' => [
 							'depends' => ['users-position'],
