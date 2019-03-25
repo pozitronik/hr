7f92a7117c0a947442c1bddb750d57044251227a
Pozitronik <antikillerxxl@gmail.com>
2019-03-25 15:04:52 +0300

    дока

--- models/core/traits/MethodsAccess.php
+++ models/core/traits/MethodsAccess.php
@@ -11,6 +11,7 @@ use yii\base\Model;
 
 /**
  * Код, включающий проверки доступов
+ * Пока пишется здесь, потом должно быть перенесено/смонтировано в ActiveRecordExtended
  * Алсо, можно добавить проверки на canGetProperty/canSetProperty
  * Trait MethodsAccess
  * @package app\models\prototypes
@@ -22,7 +23,7 @@ trait MethodsAccess {
 	 * @return bool
 	 * @throws Throwable
 	 */
-	public function beforeSave($insert) {
+	public function beforeSave($insert):bool {
 		/** @var Model $this */
 		if (parent::beforeSave($insert) && !UserAccess::canAccess($this, $insert?AccessMethods::create:AccessMethods::update)) {
 			AlertModel::AccessNotify();
@@ -35,7 +36,7 @@ trait MethodsAccess {
 	 * @return bool
 	 * @throws Throwable
 	 */
-	public function beforeDelete() {
+	public function beforeDelete():bool {
 		/** @var Model $this */
 		if (parent::beforeDelete() && !UserAccess::canAccess($this, AccessMethods::delete)) {
 			AlertModel::AccessNotify();
