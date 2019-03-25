2502bdb29609391203a1b1ebfd4d7dbde9e20660
Pozitronik <antikillerxxl@gmail.com>
2019-03-25 13:54:50 +0300

    migration fixes

--- migrations/m190325_091025_sys_log.php
+++ migrations/m190325_091025_sys_log.php
@@ -15,11 +15,15 @@ class m190325_091025_sys_log extends Migration {
 			'at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
 			'user' => $this->integer()->defaultValue(null),
 			'model' => $this->string(64)->null(),
-			'attributes' => $this->json()
+			'model_key' => $this->integer()->null(),
+			'old_attributes' => $this->json(),
+			'new_attributes' => $this->json(),
 		]);
 
 		$this->createIndex('user', 'sys_log', 'user');
 		$this->createIndex('model', 'sys_log', 'model');
+		$this->createIndex('model_key', 'sys_log', 'model_key');
+		$this->createIndex('model_model_key', 'sys_log', ['model', 'model_key']);
 
 	}
 
