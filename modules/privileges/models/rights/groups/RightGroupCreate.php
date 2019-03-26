8c8d79b53c9670425aa080dae74b399022cf04f5
Pozitronik <antikillerxxl@gmail.com>
2019-03-26 10:43:01 +0300

    Право создания группы

---
+++ modules/privileges/models/rights/groups/RightGroupCreate.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\privileges\models\rights\groups;
+
+use app\helpers\ArrayHelper;
+use app\modules\privileges\models\UserRight;
+use yii\web\Controller;
+
+/**
+ * Class RightGroupCreate
+ * @package app\modules\privileges\models\rights\groups
+ */
+class RightGroupCreate extends UserRight {
+
+
+	/**
+	 * Имя права
+	 * @return string
+	 */
+	public function getName():string {
+		return "Создание группы";
+	}
+
+	/**
+	 * Подробное описание возможностей, предоставляемых правом
+	 * @return string
+	 */
+	public function getDescription():string {
+		return "Разрешает создать новую группу в системе";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getAccess(Controller $controller, string $action, array $actionParameters = []):?bool {
+		$definedRules = [
+			'groups/groups' => [
+				'actions' => [
+					'create' => self::ACCESS_ALLOW
+				]
+			]
+		];
+		return ArrayHelper::getValue($definedRules, "{$controller->id}.actions.{$action}", parent::getAccess($controller, $action));
+	}
+}
