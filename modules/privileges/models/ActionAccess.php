dee6b9dcc42add266e2a90611e4c916ef8c721df
Pozitronik <antikillerxxl@gmail.com>
2019-03-28 11:42:15 +0300

    Набросок модели элемента карты доступов

---
+++ modules/privileges/models/ActionAccess.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\privileges\models;
+
+use yii\base\Model;
+
+/**
+ * Class ActionMap
+ * @package app\modules\privileges\models
+ * @property-read string $id
+ * @property string $actionName
+ * @property string $controllerId
+ * @property string $moduleId
+ * @property null|bool $state
+ */
+class ActionAccess extends Model {
+	private $_actionName;
+	private $_controllerId;
+	private $_moduleId;
+	private $_state;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'actionName' => 'Действие',
+			'controllerId' => 'Контроллер',
+			'moduleId' => 'Модуль',
+			'state' => 'Разрешение'
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getActionName():string {
+		return $this->_actionName;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setActionName(string $name):void {
+		$this->_actionName = $name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getControllerId():string {
+		return $this->_controllerId;
+	}
+
+	/**
+	 * @param string $controllerId
+	 */
+	public function setControllerId(string $controllerId):void {
+		$this->_controllerId = $controllerId;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getModuleId():string {
+		return $this->_moduleId;
+	}
+
+	/**
+	 * @param string $moduleId
+	 */
+	public function setModuleId(string $moduleId):void {
+		$this->_moduleId = $moduleId;
+	}
+
+	/**
+	 * @return bool|null
+	 */
+	public function getState():?bool {
+		return $this->_state;
+	}
+
+	/**
+	 * @param bool|null $state
+	 */
+	public function setState(?bool $state):void {
+		$this->_state = $state;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId():string {
+		return "{$this->moduleId}.{$this->controllerId}.{$this->actionName}";
+	}
+
+}
