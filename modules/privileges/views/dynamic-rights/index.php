96e98bcba2a24e3cff10d6f70fe662c8f44bf223
Pozitronik <antikillerxxl@gmail.com>
2019-03-29 13:48:31 +0300

    Минимально рабочая главная динамических прав. Буду сильно думать, как упростить, текущий вариант - не то, чтоб говно. Задумка охренительная, как и первый вариант, но вот состыковать их так, чтобы администратор системы не ёбнулся при настройке у меня не получается. Мне нужно подумать.

--- modules/privileges/views/dynamic-rights/index.php
+++ modules/privileges/views/dynamic-rights/index.php
@@ -7,11 +7,8 @@ declare(strict_types = 1);
  * @var ActiveDataProvider $dataProvider
  */
 
-use app\helpers\Icons;
-use app\modules\privileges\models\Privileges;
-use app\modules\privileges\widgets\navigation_menu\PrivilegesNavigationMenuWidget;
+use app\modules\privileges\models\DynamicUserRights;
 use yii\data\ActiveDataProvider;
-use yii\data\ArrayDataProvider;
 use yii\db\ActiveRecord;
 use yii\web\View;
 use kartik\grid\GridView;
@@ -39,72 +36,11 @@ $this->params['breadcrumbs'][] = $this->title;
 	'responsive' => true,
 	'columns' => [
 		[
-			'filter' => false,
-			'header' => Icons::menu(),
-			'mergeHeader' => true,
-			'headerOptions' => [
-				'class' => 'skip-export kv-align-center kv-align-middle'
-			],
-			'contentOptions' => [
-				'style' => 'width:50px',
-				'class' => 'skip-export kv-align-center kv-align-middle'
-			],
-			'value' => static function(Privileges $model) {
-				return PrivilegesNavigationMenuWidget::widget([
-					'model' => $model,
-					'mode' => PrivilegesNavigationMenuWidget::MODE_ACTION_COLUMN_MENU
-				]);
-			},
-			'format' => 'raw'
-		],
-		[
 			'attribute' => 'name',
-			'value' => static function(Privileges $model) {
+			'value' => static function(DynamicUserRights $model) {
 				return Html::a($model->name, ['update', 'id' => $model->id]);
 			},
 			'format' => 'raw'
-		],
-		'default:boolean',
-		[
-			'attribute' => 'userRights',
-			'value' => static function(Privileges $model) {
-				return GridView::widget([
-					'dataProvider' => new ArrayDataProvider([
-						'allModels' => $model->userRights
-					]),
-					'panel' => false,
-					'summary' => false,
-					'headerRowOptions' => [
-						'style' => 'display:none'
-					],
-					'toolbar' => false,
-					'export' => false,
-					'resizableColumns' => false,
-					'responsive' => true,
-					'options' => [
-						'class' => 'grid_view_cell'
-					],
-					'columns' => [
-						[
-							'attribute' => 'name',
-							'options' => [
-								'style' => 'width:20%'
-							]
-						],
-						[
-							'attribute' => 'description',
-							'format' => 'raw'
-						]
-					]
-				]);
-			},
-			'format' => 'raw'
-		],
-		[
-			'attribute' => 'usersCount',
-			'value' => static function(Privileges $model) {
-				return $model->default?"Все пользователи":$model->usersCount;
-			}
 		]
 	]
 ]) ?>
