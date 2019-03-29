abad2387d78a8d36b4a80bf90e8d092a2e9c3c0b
Pozitronik <antikillerxxl@gmail.com>
2019-03-29 10:42:08 +0300

    Модель на таблицу связей привилегий с динамическими правами

---
+++ modules/privileges/models/relations/RelPrivilegesDynamicRights.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\privileges\models\relations;
+
+use app\models\core\ActiveRecordExtended;
+use app\models\relations\Relations;
+
+/**
+ * This is the model class for table "rel_privileges_dynamic_rights".
+ *
+ * @property int id
+ * @property int $privilege id привилегии
+ * @property int $right id динамического права
+ *
+ */
+class RelPrivilegesDynamicRights extends ActiveRecordExtended {
+	use Relations;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'rel_privileges_dynamic_rights';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['privilege', 'right'], 'required'],
+			[['id', 'privilege', 'right'], 'integer'],
+			[['privilege', 'right'], 'unique', 'targetAttribute' => ['privilege', 'right']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'privilege' => 'Привилегия',
+			'right' => 'Право'
+		];
+	}
+
+}
