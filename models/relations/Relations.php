50878ef07e503fd9d8178d767a2c947d5c8d9b53
Pozitronik <antikillerxxl@gmail.com>
2019-04-01 12:40:27 +0300

    Код удаления связей переработан: теперь каждый элемент связей обрабатывается отдельно. Минусы: не получится сделать deleteAll на оба массива сразу. Плюсы: теперь можно юзать на любых классах (не только ActiveRecord), поддерживающих нужные интерфейсы, можно херачить любые иерархии идентификаторов, гибкость, короче.

--- models/relations/Relations.php
+++ models/relations/Relations.php
@@ -6,7 +6,7 @@ namespace app\models\relations;
 use app\helpers\ArrayHelper;
 use Throwable;
 use yii\base\Exception;
-use yii\db\ActiveRecord;
+use yii\base\Model;
 
 /**
  * Trait Relations
@@ -17,8 +17,8 @@ trait Relations {
 
 	/**
 	 * Линкует в этом релейшене две модели. Модели могут быть заданы как через айдишники, так и моделью, и ещё тупо строкой
-	 * @param ActiveRecord|integer $master
-	 * @param ActiveRecord|integer $slave
+	 * @param Model|int|string $master
+	 * @param Model|int|string $slave
 	 * @throws Throwable
 	 */
 	public static function linkModel($master, $slave):void {
@@ -46,8 +46,8 @@ trait Relations {
 
 	/**
 	 * Линкует в этом релейшене две модели. Модели могут быть заданы как через айдишники, так и напрямую, в виде массивов или так.
-	 * @param integer|ActiveRecord|integer[]|ActiveRecord[]|string|string[]|array $master
-	 * @param integer|ActiveRecord|integer[]|ActiveRecord[]|string|string[]|array $slave
+	 * @param int|int[]|string|string[]|Model|Model[] $master
+	 * @param int|int[]|string|string[]|Model|Model[] $slave
 	 * @throws Throwable
 	 */
 	public static function linkModels($master, $slave):void {
@@ -68,47 +68,59 @@ trait Relations {
 	}
 
 	/**
-	 * Удаляет связь между моделями в этом релейшене
-	 * @param integer|ActiveRecord|integer[]|ActiveRecord[]|string|string[]|array $master
-	 * @param integer|ActiveRecord|integer[]|ActiveRecord[]|string|string[]|array $slave
+	 * Удаляет единичную связь в этом релейшене
+	 * @param Model|int|string $master
+	 * @param Model|int|string $slave
 	 * @throws Throwable
-	 *
-	 * Функция не будет работать с объектами, не имеющими атрибута/ключа id (даже если в качестве primaryKey указан другой атрибут).
-	 * Такое поведение оставлено специально во избежание ошибок проектирования
-	 * @see Privileges::setDropUserRights
 	 */
-	public static function unlinkModels($master, $slave):void {
+	public static function unlinkModel($master, $slave):void {
 		if (empty($master) || empty($slave)) return;
 		$link = new self();
 		$first_name = ArrayHelper::getValue($link->rules(), '0.0.0', new Exception('Не удалось получить атрибут для связи'));
 		$second_name = ArrayHelper::getValue($link->rules(), '0.0.1', new Exception('Не удалось получить атрибут для связи'));
 
-		if (is_array($master)) {
-			if (!is_numeric(ArrayHelper::getValue($master, 0))) {//suppose it is ActiveRecord[]
-				$master = ArrayHelper::getColumn($master, 'id');
-			}
-		} else {
-			if (is_numeric($master)) {
-				$master = (int)$master;
-			} elseif (is_object($master)) {
-				$master = ArrayHelper::getValue($master, 'primaryKey', new Exception("Класс {$master->formName()} не имеет атрибута primaryKey"));
-			}//suppose it string field name
+		if (is_numeric($master)) {
+			$master = (int)$master;
+		} elseif (is_object($master)) {
+			$master = ArrayHelper::getValue($master, 'primaryKey', new Exception("Класс {$master->formName()} не имеет атрибута primaryKey"));
+		} else $master = (string)$master; //suppose it string field name
 
-		}
-
-		if (is_array($slave)) {
-			if (!is_numeric(ArrayHelper::getValue($slave, 0))) {//suppose it is ActiveRecord[]
-				$slave = ArrayHelper::getColumn($slave, 'id');
-			}
-		} else {
-
-			if (is_numeric($slave)) {
-				$slave = (int)$slave;
-			} elseif (is_object($slave)) {
-				$slave = ArrayHelper::getValue($slave, 'primaryKey', new Exception("Класс {$slave->formName()} не имеет атрибута primaryKey"));
-			}//suppose it string field name
-		}
+		if (is_numeric($slave)) {
+			$slave = (int)$slave;
+		} elseif (is_object($slave)) {
+			$slave = ArrayHelper::getValue($slave, 'primaryKey', new Exception("Класс {$slave->formName()} не имеет атрибута primaryKey"));
+		} else $slave = (string)$slave; //suppose it string field name
 
 		self::deleteAll([$first_name => $master, $second_name => $slave]);
+	}
+
+	/**
+	 * Удаляет связь между моделями в этом релейшене
+	 * @param int|int[]|string|string[]|Model|Model[] $master
+	 * @param int|int[]|string|string[]|Model|Model[] $slave
+	 * @throws Throwable
+	 *
+	 * Функция не будет работать с объектами, не имеющими атрибута/ключа id (даже если в качестве primaryKey указан другой атрибут).
+	 * Такое поведение оставлено специально во избежание ошибок проектирования
+	 * @see Privileges::setDropUserRights
+	 *
+	 * Передавать массивы строк/идентификаторов нельзя (только массив моделей)
+	 */
+	public static function unlinkModels($master, $slave):void {
+		if (empty($master) || empty($slave)) return;
+		if (is_array($master)) {
+			foreach ($master as $master_item) {
+				if (is_array($slave)) {
+					foreach ($slave as $slave_item) {
+						self::unlinkModel($master_item, $slave_item);
+					}
+				} else self::unlinkModel($master_item, $slave);
+			}
+		} else if (is_array($slave)) {
+			foreach ($slave as $slave_item) {
+				self::unlinkModel($master, $slave_item);
+			}
+		} else self::unlinkModel($master, $slave);
+
 	}
 }
