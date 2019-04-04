abc1e2f8a25d6a6be7c88e1177d6c422c18f9720
Pozitronik <antikillerxxl@gmail.com>
2019-04-04 16:40:01 +0300

    Новый метод в интерфейсе

--- models/prototypes/HistoryEventInterface.php
+++ models/prototypes/HistoryEventInterface.php
@@ -15,11 +15,16 @@ use app\modules\users\models\Users;
  * @property string $eventTime Во сколько сделал
  * @property string $objectName Где сделал
  * @property null|Users $subject Кто сделал
- * @property string $action Что произошло
+ * @property HistoryEventAction[] $actions Что произошло
  */
 interface HistoryEventInterface {
 	const EVENT_CREATED = 0;
 	const EVENT_CHANGED = 1;
 	const EVENT_DELETED = 2;
 
+	/**
+	 * Converts log event to timeline entry
+	 * @return TimelineEntry
+	 */
+	public function asTimelineEntry():TimelineEntry;
 }
