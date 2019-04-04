44898bb6eeb8b0deb720d3cdd1f837af5589d2a2
Pozitronik <antikillerxxl@gmail.com>
2019-04-04 13:17:57 +0300

    переменные атрибутов

--- models/prototypes/HistoryEvent.php
+++ models/prototypes/HistoryEvent.php
@@ -8,8 +8,23 @@ use yii\base\Model;
 /**
  * Class HistoryEvent
  * @package app\models\prototypes
+ *
+ * @property int $eventType Что сделал
+ * @property string $eventTypeName Что сделал
+ * @property string|null $eventIcon Иконка?
+ * @property string $eventTime Во сколько сделал
+ * @property string $objectName Где сделал
+ * @property string $subjectName Кто сделал
+ * @property string $action Что произошло
  */
 class HistoryEvent extends Model implements HistoryEventInterface {
+	public $eventType;
+	public $eventTypeName;
+	public $eventIcon;
+	public $eventTime;
+	public $objectName;
+	public $subjectName;
+	public $action;
 
 	/**
 	 * @return string
