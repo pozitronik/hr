c6894fcb4e27b53b88ebfeb4b490f64a041e2f4b
Pozitronik <antikillerxxl@gmail.com>
2019-04-04 15:54:30 +0300

    Прототип модели события истории

---
+++ models/prototypes/HistoryEventAction.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\models\prototypes;
+
+use yii\base\Model;
+
+/**
+ * Class HistoryEventAction
+ * @package app\models\prototypes
+ *
+ * @property int $type тип события
+ * @property string $attributeName изменившийся атрибут
+ * @property mixed $attributeOldValue значение атрибута до изменения
+ * @property mixed $attributeNewValue значение атрибута после изменения
+ *
+ */
+class HistoryEventAction extends Model {
+	const EVENT_CREATED = 0;
+	const EVENT_CHANGED = 1;
+	const EVENT_DELETED = 2;
+
+	public $type;
+	public $attributeName;
+	public $attributeOldValue;
+	public $attributeNewValue;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function attributeLabels() {
+		return [
+			'type' => 'Изменение',
+			'attributeName' => 'Атрибут',
+			'attributeOldValue' => 'Было',
+			'attributeNewValue' => 'Стало'
+		];
+	}
+}
