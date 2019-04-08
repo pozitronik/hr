17c472c2aff7cff044169fd9f393c4522c5c4aa9
Pozitronik <antikillerxxl@gmail.com>
2019-04-08 13:21:18 +0300

    Прототип схемы задания связей между таблицами релейшенов и таблицами данных

--- models/relations/RelUsersGroups.php
+++ models/relations/RelUsersGroups.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 namespace app\models\relations;
 
 use app\models\core\ActiveRecordExtended;
+use app\modules\groups\models\Groups;
 use app\modules\users\models\references\RefUserRoles;
+use app\modules\users\models\Users;
 use yii\db\ActiveQuery;
 
 /**
@@ -19,6 +21,26 @@ use yii\db\ActiveQuery;
  */
 class RelUsersGroups extends ActiveRecordExtended {
 	use Relations;
+
+	/**
+	 * Прототипируем задание связи отношений таблиц в истории
+	 * @return array
+	 */
+	public function historyRelationsOut():array {
+		return [
+			'Groups' => [
+				'model' => Groups::class,//модель, с которой связывает этот релейшен
+				'link' => ['id' => 'group_id'],//свойства, по которым происходит связь
+				'substitute' => ['group_id' => 'name']//подстановка, т.е. какой атрибут из подстановочной таблицы вернуть вместо входящего атрибута
+			],
+			'Users' => [
+				'model' => Users::class,
+				'link' => ['id' => 'user_id'],
+				'substitute' => ['user_id' => 'username']
+			]
+
+		];
+	}
 
 	/**
 	 * {@inheritdoc}
