f04adba7d4c8e0024c1f02f2105a45ea57a62ff7
Pozitronik <antikillerxxl@gmail.com>
2019-04-09 11:41:45 +0300

    todo done

--- modules/users/models/Users.php
+++ modules/users/models/Users.php
@@ -139,13 +139,7 @@ class Users extends ActiveRecordExtended implements StrictInterface {
 						'link' => ['id' => 'user_id'],
 						'substitute' => ['user_id' => 'username']
 					],
-//					[
-//						'model' => RelUsersGroupsRoles::class,
-//						'link' => ['user_group_id' => 'id'],//todo -> closure
-//						'substitute' => ['id' => 'user_group_id']
-//					]
 				]
-
 			],
 			'RelUsersPrivileges' => [
 				'label' => 'Пользователю добавлена привилегия',
