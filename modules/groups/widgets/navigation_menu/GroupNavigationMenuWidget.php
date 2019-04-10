67bbfb04900617f6dc35f6fcb0c654641f19c8c1
Pozitronik <antikillerxxl@gmail.com>
2019-04-10 10:06:56 +0300

    Ссылка на историю изменений у групп

--- modules/groups/widgets/navigation_menu/GroupNavigationMenuWidget.php
+++ modules/groups/widgets/navigation_menu/GroupNavigationMenuWidget.php
@@ -72,6 +72,11 @@ class GroupNavigationMenuWidget extends BaseNavigationMenuWidget {
 			],
 			[
 				'menu' => true,
+				'label' => Icons::history().'История изменений',
+				'url' => ['/history/history/show', 'for' => $this->model->formName(), 'id' => $this->model->id]
+			],
+			[
+				'menu' => true,
 				'label' => Icons::delete().'Удаление',
 				'url' => ['/groups/groups/delete', 'id' => $this->model->id],
 				'linkOptions' => [
