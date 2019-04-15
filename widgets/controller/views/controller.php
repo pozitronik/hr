92042b8f3773761fe8382aac5e65bdc71e66c238
Pozitronik <antikillerxxl@gmail.com>
2019-04-15 12:47:38 +0300

    unused view <=> replace calculated values to variable parameters

--- widgets/controller/views/controller.php
+++ widgets/controller/views/controller.php
@@ -1,18 +1,18 @@
 <?php
 declare(strict_types = 1);
 
-/* @var View $this
+/**
+ * @var View $this
  * @var WigetableController $model
+ * @var false|string $caption
+ * @var string $style
+ * @var array $action
  */
 
-use app\models\core\Magic;
 use app\models\core\WigetableController;
 use yii\web\View;
 use yii\helpers\Url;
 
-$style = Magic::hasProperty($model, 'menuIcon')?"style = 'background-image: url({$model->menuIcon});'":'';
-$action = ["{$model->route}/{$model->defaultAction}"];
-$caption = Magic::hasProperty($model, 'menuCaption')?$model->menuCaption:$model->id;
 ?>
 
 <a href="<?= Url::toRoute($action) ?>" class="icon">
