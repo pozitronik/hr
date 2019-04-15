ecd1d2f94a4df111d2760ed511d527bd8583d181
Pozitronik <antikillerxxl@gmail.com>
2019-04-15 12:57:17 +0300

    all unrelated stuff moved to CoreController

--- models/core/WigetableController.php
+++ models/core/WigetableController.php
@@ -3,18 +3,6 @@ declare(strict_types = 1);
 
 namespace app\models\core;
 
-use app\helpers\Path;
-use app\models\core\core_module\PluginsSupport;
-use app\models\core\helpers\ReflectionHelper;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use ReflectionClass;
-use ReflectionException;
-use Throwable;
-use Yii;
-use yii\base\InvalidConfigException;
-use yii\base\UnknownClassException;
-
 /**
  * Class WigetableController
  * Расширенный класс контроллера с дополнительными опциями встройки в меню и навигацию
@@ -43,83 +31,6 @@ class WigetableController extends CoreController {
 	 */
 	public function getMenuCaption() {
 		return false;
-	}
-
-	/**
-	 * Вытаскивает из имени класса контроллера его id
-	 * app/shit/BlaBlaBlaController => bla-bla-bla
-	 * @param string $className
-	 * @return string
-	 */
-	private static function ExtractControllerId(string $className):string {
-		$controllerName = preg_replace('/(^.+)(\\\)([A-Z].+)(Controller$)/', '$3', $className);//app/shit/BlaBlaBlaController => BlaBlaBla
-		return mb_strtolower(implode('-', preg_split('/([[:upper:]][[:lower:]]+)/', $controllerName, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY)));
-	}
-
-	/**
-	 * Загружает динамически класс веб-контроллера Yii2 по его пути
-	 * @param string $fileName
-	 * @param string|null $moduleId
-	 * @return self|null
-	 * @throws InvalidConfigException
-	 * @throws ReflectionException
-	 * @throws Throwable
-	 * @throws UnknownClassException
-	 */
-	public static function GetController(string $fileName, ?string $moduleId):?object {
-		$className = ReflectionHelper::ExtractNamespaceFromFile($fileName).'\\'.Path::ChangeFileExtension($fileName);
-		if (!class_exists($className)) Yii::autoload($className);
-		$class = new ReflectionClass($className);//todo: find all Reflections => move to helper
-		if ($class->isSubclassOf(__CLASS__)) {
-			if (null === $moduleId) {
-				$module = Yii::$app;
-			} else {
-				$module = PluginsSupport::GetPluginById($moduleId);
-				if (null === $module) throw new InvalidConfigException("Module $moduleId not found or plugin not configured properly.");
-			}
-			return new $className(self::ExtractControllerId($className), $module);
-		}
-		return null;
-	}
-
-	/**
-	 * Загружает динамически класс веб-контроллера Yii2 по его id и модулю
-	 * @param string $controllerId
-	 * @param string|null $moduleId
-	 * @return self|null
-	 * @throws InvalidConfigException
-	 * @throws ReflectionException
-	 * @throws Throwable
-	 * @throws UnknownClassException
-	 */
-	public static function GetControllerByControllerId(string $controllerId, ?string $moduleId):?object {
-		if (null === $plugin = PluginsSupport::GetPluginById($moduleId)) throw new InvalidConfigException("Module $moduleId not found or plugin not configured properly.");
-		$controllerId = implode('', array_map('ucfirst', preg_split('/-/', $controllerId, -1, PREG_SPLIT_NO_EMPTY)));
-		return self::GetController("{$plugin->controllerPath}/{$controllerId}Controller.php", $moduleId);
-
-	}
-
-	/**
-	 * Выгружает список контроллеров в указанном неймспейсе
-	 * @param string $path
-	 * @param string|null $moduleId
-	 * @return self[]
-	 * @throws InvalidConfigException
-	 * @throws ReflectionException
-	 * @throws Throwable
-	 * @throws UnknownClassException
-	 */
-	public static function GetControllersList(string $path, ?string $moduleId = null):array {
-		$result = [];
-
-		$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(Yii::getAlias($path)), RecursiveIteratorIterator::SELF_FIRST);
-		/** @var RecursiveDirectoryIterator $file */
-		foreach ($files as $file) {
-			if ($file->isFile() && 'php' === $file->getExtension() && null !== $controller = self::GetController($file->getRealPath(), $moduleId)) {
-				$result[] = $controller;
-			}
-		}
-		return $result;
 	}
 
 	/**
