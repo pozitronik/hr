795efa88944e7ed73c158a21337fad2bf8141a88
Pozitronik <antikillerxxl@gmail.com>
2019-04-23 10:14:32 +0300

    Визуал фильтров

--- modules/vacancy/views/vacancy/index.php
+++ modules/vacancy/views/vacancy/index.php
@@ -7,8 +7,14 @@ declare(strict_types = 1);
  * @var ActiveDataProvider $dataProvider
  */
 
+use app\helpers\ArrayHelper;
 use app\helpers\Icons;
 use app\helpers\Utils;
+use app\modules\references\widgets\reference_select\ReferenceSelectWidget;
+use app\modules\salary\models\references\RefLocations;
+use app\modules\salary\models\references\RefUserPositions;
+use app\modules\vacancy\models\references\RefVacancyRecruiters;
+use app\modules\vacancy\models\references\RefVacancyStatuses;
 use app\modules\vacancy\models\Vacancy;
 use app\modules\vacancy\models\VacancySearch;
 use app\modules\vacancy\widgets\navigation_menu\VacancyNavigationMenuWidget;
@@ -39,7 +45,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	'responsive' => true,
 	'formatter' => [
 		'class' => Formatter::class,
-		'nullDisplay' => ''
+//		'nullDisplay' => ''
 	],
 	'columns' => [
 		[
@@ -66,42 +72,100 @@ $this->params['breadcrumbs'][] = $this->title;
 			'attribute' => 'id'
 		],
 		[
-			'attribute' => 'vacancy_id'
-		],
-		[
-			'attribute' => 'ticket_id'
-		],
-		[
+			'class' => DataColumn::class,
 			'attribute' => 'status',
-			'value' => 'relRefVacancyStatus.name'
+			'value' => 'relRefVacancyStatus.name',
+			'filter' => ArrayHelper::getValue($searchModel, 'status'),
+			'filterType' => ReferenceSelectWidget::class,
+			'filterInputOptions' => ['placeholder' => 'Выберите статус'],
+			'filterWidgetOptions' => [
+				'referenceClass' => RefVacancyStatuses::class,
+				'pluginOptions' => ['allowClear' => true, 'multiple' => true]
+			]
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'group',
 			'value' => 'relGroup.name'
+
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'location',
-			'value' => 'relRefLocation.name'
+			'value' => 'relRefLocation.name',
+			'filter' => ArrayHelper::getValue($searchModel, 'location'),
+			'filterType' => ReferenceSelectWidget::class,
+			'filterInputOptions' => ['placeholder' => 'Выберите локацию'],
+			'filterWidgetOptions' => [
+				'referenceClass' => RefLocations::class,
+				'pluginOptions' => ['allowClear' => true, 'multiple' => true]
+			]
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'recruiter',
-			'value' => 'relRefVacancyRecruiter.name'
+			'value' => 'relRefVacancyRecruiter.name',
+			'filter' => ArrayHelper::getValue($searchModel, 'recruiter'),
+			'filterType' => ReferenceSelectWidget::class,
+			'filterInputOptions' => ['placeholder' => 'Выберите рекрутера'],
+			'filterWidgetOptions' => [
+				'referenceClass' => RefVacancyRecruiters::class,
+				'pluginOptions' => ['allowClear' => true, 'multiple' => true]
+			]
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'position',
-			'value' => 'relRefUserPosition.name'
+			'value' => 'relRefUserPosition.name',
+			'filter' => ArrayHelper::getValue($searchModel, 'position'),
+			'filterType' => ReferenceSelectWidget::class,
+			'filterInputOptions' => ['placeholder' => 'Выберите должность'],
+			'filterWidgetOptions' => [
+				'referenceClass' => RefUserPositions::class,
+				'pluginOptions' => ['allowClear' => true, 'multiple' => true]
+			]
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'role'
 		],
 		[
+			'class' => DataColumn::class,
 			'attribute' => 'teamlead'
 		],
 		[
-			'attribute' => 'create_date'
+			'class' => DataColumn::class,
+			'attribute' => 'create_date',
+			'filterType' => GridView::FILTER_DATE_RANGE,
+			'filterWidgetOptions' => [
+				'pluginOptions' => [
+					'locale' => [
+						'format' => 'DD.MM.YYYY',
+						'separator' => ' по '
+					],
+					'autoclose' => true,
+					'format' => 'DD.MM.YYYY',
+					'separator' => ' по ',
+					'alwaysShowCalendars' => true
+				]
+			]
 		],
 		[
-			'attribute' => 'estimated_close_date'
+			'class' => DataColumn::class,
+			'attribute' => 'estimated_close_date',
+			'filterType' => GridView::FILTER_DATE_RANGE,
+			'filterWidgetOptions' => [
+				'pluginOptions' => [
+					'locale' => [
+						'format' => 'DD.MM.YYYY',
+						'separator' => ' по '
+					],
+					'autoclose' => true,
+					'format' => 'DD.MM.YYYY',
+					'separator' => ' по ',
+					'alwaysShowCalendars' => true
+				]
+			]
 		]
 	]
 ]) ?>
