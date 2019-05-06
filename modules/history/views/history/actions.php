18d4dc401283be0d33c4f9e238bda1d7b83e2c7d
Pozitronik <antikillerxxl@gmail.com>
2019-05-06 09:42:25 +0300

    Show json serialized data for array attribute changes (fixes GridView render error)

--- modules/history/views/history/actions.php
+++ modules/history/views/history/actions.php
@@ -41,11 +41,17 @@ use yii\i18n\Formatter;
 		[
 			'class' => DataColumn::class,
 			'attribute' => 'attributeOldValue',
+			'value' => static function(HistoryEventAction $model) {
+				return is_array($model->attributeOldValue)?json_encode($model->attributeOldValue, JSON_PRETTY_PRINT):$model->attributeOldValue;
+			},
 			'width' => '25%'
 		],
 		[
 			'class' => DataColumn::class,
 			'attribute' => 'attributeNewValue',
+			'value' => static function(HistoryEventAction $model) {
+				return is_array($model->attributeNewValue)?json_encode($model->attributeNewValue, JSON_PRETTY_PRINT):$model->attributeNewValue;
+			},
 			'width' => '25%'
 		]
 	]
