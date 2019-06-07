9ac9153b021643e0ec3259a34b067aca5b2853d2
Pozitronik <Pozitronik>
2019-06-07 13:43:15 +0300

    Рефакторим черновой код рисования структуры

--- modules/groups/widgets/structure/assets/js/tree_init.js
+++ modules/groups/widgets/structure/assets/js/tree_init.js
@@ -1,43 +1,95 @@
+const positionNone = 0, //не позиционировать ноды на сервере
+	positionRound = 1;// позиционировать в круговую диаграмму
+
+/**
+ * Загружает набор нод для группы
+ * @param int groupId
+ * @param int positionMode
+ * @return object
+ */
+function load_group_graph(groupId, positionMode = positionNone) {
 
 
-init_tree = function (id) {
-	var data = {};
-	json('/groups/ajax/groups-tree?id=' + id, data, function (tree_data) {
-		var network = new vis.Network(_.$('tree-container'), tree_data, {
-			locale: 'ru',
-			layout: {
-				randomSeed: undefined,
-				improvedLayout: true,
-				hierarchical: {
-					direction: "UD",
-					enabled: true,
-					levelSeparation: 200,
-					nodeSpacing: 200,
-					treeSpacing: 200,
-					blockShifting: true,
-					edgeMinimization: true,
-					parentCentralization: true,
-					sortMethod: 'directed'   // hubsize, directed
+}
 
-				}
-			},
-			interaction: {dragNodes: true},
-			physics: {
-				enabled: false
-			},
-			configure: {
-				container: _.$('controls-block'),
+/**
+ * Загружает набор параметров конфигурации графа
+ * @param string configName
+ * @return object
+ */
+function load_graph_options(configName = 'default') {/*todo*/
+	return {
+		locale: 'ru',
+		layout: {
+			randomSeed: undefined,
+			improvedLayout: true,
+			hierarchical: {
+				direction: "UD",
 				enabled: true,
-				filter: function (option, path) {
-					if (path.indexOf('hierarchical') !== -1) {
-						return true;
-					}
-					return true;
-				},
-				showButton: true
+				levelSeparation: 200,
+				nodeSpacing: 200,
+				treeSpacing: 200,
+				blockShifting: true,
+				edgeMinimization: true,
+				parentCentralization: true,
+				sortMethod: 'directed'   // hubsize, directed
+
 			}
-		});
-	});
+		},
+		interaction: {dragNodes: true},
+		physics: {
+			enabled: false
+		},
+		configure: {
+			container: _.$('controls-block'),
+			enabled: true,
+			filter: function(option, path) {
+				if (path.indexOf('hierarchical') !== -1) {
+					return true;
+				}
+				return true;
+			},
+			showButton: true
+		}
+	}
+}
+
+/**
+ * Загружает сохранённый набор координат нод по имени конфига
+ * @param string configName
+ */
+function load_nodes_positions(configName = 'default') {
+
+}
+
+/**
+ * Сохраняет набор нод в конфиг
+ * @param array nodes
+ * @param string configName
+ */
+function save_nodes_positions(nodes, configName = 'default') {
+
+}
+
+/**
+ * Сохраняет координаты одной ноды в конфиге
+ * @param object node
+ * @param string configName
+ */
+function save_node_position(node, configName = 'default') {
+
+}
+
+init_tree = function(groupId) {
+	var nodes,
+		options = load_graph_options();
+
+
+	getJSON('/groups/ajax/groups-tree?id=' + groupId).then(
+		response => network = new vis.Network(_.$('tree-container'), response, options),
+		error => console.log(error)
+	)
+
 
 	_.$('toggle-controls').onclick = function click() {
 		_.toggle('#controls-block', 'min');
