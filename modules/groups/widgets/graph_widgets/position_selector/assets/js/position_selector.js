c542248d0abe664ca27725c75403a4d1cd7f0ed1
Pozitronik <Pozitronik>
2019-06-10 13:37:35 +0300

    Обвязка на кнопку сохранения конфига

---
+++ modules/groups/widgets/graph_widgets/position_selector/assets/js/position_selector.js
@@ -0,0 +1,11 @@
+/**
+ * Сохраняем пользовательский фильтр
+ */
+$('.js-save-user-position-config').on('click', function () {
+	var configName = $("#position-configName").val();
+	if (isEmpty(configName)) {
+		$.notify('Не заполнено имя карты', {type: "warning"});
+	} else {
+		save_nodes_positions(null, null, configName);
+	}
+});
