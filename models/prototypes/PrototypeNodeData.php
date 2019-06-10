8978e7fd57980a722bff9d2902f7866b70444c7d
Pozitronik <Pozitronik>
2019-06-10 12:54:52 +0300

    Name property

--- models/prototypes/PrototypeNodeData.php
+++ models/prototypes/PrototypeNodeData.php
@@ -8,6 +8,7 @@ use yii\base\Model;
 /**
  * Class PrototypeNodeData
  *
+ * @property string $name -- configuration name
  * @property integer $groupId
  * @property integer $nodeId
  * @property integer $x
@@ -15,6 +16,7 @@ use yii\base\Model;
  */
 class PrototypeNodeData extends Model {
 
+	public $name;
 	public $groupId;
 	public $nodeId;
 	public $x;
@@ -25,6 +27,7 @@ class PrototypeNodeData extends Model {
 	 */
 	public function rules():array {
 		return [
+			[['name'], 'string'],
 			[['groupId', 'nodeId', 'userId'], 'integer'],
 			[['x', 'y'], 'number'],
 			[['groupId', 'nodeId', 'x', 'y'], 'required']
