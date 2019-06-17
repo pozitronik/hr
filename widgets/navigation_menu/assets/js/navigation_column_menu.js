9fde8238794fc8ddeb8eea51c3ed8a814c412275
Pozitronik <Pozitronik>
2019-06-17 16:36:18 +0300

    Небольшая оптимизация по результатам инспекции

--- widgets/navigation_menu/assets/js/navigation_column_menu.js
+++ widgets/navigation_menu/assets/js/navigation_column_menu.js
@@ -1,14 +1,11 @@
-jQuery(function ($) {
-	$('table.table * .dropdown-menu').parent().on('shown.bs.dropdown', function () {
-		var menu = jQuery(this).find('.dropdown-menu');
-		var position = menu.offset();
+jQuery(function($) {
+	$('table.table * .dropdown-menu').parent().on('shown.bs.dropdown', function() {
+		let menu = jQuery(this).find('.dropdown-menu');
+		let position = menu.offset();
 		position.top -= document.documentElement.scrollTop;
 		position.left -= document.documentElement.scrollLeft;
 		menu.css('position', 'fixed').css(position);
-	});
-	$('table.table * .dropdown-menu').parent().on('hidden.bs.dropdown', function () {
-		var menu = jQuery(this).find('.dropdown-menu');
-		var position = menu.offset();
-		menu.css('position', 'absolute').css('left', '0px').css('top', '0px');
+	}).parent().on('hidden.bs.dropdown', function() {
+		jQuery(this).find('.dropdown-menu').css('position', 'absolute').css('left', '0px').css('top', '0px');
 	});
 });
