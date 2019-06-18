2972d171d1c52986fd7d8f653e8b357e7893bfe8
Pozitronik <Pozitronik>
2019-06-18 11:50:37 +0300

    Добавлен вывод ошибок импорта по завершению опреации

--- modules/import/views/fos/import.php
+++ modules/import/views/fos/import.php
@@ -5,8 +5,10 @@ declare(strict_types = 1);
  * @var View $this
  * @var int $step
  * @var int $domain
+ * @var array $errors
  */
 
+use app\helpers\Utils;
 use app\modules\import\models\fos\ImportFosDecomposed;
 use yii\helpers\Html;
 use yii\web\View;
@@ -20,14 +22,26 @@ use yii\web\View;
 	<div class="panel-body">
 		<div class="row">
 			<div class="col-md-12">
-				Импорт разбит на шаги из-за продолжительности, ошибки пока не анализируются
+				Импорт разбит на шаги из-за продолжительности.
 			</div>
+			<?php if ([] !== $errors): ?>
+				<div class="col-md-12">
+					<?= Html::label('Ошибки при импорте:') ?>
+					<?php Utils::log($errors); ?>
+				</div>
+			<?php else: ?>
+				<?= Html::label('Нет ошибок') ?>
+			<?php endif; ?>
 		</div>
 
 	</div>
 	<div class="panel-footer">
 		<?= Html::a('Повторить', ['import', 'step' => $step, 'domain' => $domain], ['class' => 'btn btn-warning pull-left']) ?>
-		<?= ($step !== ImportFosDecomposed::LAST_STEP)?Html::a('Следующий шаг', ['import', 'step' => $step + 1, 'domain' => $domain], ['class' => 'btn btn-success pull-right']):Html::a('Готово, домой', ['/home/index', 'domain' => $domain], ['class' => 'btn btn-success pull-right']) ?>
+		<?php if ($step !== ImportFosDecomposed::LAST_STEP): ?>
+			<?= Html::a('Следующий шаг', ['import', 'step' => $step + 1, 'domain' => $domain], ['class' => 'btn btn-success pull-right']); ?>
+		<?php else: ?>
+			<?= Html::a('Готово, домой', ['/home/index', 'domain' => $domain], ['class' => 'btn btn-success pull-right']); ?>
+		<?php endif; ?>
 		<div class="clearfix"></div>
 	</div>
 </div>
