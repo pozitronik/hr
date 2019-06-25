1a7ce3496d0e69bd82526442218a686a6b4cd783
Pozitronik <Pozitronik>
2019-06-25 10:04:59 +0300

    Да бял

--- migrations/m190625_064744_sys_user_position_type_override.php
+++ migrations/m190625_064744_sys_user_position_type_override.php
@@ -10,15 +10,15 @@ class m190625_064744_sys_user_position_type_override extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->createTable('rel_user_position_type', [
+		$this->createTable('rel_user_position_types', [
 			'id' => $this->primaryKey(),
 			'user_id' => $this->integer()->notNull()->comment('Пользователь'),
 			'position_type_id' => $this->integer()->notNull()->comment('Тип должности'),
 		]);
 
-		$this->createIndex('user_id', 'rel_user_position_type', 'user_id');
-		$this->createIndex('position_type_id', 'rel_user_position_type', 'position_type_id');
-		$this->createIndex('user_id_position_type_id', 'rel_user_position_type', [
+		$this->createIndex('user_id', 'rel_user_position_types', 'user_id');
+		$this->createIndex('position_type_id', 'rel_user_position_types', 'position_type_id');
+		$this->createIndex('user_id_position_type_id', 'rel_user_position_types', [
 			'user_id',
 			'position_type_id'
 		], true);
@@ -28,7 +28,7 @@ class m190625_064744_sys_user_position_type_override extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeDown() {
-		$this->dropTable('rel_user_position_type');
+		$this->dropTable('rel_user_position_types');
 	}
 
 	/*
