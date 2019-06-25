49d80a2b9cf389a0bcd099a71a39ea11b0ba7cc3
Pozitronik <Pozitronik>
2019-06-25 10:05:55 +0300

    Моделька для таблички переопределний типов должностей

---
+++ modules/users/models/relations/RelUserPositionsTypes.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\salary\models\relations;
+
+use app\models\core\ActiveRecordExtended;
+use app\models\relations\Relations;
+
+/**
+ * Class RelRefUserPositionsTypes
+ * @package app\modules\references\models\relations
+ */
+class RelUserPositionsTypes extends ActiveRecordExtended {
+	use Relations;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function tableName():string {
+		return 'rel_user_position_type';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['user_id', 'position_type_id'], 'required'],
+			[['user_id', 'position_type_id'], 'integer'],
+			[['user_id', 'position_type_id'], 'unique', 'targetAttribute' => ['user_id', 'position_type_id']]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'position_id' => 'Пользователь',
+			'position_type_id' => 'Тип должности'
+		];
+	}
+
+}
