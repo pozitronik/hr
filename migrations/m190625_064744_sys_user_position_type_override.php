0ce87e37d96fbfa4a3c99b8178c358234b0c4f3c
Pozitronik <Pozitronik>
2019-06-25 09:58:35 +0300

    Накосячил с миграцией (сегодня я буду много косячить)

--- migrations/m190625_064744_sys_user_position_type_override.php
+++ migrations/m190625_064744_sys_user_position_type_override.php
@@ -10,21 +10,21 @@ class m190625_064744_sys_user_position_type_override extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->createTable('sys_users_position_type_override', [
+		$this->createTable('rel_users_position_type', [
 			'id' => $this->primaryKey(),
 			'user_id' => $this->integer()->notNull()->comment('Пользователь'),
 			'position_type_id' => $this->integer()->notNull()->comment('Тип должности'),
 		]);
 
-		$this->createIndex('user_id', 'sys_users_position_type_override', 'user_id');
-		$this->createIndex('position_type_id', 'sys_users_position_type_override', 'position_type_id');
+		$this->createIndex('user_id', 'rel_users_position_type', 'user_id');
+		$this->createIndex('position_type_id', 'rel_users_position_type', 'position_type_id');
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeDown() {
-		$this->dropTable('sys_users_position_type_override');
+		$this->dropTable('sys_users_position_type');
 	}
 
 	/*
