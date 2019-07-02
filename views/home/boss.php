16738a19a13d890317a6680b2a3ac1171ab6883c
Pozitronik <Pozitronik>
2019-07-02 15:15:08 +0300

    Вьюха переделана на работу через ActiveDataProvuder

--- views/home/boss.php
+++ views/home/boss.php
@@ -3,17 +3,29 @@ declare(strict_types = 1);
 
 /**
  * @var View $this
- * @var Groups[] $groups
+ * @var GroupsSearch $searchModel
+ * @var ActiveDataProvider $dataProvider
  */
 
-use app\modules\groups\models\Groups;
+use app\modules\groups\models\GroupsSearch;
 use app\widgets\group_card\GroupCardWidget;
+use yii\bootstrap\Html;
+use yii\data\ActiveDataProvider;
+use yii\helpers\Url;
 use yii\web\View;
 $this->title = 'Мои группы';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?php foreach ($groups as $group): ?>
+<div class="panel">
+	<div class="panel-heading">
+		<div class="panel-control">
+		</div>
+		<h3 class="panel-title"><?= Html::encode($this->title) ?><?= Html::a("<div class='pull-right'>Таблица</div>", Url::current(['t' => 1])) ?></h3>
+	</div>
+</div>
+
+<?php foreach ($dataProvider->models as $group): ?>
 	<?= GroupCardWidget::widget([
 		'group' => $group
 	]) ?>
