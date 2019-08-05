2da86683084e6469ecd44265e02f42dbeccb796b
Pozitronik <Pozitronik>
2019-08-05 11:43:31 +0300

    Уносим поиск

--- views/home/boss.php
+++ views/home/boss.php
@@ -7,10 +7,8 @@ declare(strict_types = 1);
  * @var ActiveDataProvider $dataProvider
  */
 
-use app\modules\groups\GroupsModule;
 use app\modules\groups\models\GroupsSearch;
 use app\widgets\group_card\GroupCardWidget;
-use kartik\typeahead\Typeahead;
 use yii\bootstrap\Html;
 use yii\data\ActiveDataProvider;
 use yii\helpers\Url;
@@ -33,37 +31,7 @@ $dataProvider->pagination = false;
 		</h3>
 	</div>
 </div>
-<?= Typeahead::widget([
-	'name' => 'search',
-	'options' => ['placeholder' => 'Поиск в группах'],
-	'pluginOptions' => ['highlight' => true],
-	'pluginEvents' => [
-		"typeahead:select" => "function(e, o) {
-				let url;
-				switch (o.type) {
-				case 'user':
-					url = '/users/users/profile?id='+o.id;
-				break;
-				case 'group':
-					url = 'users?UsersSearch[groupId]='+o.id;
-				break'
-			}
-			window.open(url,'_blank');
-		}",
-	],
-	'dataset' => [
-		[
-			'datumTokenizer' => "Bloodhound.tokenizers.obj.whitespace('name')",
-			'display' => 'name',
-			'remote' => [
-				'url' => GroupsModule::to(['ajax/search']).'?term=%QUERY',
-				'wildcard' => '%QUERY'
-			]
-		]
-	]
-]) ?>
 <?php foreach ($dataProvider->models as $group): ?>
 	<?= GroupCardWidget::widget(['group' => $group]) ?>
-
 <?php endforeach; ?>
 
