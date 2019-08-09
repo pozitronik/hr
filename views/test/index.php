577857eabd55c534ec942caa0cd67a5924060ba8
Pozitronik <Pozitronik>
2019-08-09 15:11:37 +0300

    Тестовый код

--- views/test/index.php
+++ views/test/index.php
@@ -3,20 +3,10 @@ declare(strict_types = 1);
 
 /* @var View $this */
 
+use app\modules\groups\models\Groups;
+use app\widgets\group_card\GroupCardWidget;
 use yii\web\View;
 
 ?>
 
-<div class="panel panel-primary">
-	<div class="panel-heading">
-		<h3 class="panel-title">Заголовок</h3>
-	</div>
-
-	<div class="panel-body">
-		<div class="row">
-		</div>
-		<div class="col-md-6">
-		</div>
-	</div>
-
-</div>
+<?= GroupCardWidget::widget(['group' => Groups::findModel(7)]) ?>
