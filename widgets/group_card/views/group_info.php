8a29f5aefc651ee9902458388d5c8dfc65871972
Pozitronik <Pozitronik>
2019-08-15 17:34:08 +0300

    Я добавил виджет в виджет, чтобы ты мог использовать виджет, пока используешь виджет

--- widgets/group_card/views/group_info.php
+++ widgets/group_card/views/group_info.php
@@ -7,44 +7,14 @@ declare(strict_types = 1);
  */
 
 use app\modules\groups\models\Groups;
-use app\modules\salary\models\references\RefUserPositionTypes;
 use app\modules\users\models\references\RefUserRoles;
 use app\modules\users\UsersModule;
-use app\modules\vacancy\VacancyModule;
 use app\widgets\badge\BadgeWidget;
+use app\widgets\group_card\GroupCardWidget;
 use yii\web\View;
 
 ?>
-<?= BadgeWidget::widget([
-	'models' => "Всего: ".count($group->relUsers),
-	"badgeOptions" => [
-		'class' => "badge badge-info"
-	],
-	'linkScheme' => ['users', 'UsersSearch[groupId]' => $group->id]
-
-]) ?>
-
-<?php foreach ($group->getGroupPositionTypeData() as $positionId => $positionCount): ?>
-	<?php /** @var RefUserPositionTypes $positionType */
-	$positionType = RefUserPositionTypes::findModel($positionId); ?>
-	<?= BadgeWidget::widget([
-		'models' => "{$positionType->name}: {$positionCount}",
-		"badgeOptions" => [
-			'style' => "background: {$positionType->color}; color: $positionType->textcolor"
-		],
-		'linkScheme' => ['users', 'UsersSearch[positionType]' => $positionId, 'UsersSearch[groupId]' => $group->id]
-
-	]) ?>
-
-<?php endforeach; ?>
-
-<?= BadgeWidget::widget([
-	'models' => "Вакансии: ".count($group->relVacancy),
-	"badgeOptions" => [
-		'class' => "badge badge-danger"
-	],
-	'linkScheme' => [VacancyModule::to('groups'), 'id' => $group->id]
-]) ?>
+<?= GroupCardWidget::widget(['group' => $group, 'view' => 'group_users', 'options' => ['column_view' => false]]); ?>
 
 <?= BadgeWidget::widget([
 	'models' => static function() use ($group) {
