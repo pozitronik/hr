30320869c19d134d22a1e31f908b44dbe4888df9
Pozitronik <Pozitronik>
2019-08-19 10:17:39 +0300

    Напилен виджет кнопок управления (черновой вариант)

--- widgets/button_controls/ButtonControlsWidget.php
+++ widgets/button_controls/ButtonControlsWidget.php
@@ -8,8 +8,21 @@ use pozitronik\widgets\CachedWidget;
 /**
  * Class ButtonControlsWidget
  * @package app\widgets\button_controls
+ * @property bool $radioMode -- если установлено, используем радиокнопки (только одна может быть нажата)
+ * @property string $name -- имя блока
+ * @property string|array|null $selection -- the selected value(s). String for single or array for multiple selection(s).
+ * @property array $options - массив html-опций контейнера
+ * @property array $items - массив кнопок, описываемых в формате:
+ *    'value' => значение кнопки,
+ *    'label' => подпись кнопки,
+ *    'options' => массив html-опций для кнопки
  */
 class ButtonControlsWidget extends CachedWidget {
+	public $radioMode = false;
+	public $name;
+	public $selection;
+	public $options = [];
+	public $items = [];
 
 	/**
 	 * Функция инициализации и нормализации свойств виджета
@@ -24,6 +37,12 @@ class ButtonControlsWidget extends CachedWidget {
 	 * @return string
 	 */
 	public function run():string {
-		return $this->render('button_controls');
+		return $this->render($this->radioMode?'radio':'checkbox', [
+			'name' => $this->name,
+			'selection' => $this->selection,
+			'options' => $this->options,
+			'items' => $this->items
+		]);
+
 	}
 }
---
+++ widgets/button_controls/views/radio.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var string $name
+ * @var array|string|null $selection
+ * @var array $options
+ * @var array $items
+ */
+
+use yii\bootstrap\Html;
+use yii\web\View;
+
+?>
+
+<?= Html::radioList($name, null, $items, $options + [
+		'item' => function($index, $label, $name, $checked, $value) {
+			return Html::input('radio', $name, $value, ['id' => $value, 'class' => 'hidden']).Html::label($label, $value, ['class' => "button $value"]);
+		},
+
+	]) ?>
+
