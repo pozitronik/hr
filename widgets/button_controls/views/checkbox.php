0c3653c65051ac15b5122379ee4d98d9302a5c14
Pozitronik <Pozitronik>
2019-08-20 10:39:52 +0300

    В этой вьюхе также подерживаем передачу параметров каждого элемента массивом

--- widgets/button_controls/views/checkbox.php
+++ widgets/button_controls/views/checkbox.php
@@ -9,6 +9,7 @@ declare(strict_types = 1);
  * @var array $items
  */
 
+use pozitronik\helpers\ArrayHelper;
 use yii\bootstrap\Html;
 use yii\web\View;
 
@@ -16,7 +17,13 @@ use yii\web\View;
 
 <?= Html::checkboxList($name, $selection, $items, $options + [
 		'item' => static function($index, $label, $name, $checked, $value) {
-			return Html::input('checkbox', $name, $value, ['id' => $value, 'class' => 'hidden']).Html::label($label, $value, ['class' => "button $value"]);
+			$inputOptions = [];
+			if (is_array($label)) {//если будем добавлять другие параметры, они пойдут сюда
+				$inputOptions = ArrayHelper::getValue($label, 'options', []);
+				$value = ArrayHelper::getValue($label, 'value');
+				$label = ArrayHelper::getValue($label, 'label');
+			}
+			return Html::input('checkbox', $name, $value, ['id' => $value, 'class' => 'hidden'] + $inputOptions).Html::label($label, $value, ['class' => "button $value"]);
 		}
 	]) ?>
 
