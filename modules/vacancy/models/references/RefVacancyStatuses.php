4361a3a28cc86d0ff9c3959e765aa807ed3a9cd4
Pozitronik <Pozitronik>
2019-08-21 16:26:50 +0300

    Реляционные атрибуты для связи с группами и вакансиями + псевдоаттрибут для расчёта среза

--- modules/vacancy/models/references/RefVacancyStatuses.php
+++ modules/vacancy/models/references/RefVacancyStatuses.php
@@ -3,17 +3,22 @@ declare(strict_types = 1);
 
 namespace app\modules\vacancy\models\references;
 
-
+use app\modules\groups\models\Groups;
 use app\modules\references\models\CustomisableReference;
+use app\modules\vacancy\models\Vacancy;
+use yii\db\ActiveQuery;
 
 /**
  * Class RefVacancyStatuses
  * @package app\modules\salary\models\vacancy
+ * @property Vacancy[]|ActiveQuery $relVacancy
+ * @property Groups[]|ActiveQuery $relGroups
  */
 class RefVacancyStatuses extends CustomisableReference {
 	public $menuCaption = 'Статусы вакансий';
 	public $menuIcon = false;
 
+	public $count = 0;//Псевдоаттрибут, заполняется при подсчёте среза по статусам вакансий
 
 	/**
 	 * {@inheritdoc}
@@ -21,5 +26,17 @@ class RefVacancyStatuses extends CustomisableReference {
 	public static function tableName():string {
 		return 'ref_vacancy_statuses';
 	}
+	/**
+	 * @return Vacancy[]|ActiveQuery
+	 */
+	public function getRelVacancy() {
+		return $this->hasMany(Vacancy::class, ['status' => 'id']);
+	}
 
+	/**
+	 * @return Groups[]|ActiveQuery
+	 */
+	public function getRelGroups() {
+		return $this->hasMany(Groups::class, ['id' => 'group'])->via('relVacancy');
+	}
 }
