ad4a0f21a3edc094a109c54dce59197e771660cc
Pozitronik <Pozitronik>
2019-08-27 10:54:08 +0300

    Пустая миграция применилась, попадает в индекс (

---
+++ migrations/m190821_140834_add_textcolor_indexes.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190821_140834_add_textcolor_indexes
+ */
+class m190821_140834_add_textcolor_indexes extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190821_140834_add_textcolor_indexes cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190821_140834_add_textcolor_indexes cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
