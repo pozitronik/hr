88d28c078cdf7326a89754326ece1e0f0141d705
Pozitronik <Pozitronik>
2019-08-29 10:51:57 +0300

    Миграция на собственный тип должности юзера

---
+++ migrations/m190829_074824_user_position_type.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190829_074824_user_position_type
+ */
+class m190829_074824_user_position_type extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->addColumn('sys_users', 'position_type', $this->integer()->null()->defaultValue(null)->after('position')->comment('Прямое определение типа позиции пользователя'));
+		$this->createIndex('position_type', 'sys_users', 'position_type');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropColumn('sys_users', 'position_type');
+	}
+
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m190829_074824_user_position_type cannot be reverted.\n";
+
+		return false;
+	}
+	*/
+}
