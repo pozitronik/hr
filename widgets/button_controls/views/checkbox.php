651d69f56b1ca6da3bbd3d4312f9345497bf2b0a
Pozitronik <Pozitronik>
2019-09-20 18:24:22 +0300

    Стиль леблокнопок в виджете перехватывается от стиля чекбоксов

--- widgets/button_controls/views/checkbox.php
+++ widgets/button_controls/views/checkbox.php
@@ -16,14 +16,15 @@ use yii\web\View;
 ?>
 
 <?= Html::checkboxList($name, $selection, $items, $options + [
-		'item' => static function($index, $label, $name, $checked, $value) {
+		'item' => static function($index, $label, $name, $checked, $value) use ($options) {
 			$inputOptions = [];
 			if (is_array($label)) {//если будем добавлять другие параметры, они пойдут сюда
 				$inputOptions = ArrayHelper::getValue($label, 'options', []);
 				$value = ArrayHelper::getValue($label, 'value', sha1(ArrayHelper::getValue($label, 'label')));
 				$label = ArrayHelper::getValue($label, 'label');
 			}
-			return Html::input('checkbox', $name, $value, ['id' => $value, 'class' => 'hidden'] + $inputOptions).Html::label($label, $value, ['class' => "button $value"]);
+
+			return Html::input('checkbox', $name, $value, ['id' => $value, 'class' => 'hidden'] + $inputOptions).Html::label($label, $value, ['class' => "button $value", 'style' => ArrayHelper::getValue($inputOptions, 'style')]);
 		}
 	]) ?>
 
