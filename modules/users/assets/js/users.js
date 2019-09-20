ff699928e890a2607e237a610f18f15218400d77
Pozitronik <Pozitronik>
2019-09-20 13:59:15 +0300

    url fixed

--- modules/users/assets/js/users.js
+++ modules/users/assets/js/users.js
@@ -1,12 +1,12 @@
 function set_option(key, value) {
 	jQuery.ajax({
-		url: '/users/ajax/user-set-bookmark',
+		url: '/users/ajax/user-set-option',
 		data: {
 			key: key,
 			value: value,
 		},
 		method: 'POST'
 	}).done(function (data) {
-
+		console.log(data);
 	});
 }
