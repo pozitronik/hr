0c72b6cbba082eb14ffb8a3415b0fcaa917b54b2
Pozitronik <Pozitronik>
2019-09-20 15:03:54 +0300

    debug

--- modules/home/views/home/dashboard.php
+++ modules/home/views/home/dashboard.php
@@ -11,6 +11,7 @@ declare(strict_types = 1);
 
 use app\assets\IsotopeAsset;
 use app\assets\MasonryAsset;
+use app\helpers\Utils;
 use app\models\user\CurrentUser;
 use app\modules\groups\models\GroupsSearch;
 use app\modules\groups\models\references\RefGroupTypes;
@@ -37,6 +38,7 @@ $this->registerJs("Controls.refresh()", View::POS_END);
 
 $userGroupTypes = RefGroupTypes::getGroupsTypesScope(ArrayHelper::getColumn($dataProvider->models, 'type'));
 $userDashboardFilter = CurrentUser::User()->options->get('dashboardFilter');
+Utils::log($userDashboardFilter);
 array_walk($userGroupTypes, static function(&$value, &$key) use ($userDashboardFilter) {
 	$key = "filter-type{$value['id']}";
 	$value = [
