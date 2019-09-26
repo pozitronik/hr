646e6be37096308d33ac3c657a628fdf33a8b363
Pozitronik <Pozitronik>
2019-09-26 11:57:29 +0300

    Переименован параметр

--- modules/groups/widgets/group_card/views/group_small.php
+++ modules/groups/widgets/group_card/views/group_small.php
@@ -24,7 +24,7 @@ use yii\web\View;
 	<div class="panel-heading">
 		<?php if (ArrayHelper::getValue($options, 'showChildGroups', true) && $group->getChildGroupsCount() > 0): ?>
 			<div class="panel-control">
-				<?= $this->render('control_block', ['target' => "childGroups-{$group->id}", 'expanded' => false]) ?>
+				<?= $this->render('control_block', ['targetId' => "childGroups-{$group->id}", 'expanded' => false]) ?>
 			</div>
 		<?php endif; ?>
 		<div class="panel-title">
