dd327fab2278f04feaf5fdd82d64725c0bfbd578
Pozitronik <Pozitronik>
2019-09-26 11:57:04 +0300

    Оптимизация генератора JS: событие вешается на общий селектор, а не на каждый элемент отдельно

--- modules/groups/widgets/group_card/views/control_block.php
+++ modules/groups/widgets/group_card/views/control_block.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 /**
  * @var View $this
- * @var string $target
+ * @var string $targetId
  * @var bool $expanded -- initial state
  **/
 
@@ -12,17 +12,17 @@ use yii\bootstrap\Html;
 use yii\web\JsExpression;
 use yii\web\View;
 
-$this->registerJs("$('#{$target}').on('hidden.bs.collapse', function(){
+$this->registerJs("$('.panel-card, .panel-card-small').on('hidden.bs.collapse', function(){
     if ('undefined' !== typeof (Msnry)) Msnry.layout();
   });");
-$this->registerJs("$('#{$target}').on('shown.bs.collapse', function(){
+$this->registerJs("$('.panel-card, .panel-card-small').on('shown.bs.collapse', function(){
    if ('undefined' !== typeof (Msnry)) Msnry.layout();
   });");
 ?>
 <?= Html::button($expanded?IconsHelper::minimize():IconsHelper::maximize(), [
 	'class' => 'btn btn-default',
 	'data-toggle' => "collapse",
-	'data-target' => "#{$target}",
+	'data-target' => "#{$targetId}",
 	'aria-expanded' => $expanded?'true':'false',
 	'onClick' => new JsExpression("changeIcon($(this));")
 ]) ?>
