3f24571dd6acf629c8170a95ae58cdcc0e84a8bb
Pozitronik <Pozitronik>
2019-10-24 13:12:25 +0300

    Справочник веток должностей ссылается на справочник должностей с корректным фильтром

--- modules/salary/models/references/RefUserPositionBranches.php
+++ modules/salary/models/references/RefUserPositionBranches.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 namespace app\modules\salary\models\references;
 
 use app\modules\references\models\Reference;
+use app\modules\references\ReferencesModule;
+use app\widgets\badge\BadgeWidget;
+use yii\helpers\Html;
 
 /**
  * Справочник веток должностей. Ветка должности - необязательный, не влияющий ни на что атрибут должности.
@@ -31,4 +34,47 @@ class RefUserPositionBranches extends Reference {
 		];
 	}
 
+	/**
+	 * Набор колонок для отображения на главной
+	 * @return array
+	 */
+	public function getColumns():array {
+		return [
+			[
+				'attribute' => 'id',
+				'options' => [
+					'style' => 'width:36px;'
+				]
+			],
+			[
+				'attribute' => 'name',
+				'value' => static function($model) {
+					/** @var self $model */
+					return $model->deleted?Html::tag('span', "Удалено:", [
+							'class' => 'label label-danger'
+						]).$model->name:BadgeWidget::widget([
+						'models' => $model,
+						'attribute' => 'name',
+						'linkScheme' => [ReferencesModule::to(['references/update']), 'id' => 'id', 'class' => $model->formName()],
+						'itemsSeparator' => false
+					]);
+				},
+				'format' => 'raw'
+			],
+			[
+				'attribute' => 'usedCount',
+				'filter' => false,
+				'value' => static function($model) {
+					/** @var self $model */
+					return BadgeWidget::widget([
+						'models' => $model,
+						'attribute' => 'usedCount',
+						'linkScheme' => [ReferencesModule::to(['references/index']), 'class' => 'RefUserPositions', 'RefUserPositions[branch]' => 'id'],
+						'itemsSeparator' => false
+					]);
+				},
+				'format' => 'raw'
+			]
+		];
+	}
 }
