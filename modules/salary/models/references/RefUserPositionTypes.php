c46323264bc2706a657f1d619149da8f7ebf8072
Pozitronik <Pozitronik>
2019-10-24 12:23:50 +0300

    Комментарий о работе счётчика + ссылка с бейджа на фильтр

--- modules/salary/models/references/RefUserPositionTypes.php
+++ modules/salary/models/references/RefUserPositionTypes.php
@@ -6,10 +6,14 @@ namespace app\modules\salary\models\references;
 use app\models\relations\RelUsersGroups;
 use app\modules\groups\models\Groups;
 use app\modules\references\models\CustomisableReference;
+use app\modules\references\ReferencesModule;
 use app\modules\salary\models\relations\RelRefUserPositionsTypes;
 use app\modules\users\models\relations\RelUserPositionsTypes;
 use app\modules\users\models\Users;
+use app\modules\users\UsersModule;
+use app\widgets\badge\BadgeWidget;
 use yii\db\ActiveQuery;
+use yii\helpers\Html;
 
 /**
  * Справочник типов должностей. Тип должности -  необязательный, не влияющий ни на что атрибут должности.
@@ -75,7 +79,57 @@ class RefUserPositionTypes extends CustomisableReference {
 	 * @return int
 	 */
 	public function getUsedCount():int {
+		/* Посчитает только отношения к должностям, привязка типов должностей непосредственно к пользователям учитываться не будет. Это корректно в данном разрезе.
+		 * Это может привести к разночтениям после фильтрации на странице поиска (там условие учитывает все варианты).
+		 */
 		return (int)RelUserPositionsTypes::find()->where(['position_type_id' => $this->id])->count();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getColumns():array {
+		return [
+			[
+				'attribute' => 'id',
+				'options' => [
+					'style' => 'width:36px;'
+				]
+			],
+			[
+				'attribute' => 'name',
+				'value' => static function($model) {
+					/** @var self $model */
+					return $model->deleted?Html::tag('span', "Удалено:", [
+							'class' => 'label label-danger'
+						]).$model->name:BadgeWidget::widget([
+						'models' => $model,
+						'attribute' => 'name',
+						'linkScheme' => [ReferencesModule::to(['references/update']), 'id' => 'id', 'class' => $model->formName()],
+						'itemsSeparator' => false,
+						"optionsMap" => self::colorStyleOptions()
+					]);
+				},
+				'format' => 'raw'
+			],
+			[
+				'attribute' => 'usedCount',
+				'filter' => false,
+				'value' => static function($model) {
+					/** @var self $model */
+					return BadgeWidget::widget([
+						'models' => $model,
+						'attribute' => 'usedCount',
+						'linkScheme' => [UsersModule::to(['users/index']), 'UsersSearch[positionType]' => 'id'],
+						'itemsSeparator' => false,
+						"optionsMap" => static function() {
+							return self::colorStyleOptions();
+						}
+					]);
+				},
+				'format' => 'raw'
+			]
+		];
 	}
 
 }
