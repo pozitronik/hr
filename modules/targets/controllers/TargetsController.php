af3c8e6bb29e2c58a0cd00fb227bf54d67990113
Pozitronik <Pozitronik>
2020-01-22 12:13:10 +0300

    WigetableController stuff

--- modules/targets/controllers/TargetsController.php
+++ modules/targets/controllers/TargetsController.php
@@ -3,15 +3,20 @@ declare(strict_types = 1);
 
 namespace app\modules\targets\controllers;
 
+use app\models\core\WigetableController;
 use app\modules\targets\models\TargetsSearch;
 use Yii;
-use yii\base\Controller;
 
 /**
  * Class TargetsController
  * @package app\modules\targets\controllers
  */
-class TargetsController extends Controller {
+class TargetsController extends WigetableController {
+	public $menuCaption = "<i class='fa fa-arrow-circle-left'></i>Целеполагание";
+	public $menuIcon = "/img/admin/privileges.png";
+	public $menuDisabled = false;
+	public $orderWeight = 15;
+	public $defaultRoute = 'targets/targets';
 
 	/**
 	 * @return string
