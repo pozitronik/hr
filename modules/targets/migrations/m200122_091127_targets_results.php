2b3f61e0781e91396617289b2c234e26c29c28cd
Pozitronik <Pozitronik>
2020-01-22 12:47:33 +0300

    Фикс миграции

--- modules/targets/migrations/m200122_091127_targets_results.php
+++ modules/targets/migrations/m200122_091127_targets_results.php
@@ -12,6 +12,7 @@ class m200122_091127_targets_results extends Migration {
 	public function safeUp() {//Шкала
 		$this->createTable('sys_targets_results',[
 			'id' => $this->primaryKey(),
+			'target' => $this->integer()->notNull()->comment('id цели'),
 			'comment' => $this->text()->null()->comment('Описание результата'),
 			'create_date' => $this->dateTime()->notNull()->comment('Дата создания'),
 			'marked_date' => $this->dateTime()->notNull()->comment('Дата, на которую приходится результата'),
@@ -19,6 +20,7 @@ class m200122_091127_targets_results extends Migration {
 			'status' => $this->integer()->null()->comment('Тип статуса исполнения'),
 		]);
 
+		$this->createIndex('target','sys_targets_results','target');
 		$this->createIndex('daddy','sys_targets_results','daddy');
 		$this->createIndex('status','sys_targets_results','status');
 	}
