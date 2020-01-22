15bdfd867a8fdadf0dab905483fc9311a123b918
Pozitronik <Pozitronik>
2020-01-22 13:05:53 +0300

    getName

--- modules/targets/TargetsModule.php
+++ modules/targets/TargetsModule.php
@@ -5,12 +5,16 @@ namespace app\modules\targets;
 
 use app\models\core\core_module\CoreModule;
 
-/** @noinspection EmptyClassInspection */
-
 /**
  * Class TargetsModule
  * @package app\modules\targets
  */
 class TargetsModule extends CoreModule {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getName():string {
+		return 'Целеполагание';
+	}
 
 }
