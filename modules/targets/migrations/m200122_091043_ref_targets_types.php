9f17c45f3798aae275bc1f36ee322bd6875058df
Pozitronik <Pozitronik>
2020-01-22 12:29:53 +0300

    Миграция на справочник типов целей

---
+++ modules/targets/migrations/m200122_091043_ref_targets_types.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200122_091043_ref_targets_types
+ */
+class m200122_091043_ref_targets_types extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable('ref_targets_types', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(256)->notNull()->comment('Название'),
+			'value' => $this->string(512)->notNull()->comment('Описание'),
+			'deleted' => $this->boolean()->notNull()->defaultValue(false)
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable('ref_targets_types');
+	}
+
+	/*
+	// Use up()/down() to run migration code without a transaction.
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m200122_091043_ref_targets_types cannot be reverted.\n";
+
+		return false;
+	}
+	*/
+}
