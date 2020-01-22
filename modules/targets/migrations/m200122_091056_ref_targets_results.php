89a708f5ce591decba42bcf145343dfb8045fd85
Pozitronik <Pozitronik>
2020-01-22 12:35:01 +0300

    Миграция на типы результатов исполнения целей

---
+++ modules/targets/migrations/m200122_091056_ref_targets_results.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200122_091056_ref_targets_results
+ */
+class m200122_091056_ref_targets_results extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->createTable('ref_targets_results', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(256)->notNull()->comment('Название'),
+			'value' => $this->string(512)->notNull()->comment('Описание'),
+			'deleted' => $this->boolean()->notNull()->defaultValue(false)
+		]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('ref_targets_results');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200122_091056_ref_targets_results cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
