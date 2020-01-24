1e7839d1c1a82638478d96e85d9d0e6f22b23947
Pozitronik <Pozitronik>
2020-01-24 19:43:23 +0300

    Попытка написать универсальный виджет-выбиралку

---
+++ widgets/select_model/SelectModelWidgetAssets.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\widgets\select_model;
+
+use yii\web\AssetBundle;
+use app\assets\AppAsset;
+
+/**
+ * Class SelectModelWidgetAssets
+ * @package app\components\select_model
+ */
+class SelectModelWidgetAssets extends AssetBundle {
+	public $sourcePath = '@app/widgets/select_model/assets';
+	public $css = [
+		'css/select_model.css'
+	];
+	public $js = [
+		'js/select_model.js'
+	];
+	public $depends = [
+		AppAsset::class
+	];
+
+	public $publishOptions = [
+		'forceCopy' => YII_ENV_DEV
+	];
+}
