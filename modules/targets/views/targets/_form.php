d63dc0872620cdfecc67d0988e98ce87e0ecc88c
Pozitronik <Pozitronik>
2020-01-24 17:57:17 +0300

    Пока такое решение

--- modules/targets/views/targets/_form.php
+++ modules/targets/views/targets/_form.php
@@ -6,12 +6,16 @@ declare(strict_types = 1);
  * @var Targets $model
  */
 
+use app\modules\groups\models\Groups;
 use app\modules\groups\widgets\group_select\GroupSelectWidget;
 use app\modules\references\widgets\reference_select\ReferenceSelectWidget;
 use app\modules\targets\models\references\RefTargetsResults;
 use app\modules\targets\models\references\RefTargetsTypes;
 use app\modules\targets\models\Targets;
+use app\modules\users\models\Users;
 use app\modules\users\widgets\user_select\UserSelectWidget;
+use kartik\select2\Select2;
+use pozitronik\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\web\View;
 use yii\widgets\ActiveForm;
@@ -56,13 +60,26 @@ use yii\widgets\ActiveForm;
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-					<?= $form->field($model, 'relGroups')->widget(GroupSelectWidget::class, [
-						'notData' => $model->relGroups,
-						'multiple' => true,
-						'mode' => GroupSelectWidget::MODE_FIELD,
-						'dataMode' => GroupSelectWidget::DATA_MODE_AJAX
-					]) ?>
-
+					<?= $form->field($model, 'relGroups')->widget(Select2::class, [
+						'data' => ArrayHelper::map(Groups::find()->all(), 'id', 'name'),
+						'options' => ['placeholder' => ''],
+						'pluginOptions' => [
+							'allowClear' => true,
+							'multiple' => true
+						],
+					]); ?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<?= $form->field($model, 'relUsers')->widget(Select2::class, [
+						'data' => ArrayHelper::map(Users::find()->all(), 'id', 'username'),
+						'options' => ['placeholder' => ''],
+						'pluginOptions' => [
+							'allowClear' => true,
+							'multiple' => true
+						],
+					]); ?>
 				</div>
 			</div>
 			<div class="row">
@@ -72,12 +89,7 @@ use yii\widgets\ActiveForm;
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-					<?= $form->field($model, 'relUsers')->widget(UserSelectWidget::class, [
-						'notData' => $model->relUsers,
-						'multiple' => true,
-						'mode' => GroupSelectWidget::MODE_FIELD,
-						'dataMode' => GroupSelectWidget::DATA_MODE_AJAX
-					]) ?>
+
 				</div>
 			</div>
 		</div>
