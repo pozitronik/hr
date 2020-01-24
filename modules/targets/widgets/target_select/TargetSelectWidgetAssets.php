d0ded0c3c1c8bd44315dc4afa0d8f2d25cf26d9c
Pozitronik <Pozitronik>
2020-01-24 12:00:41 +0300

    Стаб виджета выбора цели

---
+++ modules/targets/widgets/target_select/TargetSelectWidgetAssets.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\targets\widgets\target_select;
+
+use yii\web\AssetBundle;
+use app\assets\AppAsset;
+
+/**
+ * Class TargetSelectWidgetAssets
+ * @package app\components\target_select
+ */
+class TargetSelectWidgetAssets extends AssetBundle {
+	public $sourcePath = '@app/modules/targets/widgets/target_select/assets';
+	public $css = [
+		'css/target_select.css'
+	];
+	public $js = [
+		'js/target_select.js'
+	];
+	public $depends = [
+		AppAsset::class
+	];
+}
+
+
+
+
+
+
+
+
