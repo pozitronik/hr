e0df7e85676a51c6ecb377569e81f868e770e308
Pozitronik <Pozitronik>
2020-01-27 12:54:56 +0300

    Виджет переписан, как потомок SelectModelWidget

--- modules/users/widgets/user_select/UserSelectWidget.php
+++ modules/users/widgets/user_select/UserSelectWidget.php
@@ -3,42 +3,19 @@ declare(strict_types = 1);
 
 namespace app\modules\users\widgets\user_select;
 
-use pozitronik\helpers\ArrayHelper;
-use app\models\core\SelectionWidgetInterface;
 use app\modules\users\models\Users;
-use kartik\base\InputWidget;
-use yii\db\ActiveRecord;
-use yii\helpers\Html;
+use app\widgets\select_model\SelectModelWidget;
 
 /**
  * Class UserSelectWidget
  * Виджет списка пользователей (для добавления в группу)
  *
- * @property ActiveRecord|null $model При использовании виджета в ActiveForm ассоциируем с моделью...
- * @property string|null $attribute ...и свойством модели
- * @property array $notData Группы, исключённые из списка (например те, в которых пользователь уже есть)
- * @property bool $groupByType Группировка списка по типам групп (двухуровневый список)
- * @property string $formAction Свойство для переопределения экшона формы постинга (при MODE_FORM)
- * @property boolean $multiple
- * @property int $mode Режим рендеринга
- * @property int $dataMode Режим загрузки данных
- *
- * @todo: я плохо спроектировал эту выбиралку. Она привязана к моделям групп, и не является универсальной. Требуется полностью её переделать, соблюдая следующие принципы:
- * 1) Выбиралка должна учитывать ТОЛЬКО пользоватеелй. Ни о каких более моделях она не знает.
- * 2) Соответственно, вся фильтрация должна происходить уровнем выше.
- * 3) Аяксовая фильтрация, используемая соответствующим методом загрузки... да хрен с ней, добавление дубликатов фильтруется на сервере
- *
- * @fixme: действительно, плохо спроектировал. Привязка к модели для автоматической генерации имени поля заодно вытаскивает и данные этой модели для отображения. Пока пользоваться можно, но лучше переделать.
- *
- * @deprecated
  */
-class UserSelectWidget extends InputWidget implements SelectionWidgetInterface {
-	public $mode = self::MODE_FIELD;
-	public $dataMode = self::DATA_MODE_LOAD;
-	public $notData = [];
+class UserSelectWidget extends SelectModelWidget {
+	public $selectModel = Users::class;
 	public $multiple = false;
-	public $groupByType = true;
-	public $formAction = '';
+	public $ajaxSearchUrl = '/users/ajax/user-search';
+	public $mapAttribute = 'username';
 
 	/**
 	 * Функция инициализации и нормализации свойств виджета
@@ -46,55 +23,7 @@ class UserSelectWidget extends InputWidget implements SelectionWidgetInterface {
 	public function init() {
 		parent::init();
 		UserSelectWidgetAssets::register($this->getView());
-		$this->options['id'] = isset($this->options['id'])?$this->options['id'].$this->model->primaryKey:Html::getInputId($this->model, $this->attribute).$this->model->primaryKey;
+		$this->postUrl = $this->renderingMode === self::MODE_AJAX?'/users/ajax/users-add-to-group':$this->postUrl;
 	}
 
-	/**
-	 * Функция возврата результата рендеринга виджета
-	 * @return string
-	 */
-	public function run():string {
-		$data = self::DATA_MODE_AJAX === $this->dataMode?[]:ArrayHelper::map(Users::find()->active()->where(['not in', 'id', ArrayHelper::getColumn($this->notData, 'id')])
-			->all(), 'id', 'username');
-
-		switch ($this->mode) {
-			default:
-			case self::MODE_FIELD:
-				return $this->render('user_select_field', [
-					'model' => $this->model,
-					'attribute' => $this->attribute,
-					'data' => $data,
-					'data_mode' => $this->dataMode,
-					'multiple' => $this->multiple,
-					'options' => $this->options,
-					'ajax_search_url' => '/users/ajax/user-search'
-				]);
-			break;
-			case self::MODE_FORM:
-				return $this->render('user_select_form', [
-					'model' => $this->model,
-					'attribute' => $this->attribute,
-					'data' => $data,
-					'data_mode' => $this->dataMode,
-					'multiple' => $this->multiple,
-					'formAction' => $this->formAction,
-					'options' => $this->options,
-					'ajax_search_url' => '/users/ajax/user-search'
-				]);
-			break;
-			case self::MODE_AJAX:
-				return $this->render('user_select_ajax', [
-					'model' => $this->model,
-					'attribute' => $this->attribute,
-					'data' => $data,
-					'data_mode' => $this->dataMode,
-					'multiple' => $this->multiple,
-					'ajax_post_url' => '/users/ajax/users-add-to-group',
-					'options' => $this->options,
-					'ajax_search_url' => '/users/ajax/user-search'
-				]);
-			break;
-		}
-
-	}
 }
--- modules/users/widgets/user_select/assets/js/user_select.js
+++ modules/users/widgets/user_select/assets/js/user_select.js
@@ -1,53 +1,27 @@
-function formatUser(item) {
+/**
+ * Форматирование элемента списка по умолчанию
+ * @param item
+ * @returns {*}
+ */
+function templateResult(item) {
 	return item.text;
 }
 
-function submit_toggle(select) {
-	let input = jQuery(select.target).parent().find(':submit');
-	if (0 < jQuery(select.target).val().length) {
-		input.removeAttr('disabled');
-	} else {
-		input.attr('disabled', 'disabled');
-	}
-}
-
-function ajax_submit_toggle(select, button_id) {
-	let input = jQuery('#' + button_id);
-	if (0 < jQuery(select.target).val().length) {
-		input.removeAttr('disabled');
-	} else {
-		input.attr('disabled', 'disabled');
-	}
-}
-
-function ajax_post(postUrl, button_id, group_id) {
-	let input = jQuery('#' + button_id);
-	let values = input.parent().parent().find('select').val();
-
-	if (0 < values.length) {
-		input.attr('disabled', 'disabled');
-		jQuery.ajax({
-			url: postUrl,
-			data: {
-				userId: values,
-				groupId: group_id
-			},
-			method: 'POST'
-		}).done(function (data) {
-			input.removeAttr('disabled');
-		});
-	}
-
+/**
+ * Форматирование элемента списка при AJAX-запросе
+ * @param item
+ * @returns {*}
+ */
+function templateResultAJAX(item) {
+	return item.text;
 }
 
 
-function processResults(data, params) {//unused
-	params.page = params.page || 1;
-	return {
-		results: data.results,
-		pagination: {
-			more: 20 === data.results.length
-		}
-	};
-
+/**
+ * Форматирование <не помню для чего>
+ * @param markup
+ * @returns {*}
+ */
+function escapeMarkup(markup) {
+	return markup;
 }
