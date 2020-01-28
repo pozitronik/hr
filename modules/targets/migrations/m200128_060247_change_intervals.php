a30a9f8d7f65629faae06e66cca722873287fd74
Pozitronik <Pozitronik>
2020-01-28 09:11:28 +0300

    Фикс миграции

--- modules/targets/migrations/m200128_060247_change_intervals.php
+++ modules/targets/migrations/m200128_060247_change_intervals.php
@@ -14,7 +14,12 @@ class m200128_060247_change_intervals extends Migration {
 		$this->alterColumn('sys_targets_intervals', 'finish_date', $this->dateTime()->null()->comment('Дата начала интервала'));
 
 		$this->addColumn('sys_targets_intervals', 'start_quarter', $this->smallInteger()->null()->comment('Стартовый квартал'));
-		$this->addColumn('sys_targets_intervals', 'finish_quarter', $this->smallInteger()->null()->comment('Abyfkmysq квартал'));
+		$this->addColumn('sys_targets_intervals', 'finish_quarter', $this->smallInteger()->null()->comment('Финальный квартал'));
+		$this->addColumn('sys_targets_intervals', 'year', $this->integer()->null()->comment('Год'));
+
+		$this->createIndex('start_quarter', 'sys_targets_intervals', 'start_quarter');
+		$this->createIndex('finish_quarter', 'sys_targets_intervals', 'finish_quarter');
+		$this->createIndex('year', 'sys_targets_intervals', 'year');
 	}
 
 	/**
@@ -26,6 +31,7 @@ class m200128_060247_change_intervals extends Migration {
 
 		$this->dropColumn('sys_targets_intervals', 'start_quarter');
 		$this->dropColumn('sys_targets_intervals', 'finish_quarter');
+		$this->dropColumn('sys_targets_intervals', 'year');
 	}
 
 	/*
