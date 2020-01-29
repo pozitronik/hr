25585e2da0fe3b2c57bdf00d26bbaf8503b0a7c9
Pozitronik <Pozitronik>
2020-01-29 11:39:59 +0300

    Промежуточная поисковая модель и вьюха для импорта

---
+++ modules/targets/views/import/index.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @var View $this
+ * @var ActiveRecord $searchModel
+ * @var ActiveDataProvider $dataProvider
+ * @var int $domain
+ */
+
+use app\helpers\Utils;
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveRecord;
+use yii\web\View;
+use kartik\grid\GridView;
+use yii\bootstrap\Html;
+
+?>
+
+<?= GridView::widget([
+	'dataProvider' => $dataProvider,
+	'filterModel' => $searchModel,
+	'panel' => [
+		'heading' => 'Загружено '.Utils::pluralForm($dataProvider->totalCount, ['строка', 'строки', 'строк'])
+	],
+	'toolbar' => [
+		[
+			'content' => Html::a('Назад', ['upload'], ['class' => 'btn btn-default'])
+		],
+		[
+			'content' => Html::a('Декомпозиция', ['decompose', 'domain' => $domain], ['class' => 'btn btn-success'])
+		]
+	],
+	'export' => false,
+	'resizableColumns' => true,
+	'responsive' => true,
+	'columns' => [
+	]
+]) ?>
