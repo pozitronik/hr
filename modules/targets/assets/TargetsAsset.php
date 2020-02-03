5475bb4dc2203cfa57fc3fb1c23d8b6c63d185be
Pozitronik <Pozitronik>
2020-02-03 12:32:10 +0300

    Ассеты для дополнительного стиля бейджа

---
+++ modules/targets/assets/TargetsAsset.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\targets\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class TargetsAsset
+ * @package app\modules\targets\assets
+ */
+class TargetsAsset extends AssetBundle {
+	public $sourcePath = '@app/modules/targets/assets';
+
+	public $css = [
+		'css/targets.css'
+	];
+	public $js = [
+	];
+
+	public $publishOptions = [
+		'forceCopy' => YII_ENV_DEV
+	];
+}
