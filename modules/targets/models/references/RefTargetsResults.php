0c07a5a42ab5481ffd145e7c5cd3c79c68a97b72
Pozitronik <Pozitronik>
2020-02-11 11:23:06 +0300

    реализация usedCount в справочнике типов ответов

--- modules/targets/models/references/RefTargetsResults.php
+++ modules/targets/models/references/RefTargetsResults.php
@@ -5,6 +5,7 @@ namespace app\modules\targets\models\references;
 
 
 use app\modules\references\models\CustomisableReference;
+use app\modules\targets\models\Targets;
 
 /**
  * @property int $id
@@ -21,5 +22,13 @@ class RefTargetsResults extends CustomisableReference {
 	public static function tableName():string {
 		return 'ref_targets_results';
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getUsedCount():int {
+		return (int)Targets::find()->where(['result_type' => $this->id])->count();
+	}
+
 
 }
