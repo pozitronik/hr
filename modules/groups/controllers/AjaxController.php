989540935986a662fc473773ee88edeb5154929f
Pozitronik <Pozitronik>
2020-02-14 12:09:55 +0300

    Экшены из ajax-контроллера референсов перенесены в ajax-контроллер групп (пока без правки урлов)

--- modules/groups/controllers/AjaxController.php
+++ modules/groups/controllers/AjaxController.php
@@ -125,4 +125,70 @@ class AjaxController extends BaseAjaxController {
 		$parentGroup->setDropChildGroups([$childGroup]);
 		return $this->answer->answer;
 	}
+
+//todo: поправить урлы нижеследующих вызовов. Было /references/ajax, стало groups/ajax
+
+	/**
+	 * Принимает массив ролей пользователя, применяя их
+	 * @return array
+	 * @throws Throwable
+	 */
+	public function actionSetUserRolesInGroup():array {
+		$groupId = Yii::$app->request->post('groupId', false);
+		$userId = Yii::$app->request->post('userId', false);
+		if (!($groupId && $userId)) {
+			return $this->answer->addError('parameters', 'Not enough parameters');
+		}
+		/** @var Groups $group */
+		if (null === ($group = Groups::findModel($groupId))) {
+			return $this->answer->addError('group', 'Not found');
+		}
+
+		$group->setRolesInGroup([$userId => Yii::$app->request->post('roles', [])]);
+		return $this->answer->answer;
+
+	}
+
+	/**
+	 * Принимает и применяет тип релейшена между двумя группами
+	 * Предполагается, что релейшен уже существует
+	 * @return array
+	 * @throws Throwable
+	 */
+	public function actionSetGroupRelationType():array {
+		$parentGroupId = Yii::$app->request->post('parentGroupId', false);
+		$childGroupId = Yii::$app->request->post('childGroupId', false);
+		$relation = Yii::$app->request->post('relation', false);
+		if (!($parentGroupId && $childGroupId)) {
+			return $this->answer->addError('parameters', 'Not enough parameters');
+		}
+
+		/** @var Groups $group */
+		if (false === ($groupsRelation = RelGroupsGroups::find()->where(['parent_id' => $parentGroupId, 'child_id' => $childGroupId])->one())) {
+			return $this->answer->addError('groupsRelation', 'Not found');
+		}
+		$groupsRelation->setAndSaveAttribute('relation', $relation);
+		return $this->answer->answer;
+
+	}
+
+	/**
+	 * Принимает и применяет тип группы
+	 * @return array
+	 * @throws Throwable
+	 */
+	public function actionSetGroupType():array {
+		$groupId = Yii::$app->request->post('groupId', false);
+		$type = Yii::$app->request->post('type', false);
+		if (!$groupId) {
+			return $this->answer->addError('parameters', 'Not enough parameters');
+		}
+		/** @var Groups $group */
+		if (null === ($group = Groups::findModel($groupId))) {
+			return $this->answer->addError('group', 'Not found');
+		}
+
+		$group->setAndSaveAttribute('type', $type);
+		return $this->answer->answer;
+	}
 }
