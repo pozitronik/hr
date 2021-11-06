25aa9c681161aaacd0a6c44c50a0205e42a807e2
Pozitronik <antikillerxxl@gmail.com>
2021-11-06 14:41:00 +0300

    Оживляю труп. Вернул заrоменченную фичу

--- widgets/navbar/views/dropdown.php
+++ widgets/navbar/views/dropdown.php
@@ -8,6 +8,8 @@ declare(strict_types = 1);
 
 use app\components\pozitronik\core\models\core_module\PluginsSupport;
 use app\modules\home\HomeModule;
+use app\modules\privileges\models\rights\admin\ServiceAccess;
+use app\modules\privileges\models\UserAccess;
 use yii\base\View;
 use yii\helpers\Html;
 use app\modules\users\widgets\bookmarks\BookmarksWidget;
@@ -33,7 +35,7 @@ use app\modules\users\models\Users;
 					<li class="dropdown-header">Навигация</li>
 					<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no">Дашборд</p><small class="text-muted">Дашборд всех моих групп</small></div>', HomeModule::to("home")) ?></li>
 					<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no">Атрибуты</p><small class="text-muted">Поиск сотрудников по атрибутам</small></div>', ["/attributes/attributes/search"]) ?></li>
-					<?php if (false /*&& UserAccess::GetFlag(ServiceAccess::FLAG_SERVICE)*/): ?>
+					<?php if (UserAccess::GetFlag(ServiceAccess::FLAG_SERVICE)): ?>
 						<li><?= Html::a('<div class="media-body"><p class="text-semibold text-dark mar-no"><i class="fa fa-radiation-alt"></i> Ядерный пепел <i class="fa fa-radiation-alt"></i></p><small class="text-muted">Сброс сервиса в исходное состояние</small></div>', ["/service/service/index"]) ?></li>
 					<?php endif; ?>
 				</ul>
