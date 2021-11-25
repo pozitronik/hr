93d7823b67a9d2f39fd82a93793e69c2e69f9b02
Pozitronik <antikillerxxl@gmail.com>
2021-11-25 16:29:08 +0300

    Модели структуры импорта билдайновского

---
+++ modules/import/models/beeline/active_record/ImportBeelineBase.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\import\models\beeline\activerecord;
+
+use app\components\pozitronik\core\traits\ARExtended;
+use yii\db\ActiveRecord;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $domain
+ * @property null|int $hr_group_id
+ */
+abstract class ImportBeelineBase extends ActiveRecord {
+	use ARExtended;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['name'], 'string', 'max' => 255],
+			[['name'], 'unique'],
+			[['name'], 'required'],
+			['domain', 'integer'], ['domain', 'required'],
+			['hr_group_id', 'integer']
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'name' => 'Name'
+		];
+	}
+}
---
+++ modules/import/models/beeline/active_record/ImportBeelineDepartment.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\import\models\beeline\active_record;
+
+use app\modules\import\models\beeline\activerecord\ImportBeelineBase;
+
+/**
+ * Class ImportBeelineBusinessBlock
+ */
+class ImportBeelineDepartment extends ImportBeelineBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function tableName():string {
+		return 'import_beeline_department';
+	}
+
+}
