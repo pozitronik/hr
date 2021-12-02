ddf05f42c72f7dc49d495d6cebf01c1ef16aa52a
Pozitronik <antikillerxxl@gmail.com>
2021-12-02 13:03:41 +0300

    Таблица раскупоривания руководителей

---
+++ migrations/m211202_092910_import_beeline_boss.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+use yii\db\Migration;
+
+/**
+ * Class m211202_092910_import_beeline_boss
+ */
+class m211202_092910_import_beeline_boss extends Migration {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeUp() {
+		$this->createTable('import_beeline_boss', [
+			'id' => $this->primaryKey(),
+			'name' => $this->string(255),
+			'position' => $this->string(255),
+			'level' => $this->integer()->null(),
+			'hr_user_id' => $this->integer()->null()
+		]);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function safeDown() {
+		$this->dropTable('import_beeline_boss');
+	}
+
+}
---
+++ modules/import/models/beeline/active_record/ImportBeelineBoss.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\import\models\beeline\active_record;
+
+use app\components\pozitronik\core\traits\ARExtended;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "import_beeline_boss".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $position
+ * @property int $level
+ * @property int $hr_user_id
+ */
+class ImportBeelineBoss extends ActiveRecord {
+	use ARExtended;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'import_beeline_boss';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['level', 'hr_user_id'], 'integer'],
+			[['name', 'position'], 'string', 'max' => 255],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'name' => 'Name',
+			'position' => 'Position',
+			'level' => 'Level',
+			'hr_user_id' => 'Hr User ID',
+		];
+	}
+}
