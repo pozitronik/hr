7da2c6fc05c03336d47b4293d4ff1e32f47ca046
Pozitronik <antikillerxxl@gmail.com>
2021-12-09 21:38:29 +0300

    Модели дополнений структуры

---
+++ modules/import/models/beeline/active_record/ImportBeelineCommand.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\import\models\beeline\active_record;
+
+use app\components\pozitronik\core\traits\ARExtended;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "import_beeline_command".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $domain
+ */
+class ImportBeelineCommand extends ActiveRecord {
+	use ARExtended;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'import_beeline_command';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['user_id', 'domain'], 'integer'],
+			[['domain'], 'required'],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'user_id' => 'User ID',
+			'domain' => 'Domain',
+		];
+	}
+}
---
+++ modules/import/models/beeline/active_record/ImportBeelineTribe.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace app\modules\import\models\beeline\active_record;
+
+use app\components\pozitronik\core\traits\ARExtended;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "import_beeline_tribe".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $domain
+ */
+class ImportBeelineTribe extends ActiveRecord {
+	use ARExtended;
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName():string {
+		return 'import_beeline_tribe';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules():array {
+		return [
+			[['user_id', 'domain'], 'integer'],
+			[['domain'], 'required'],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels():array {
+		return [
+			'id' => 'ID',
+			'user_id' => 'User ID',
+			'domain' => 'Domain',
+		];
+	}
+}
